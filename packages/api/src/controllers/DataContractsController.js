a7c9fedc3207f7ca0a49bad31061335779b9ef0b
pshenmic <pshenmic@gmail.com>
2023-10-06 23:23:57 +0700

    Refactor to rank

--- packages/api/src/controllers/DataContractsController.js
+++ packages/api/src/controllers/DataContractsController.js
@@ -7,15 +7,12 @@ class DataContractsController {
 
     getDataContracts = async (request, response) => {
         const subquery = this.knex('data_contracts')
-            .distinctOn('identifier')
-            .select('data_contracts.id as id',
-                'data_contracts.identifier as identifier',
-                'data_contracts.version as version')
+            .select(this.knex.raw('data_contracts.id as id, data_contracts.identifier as identifier, data_contracts.version as version, rank() over (partition by identifier order by version desc) rank'))
             .as('data_contracts')
 
         const rows = await this.knex(subquery)
-            .select('id', 'identifier', 'version')
-            .orderBy('id', 'asc')
+            .select('id', 'identifier', 'version', 'rank')
+            .where('rank', '=', 1)
 
         response.send(rows.map(dataContract => DataContract.fromJSON(dataContract)));
     }
