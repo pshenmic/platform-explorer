6d4d42aa1403af27ddd29f06f3f3bf635a6035af
pshenmic <pshenmic@gmail.com>
2024-03-14 02:33:26 +0300

    Add database migrations

---
+++ packages/api/test/utils/migrate.js
@@ -0,0 +1,43 @@
+const pg = require('pg');
+const fs = require('fs');
+const path = require('path');
+
+const [, , filepath] = process.argv;
+
+if (!filepath) {
+    throw new Error("Pass filepath as an argument")
+}
+
+
+const user = process.env.POSTGRES_USER
+const password = process.env.POSTGRES_PASS
+const host = process.env.POSTGRES_HOST
+const database = process.env.POSTGRES_DB
+
+const client = new pg.Client({
+    host,
+    database,
+    user,
+    password,
+})
+
+client
+    .connect()
+    .then(async () => {
+        console.log(path.join(process.cwd(), filepath))
+        const migrations = fs
+            .readdirSync(path.join(process.cwd(), filepath))
+            .map(sql => ({
+                // parsing migration id into number
+                id: parseInt(sql.match(/[^V\[](.*)__/g)[0].replace('__', '')),
+                sql_path: sql
+            }))
+            .sort((a, b) => a.id - b.id)
+            .map(({sql_path}) => fs.readFileSync(path.join(process.cwd(), filepath, sql_path)).toString())
+
+        for (const sql of migrations) {
+            await client.query(sql)
+        }
+    })
+    .then(() => console.log('Done'))
+    .finally(() => client.end())
