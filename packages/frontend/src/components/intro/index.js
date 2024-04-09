e9e0d466df4c3fee5ec95ac555d7de64dd0a4700
alexeyandreevsky <andreewsky.alexey@gmail.com>
2024-04-09 17:29:14 +0300

    Decrease padding of intro text block

--- packages/frontend/src/components/intro/index.js
+++ packages/frontend/src/components/intro/index.js
@@ -9,7 +9,7 @@ function Intro ({title, contentSource}) {
         <div>
             <Heading as={'h1'} size={'lg'} m={0}>{title}</Heading>
 
-            <Box my={6} w={16} h={'px'} background={'gray.700'} />
+            <Box my={4} w={16} h={'px'} background={'gray.700'} />
 
             <div>{contentSource}</div>
         </div>
