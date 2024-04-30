8aee145b92cdf05dbb2ec4dcdeebee985c61a218
pshenmic <pshenmic@gmail.com>
2024-04-30 15:21:42 +0700

    Fix missing from prev commit

--- packages/indexer/src/entities/document.rs
+++ packages/indexer/src/entities/document.rs
@@ -5,6 +5,9 @@ use dpp::state_transition::documents_batch_transition::document_create_transitio
 use dpp::state_transition::documents_batch_transition::document_delete_transition::v0::v0_methods::DocumentDeleteTransitionV0Methods;
 use dpp::state_transition::documents_batch_transition::document_replace_transition::v0::v0_methods::DocumentReplaceTransitionV0Methods;
 use dpp::state_transition::documents_batch_transition::document_transition::{DocumentTransition, DocumentTransitionV0Methods};
+use dpp::state_transition::documents_batch_transition::document_transition::document_purchase_transition::v0::v0_methods::DocumentPurchaseTransitionV0Methods;
+use dpp::state_transition::documents_batch_transition::document_transition::document_transfer_transition::v0::v0_methods::DocumentTransferTransitionV0Methods;
+use dpp::state_transition::documents_batch_transition::document_transition::document_update_price_transition::v0::v0_methods::DocumentUpdatePriceTransitionV0Methods;
 use serde_json::Value;
 
 #[derive(Clone)]
@@ -16,7 +19,7 @@ pub struct Document {
     pub data: Option<Value>,
     pub deleted: bool,
     pub revision: Revision,
-    pub is_system: bool
+    pub is_system: bool,
 }
 
 impl From<DocumentTransition> for Document {
@@ -64,7 +67,57 @@ impl From<DocumentTransition> for Document {
                 let base = transition.base().clone();
                 let identifier = base.id();
                 let data_contract_identifier = base.data_contract_id();
-                let revision: Revision = Revision::from(0 as u64);
+
+                return Document {
+                    id: None,
+                    identifier,
+                    owner: None,
+                    data: None,
+                    data_contract_identifier,
+                    revision: Revision::from(0 as u64),
+                    deleted: true,
+                    is_system: false,
+                };
+            }
+            DocumentTransition::Transfer(transition) => {
+                let base = transition.base().clone();
+                let identifier = base.id();
+                let data_contract_identifier = base.data_contract_id();
+
+
+                return Document {
+                    id: None,
+                    identifier,
+                    owner: None,
+                    data: None,
+                    data_contract_identifier,
+                    revision: Revision::from(0 as u64),
+                    deleted: true,
+                    is_system: false,
+                };
+            }
+            DocumentTransition::UpdatePrice(transition) => {
+                let base = transition.base().clone();
+                let identifier = base.id();
+                let data_contract_identifier = base.data_contract_id();
+
+
+                return Document {
+                    id: None,
+                    identifier,
+                    owner: None,
+                    data: None,
+                    data_contract_identifier,
+                    revision: Revision::from(0 as u64),
+                    deleted: true,
+                    is_system: false,
+                };
+            }
+            DocumentTransition::Purchase(transition) => {
+                let base = transition.base().clone();
+                let identifier = base.id();
+                let data_contract_identifier = base.data_contract_id();
+
 
                 return Document {
                     id: None,
