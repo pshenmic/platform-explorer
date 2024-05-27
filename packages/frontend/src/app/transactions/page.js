2af3f7159458d6c1305d18fddf59eba74e68b30e
alexeyandreevsky <andreewsky.alexey@gmail.com>
2024-05-27 17:56:31 +0300

    Add Transactions history chart to the Transactions page

--- packages/frontend/src/app/transactions/page.js
+++ packages/frontend/src/app/transactions/page.js
@@ -2,9 +2,8 @@ import Transactions from './Transactions'
 import Intro from '../../components/intro/index.js'
 import Markdown from '../../components/markdown'
 import introContent from './intro.md'
-import {
-  Container
-} from '@chakra-ui/react'
+import { Container, Flex, Box } from '@chakra-ui/react'
+import TransactionsHistory from '../../components/charts/TransactionsHistory'
 
 export const metadata = {
   title: 'Transactions — Dash Platform Explorer',
@@ -21,10 +20,24 @@ function TransactionsRoute () {
       mt={8}
       mb={0}
     >
-        <Intro
-            title={'Transactions'}
-            contentSource={<Markdown>{introContent}</Markdown>}
-        />
+      <Flex
+          justifyContent={'space-between'}
+          alignItems={'center'}
+          wrap={['wrap', 'wrap', 'wrap', 'nowrap']}
+      >
+          <Container maxW={['100%', '100%', '100%', 'calc(50% - 20px)']}>
+            <Intro
+              title={'Transactions'}
+              contentSource={<Markdown>{introContent}</Markdown>}
+            />
+          </Container>
+
+          <Box flexShrink={'0'} w={10} h={10} />
+
+          <Container maxW={'none'} p={0}>
+              <TransactionsHistory/>
+          </Container>
+      </Flex>
     </Container>
     <Transactions/>
   </>
