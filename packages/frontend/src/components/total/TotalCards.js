a9b2876179e849792d24037b8b9b306bcb067f9e
alexeyandreevsky <andreewsky.alexey@gmail.com>
2024-06-09 11:57:46 +0300

    Upload new components

---
+++ packages/frontend/src/components/total/TotalCards.js
@@ -0,0 +1,19 @@
+import InfoCard from '../cards'
+import ValueBlock from './ValueBlock'
+import './TotalCards.scss'
+
+export default function TotalCards ({ cards }) {
+  return (
+    <div className={'TotalCards'}>
+        {cards.map((card, i) => (
+            <InfoCard className={'TotalCards__Item'} key={i}>
+                <ValueBlock
+                    title={card.title}
+                    value={card.value}
+                    icon={card.icon}
+                />
+            </InfoCard>
+        ))}
+    </div>
+  )
+}
