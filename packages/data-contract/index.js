3f49b350d047a4f9c11a965d9d1402c3026955c4
owl352 <owl352@kokosinca123@gmail.com>
2024-06-11 02:01:34 +0500

    lint

--- packages/data-contract/index.js
+++ packages/data-contract/index.js
@@ -1,61 +1,60 @@
 require('dotenv').config()
 
-
 const Dash = require('dash')
 
-function logInfo(...messages) {
-    console.log('\x1b[32m [INFO]', ...messages, '\x1b[0m ')
+function logInfo (...messages) {
+  console.log('\x1b[32m [INFO]', ...messages, '\x1b[0m ')
 }
 
-async function main() {
-    logInfo('Client Initialization')
+async function main () {
+  logInfo('Client Initialization')
 
-    const schema = {
-        "dataContracts": {
-            "type": "object",
-            "properties": {
-                "identifier": {
-                    "type": "string",
-                    "minLength": 43,
-                    "maxLength": 44,
-                    "position": 0
-                },
-                "name": {
-                    "type": "string",
-                    "maxLength": 32,
-                    "minLength": 3,
-                    "position": 1.
-                }
-            },
-            "required": [
-                "identifier",
-                "name"
-            ],
-            "additionalProperties": false
-        }
-    };
-
-    let options = {
-        network: 'testnet',
-        wallet: {
-            mnemonic: process.env.MNEMONIC,
+  const schema = {
+    dataContracts: {
+      type: 'object',
+      properties: {
+        identifier: {
+          type: 'string',
+          minLength: 43,
+          maxLength: 44,
+          position: 0
         },
-    };
-
-    if (process.env.SKIP_SYNCHRONIZATION_BEFORE_HEIGHT) {
-        options.unsafeOptions = { skipSynchronizationBeforeHeight: Number(process.env.SKIP_SYNCHRONIZATION_BEFORE_HEIGHT), }
+        name: {
+          type: 'string',
+          maxLength: 32,
+          minLength: 3,
+          position: 1.0
+        }
+      },
+      required: [
+        'identifier',
+        'name'
+      ],
+      additionalProperties: false
     }
-    const client = new Dash.Client(options);
+  }
 
-    logInfo('Contract Deployment')
+  const options = {
+    network: 'testnet',
+    wallet: {
+      mnemonic: process.env.MNEMONIC
+    }
+  }
 
-    const identity = await client.platform.identities.get(process.env.OWNER_IDENTIFIER)
+  if (process.env.SKIP_SYNCHRONIZATION_BEFORE_HEIGHT) {
+    options.unsafeOptions = { skipSynchronizationBeforeHeight: Number(process.env.SKIP_SYNCHRONIZATION_BEFORE_HEIGHT) }
+  }
+  const client = new Dash.Client(options)
 
-    const contract = await client.platform.contracts.create(schema, identity);
-    const deployedContract = await client.platform.contracts.publish(contract, identity);
+  logInfo('Contract Deployment')
 
-    logInfo('All Done!');
-    logInfo(`Contract deployed at: ${deployedContract.getDataContract().getId(client)}`);
+  const identity = await client.platform.identities.get(process.env.OWNER_IDENTIFIER)
+
+  const contract = await client.platform.contracts.create(schema, identity)
+  const deployedContract = await client.platform.contracts.publish(contract, identity)
+
+  logInfo('All Done!')
+  logInfo(`Contract deployed at: ${deployedContract.getDataContract().getId(client)}`)
 }
 
 main().catch(console.error)
