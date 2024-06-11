f4042ee71ec7cb0d12ad2be944542a1c3b6b7d3a
alexeyandreevsky <andreewsky.alexey@gmail.com>
2024-06-11 13:40:00 +0300

    Update info cards size

--- packages/frontend/src/components/total/TotalCards.js
+++ packages/frontend/src/components/total/TotalCards.js
@@ -1,5 +1,6 @@
 import { InfoCard } from '../cards'
 import ValueBlock from './ValueBlock'
+import { Box } from '@chakra-ui/react'
 import './TotalCards.scss'
 
 export default function TotalCards ({ cards, loading = false }) {
@@ -7,12 +8,13 @@ export default function TotalCards ({ cards, loading = false }) {
     <div className={'TotalCards'}>
         {cards.map((card, i) => (
             <InfoCard className={'TotalCards__Item'} loading={loading} key={i}>
-              {!loading &&
-                <ValueBlock
-                  title={card.title}
-                  value={card.value}
-                  icon={card.icon}
-                />
+              {!loading
+                ? <ValueBlock
+                    title={card.title}
+                    value={card.value}
+                    icon={card.icon}
+                  />
+                : <Box h={'27px'}/>
               }
             </InfoCard>
         ))}
