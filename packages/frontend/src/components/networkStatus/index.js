8a02383fa2cc96b47a6170c925ca677539685088
alexeyandreevsky <andreewsky.alexey@gmail.com>
2024-06-18 23:08:50 +0300

    Update API request in NetworkStatus component

--- packages/frontend/src/components/networkStatus/index.js
+++ packages/frontend/src/components/networkStatus/index.js
@@ -12,18 +12,9 @@ function NetworkStatus () {
   const [status, setStatus] = useState({ data: {}, loading: true, error: false })
 
   const fetchData = useCallback(() => {
-    Promise.all([
-      Api.getStatus()
-        .then(res => fetchHandlerSuccess(setStatus, res))
-        .catch(err => fetchHandlerError(setStatus, err)),
-
-      Api.getBlocks(1, 1, 'desc')
-        .then(res => {
-          const [latestBlock] = res.resultSet
-          fetchHandlerSuccess(setStatus, { latestBlock })
-        })
-        .catch(err => fetchHandlerError(setStatus, err))
-    ])
+    Api.getStatus()
+      .then(res => fetchHandlerSuccess(setStatus, res))
+      .catch(err => fetchHandlerError(setStatus, err))
       .finally(() => setTimeout(fetchData(), 60000))
   }, [])
 
@@ -44,9 +35,9 @@ function NetworkStatus () {
     : <WarningTwoIcon color={'yellow.400'} ml={2}/>
 
   function getLastBlocktimeString () {
-    if (!status?.data?.latestBlock?.header?.timestamp) return 'n/a'
+    if (!status?.data?.api?.block?.timestamp) return 'n/a'
 
-    const diff = new Date() - new Date(status?.data?.latestBlock?.header?.timestamp)
+    const diff = new Date() - new Date(status?.data?.api?.block?.timestamp)
 
     if (diff < 60 * 1000) {
       return `${Math.floor((diff / 1000))} sec. ago`
@@ -143,10 +134,10 @@ function NetworkStatus () {
             <div className={`NetworkStatus__InfoItem ${status?.loading ? 'NetworkStatus__InfoItem--Loading' : ''}`}>
                 <div className={'NetworkStatus__Title'}>Latest block:</div>
 
-                {status?.data?.latestBlock?.header !== undefined
+                {status?.data?.api?.block?.height !== undefined
                   ? <div className={'NetworkStatus__Value'}>
-                        <Link href={`/block/${status.data.latestBlock.header.hash}`}>
-                            #{status.data.latestBlock.header.height}, {getLastBlocktimeString()}
+                        <Link href={`/block/${status?.data?.api?.block?.hash}`}>
+                            #{status?.data?.api?.block?.height}, {getLastBlocktimeString()}
                         </Link>
                     </div>
                   : <div className={'NetworkStatus__Value'}>n/a</div>}
