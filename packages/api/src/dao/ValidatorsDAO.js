bce960c4c70607bc5ba6a3c13054e33a07758beb
owl352 <kokosinca123@gmail.com>
2024-06-22 18:39:47 +0500

    query optimization

--- packages/api/src/dao/ValidatorsDAO.js
+++ packages/api/src/dao/ValidatorsDAO.js
@@ -1,29 +1,31 @@
 const Validator = require('../models/Validator')
 const PaginatedResultSet = require('../models/PaginatedResultSet')
 module.exports = class ValidatorsDAO {
-  constructor (knex) {
+  constructor(knex) {
     this.knex = knex
   }
 
   getValidatorByProTxHash = async (proTxHash) => {
     const [row] = await this.knex('validators')
-      .select('validators.pro_tx_hash as pro_tx_hash')
+      .select(
+        'validators.pro_tx_hash as pro_tx_hash',
+        this.knex('blocks')
+          .where('validator', proTxHash)
+          .select(this.knex.raw('MAX(timestamp) as latest_timestamp'),).as('latest_timestamp'),
+        this.knex('blocks')
+          .where('validator', proTxHash)
+          .select(this.knex.raw('MAX(height) as latest_height'),).as('latest_height'),
+        this.knex('blocks')
+          .where('validator', proTxHash)
+          .select(this.knex.raw('CAST( COUNT(*) as INT) as blocks_count'),).as('blocks_count')
+      )
       .where('validators.pro_tx_hash', proTxHash)
 
     if (!row) {
       return null
     }
 
-    const [proposedBlocks] = await this.knex('blocks')
-      .where('validator', row.pro_tx_hash)
-      .select(
-        'validator as pro_tx_hash',
-        this.knex.raw('MAX(timestamp) as latest_timestamp'),
-        this.knex.raw('MAX(height) as latest_height'),
-        this.knex.raw('CAST( COUNT(*) as INT) as blocks_count '))
-      .groupBy('pro_tx_hash')
-
-    return Validator.fromRow(proposedBlocks)
+    return Validator.fromRow(row)
   }
 
   getValidators = async (page, limit, order) => {
@@ -31,31 +33,31 @@ module.exports = class ValidatorsDAO {
     const toRank = fromRank + limit - 1
 
     const subquery = this.knex('validators')
-      .select(this.knex('validators').count('pro_tx_hash').as('total_count'),
-        'validators.pro_tx_hash as pro_tx_hash', 'id')
+      .select(
+        this.knex('validators').count('pro_tx_hash').as('total_count'),
+        'validators.pro_tx_hash as pro_tx_hash',
+        'id',
+        this.knex('blocks')
+          .whereRaw('pro_tx_hash = validator')
+          .select(this.knex.raw('MAX(timestamp) as latest_timestamp'),).as('latest_timestamp'),
+        this.knex('blocks')
+          .whereRaw('pro_tx_hash = validator')
+          .select(this.knex.raw('MAX(height) as latest_height'),).as('latest_height'),
+        this.knex('blocks')
+          .whereRaw('pro_tx_hash = validator')
+          .select(this.knex.raw('CAST( COUNT(*) as INT) as blocks_count'),).as('blocks_count'))
       .select(this.knex.raw(`rank() over (order by id ${order}) rank`))
       .as('validators')
 
     const rows = await this.knex(subquery)
-      .select('id', 'rank', 'total_count', 'pro_tx_hash')
+      .select('id', 'rank', 'total_count', 'pro_tx_hash', 'blocks_count', 'latest_height', 'latest_timestamp')
       .whereBetween('rank', [fromRank, toRank])
       .orderBy('id', order)
 
     const totalCount = rows.length > 0 ? Number(rows[0].total_count) : 0
 
-    const proTxHashes = rows.map((row) => row.pro_tx_hash)
-
-    const proposedBlocks = await this.knex('blocks')
-      .whereIn('validator', proTxHashes)
-      .select(
-        'validator as pro_tx_hash',
-        this.knex.raw('MAX(timestamp) as latest_timestamp'),
-        this.knex.raw('MAX(height) as latest_height'),
-        this.knex.raw('CAST( COUNT(*) as INT) as blocks_count '))
-      .groupBy('pro_tx_hash')
-
     const resultSet = rows.map((row) =>
-      Validator.fromRow(proposedBlocks.find((block) => block.pro_tx_hash === row.pro_tx_hash))
+      Validator.fromRow(row)
     )
 
     return new PaginatedResultSet(resultSet, page, limit, totalCount)
