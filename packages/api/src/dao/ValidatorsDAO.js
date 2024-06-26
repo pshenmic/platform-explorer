bc8bff3b8fb078ca3a9c4915443d7e5bee703f4f
owl352 <kokosinca123@gmail.com>
2024-06-26 15:15:05 +0500

    `getValidatorByProTxHash` now works as `getValidators`

--- packages/api/src/dao/ValidatorsDAO.js
+++ packages/api/src/dao/ValidatorsDAO.js
@@ -2,30 +2,58 @@ const Validator = require('../models/Validator')
 const PaginatedResultSet = require('../models/PaginatedResultSet')
 
 module.exports = class ValidatorsDAO {
-  constructor (knex) {
+  constructor(knex) {
     this.knex = knex
   }
 
   getValidatorByProTxHash = async (proTxHash) => {
-    const [row] = await this.knex('validators')
+    const validatorsSubquery = this.knex('validators')
       .select(
         'validators.pro_tx_hash as pro_tx_hash',
+        'validators.id',
         this.knex('blocks')
-          .where('validator', proTxHash)
           .count('*')
+          .whereRaw('blocks.validator = validators.pro_tx_hash')
           .as('proposed_blocks_amount'),
-        'blocks.timestamp as latest_timestamp',
-        'blocks.hash as block_hash',
-        'blocks.l1_locked_height as l1_locked_height',
-        'blocks.created_at as created_at',
-        'blocks.app_version as app_version',
-        'blocks.block_version as block_version',
-        'blocks.height as latest_height'
+        this.knex('blocks')
+          .select('hash')
+          .whereRaw('pro_tx_hash = blocks.validator')
+          .orderBy('height', 'desc')
+          .limit(1)
+          .as('proposed_block_hash')
       )
-      .rightJoin('blocks', 'blocks.validator', 'pro_tx_hash')
-      .orderBy('blocks.height', 'desc')
-      .where('validators.pro_tx_hash', proTxHash)
+      .where('validators.pro_tx_hash', proTxHash) // Добавляем это условие, чтобы искать только по нужному pro_tx_hash
+      .as('validators');
 
+    const subquery = this.knex(validatorsSubquery)
+      .select(
+        'pro_tx_hash',
+        'id',
+        'proposed_blocks_amount',
+        'blocks.hash as block_hash',
+        'blocks.height as latest_height',
+        'blocks.timestamp as latest_timestamp',
+        'blocks.l1_locked_height as l1_locked_height',
+        'blocks.app_version as app_version',
+        'blocks.block_version as block_version'
+      )
+      .leftJoin('blocks', 'blocks.hash', 'proposed_block_hash')
+      .as('blocks');
+
+    const [row] = await this.knex(subquery)
+      .select(
+        'id',
+        'pro_tx_hash',
+        'proposed_blocks_amount',
+        'block_hash',
+        'latest_height',
+        'latest_timestamp',
+        'l1_locked_height',
+        'app_version',
+        'block_version'
+      )
+      .where('pro_tx_hash', proTxHash) 
+    
     if (!row) {
       return null
     }
