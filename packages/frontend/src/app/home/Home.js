9b09ceb40261bf499e09376638df3f61a72c27ad
alexeyandreevsky <andreewsky.alexey@gmail.com>
2024-06-29 00:11:14 +0300

    Replace Chart on the home page to common chart component

--- packages/frontend/src/app/home/Home.js
+++ packages/frontend/src/app/home/Home.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, createRef } from 'react'
 import * as Api from '../../util/Api'
-import { LineChart } from '../../components/charts/index.js'
+import TransactionsHistory from '../../components/charts/TransactionsHistory'
 import { SimpleList } from '../../components/lists'
 import TotalInfo from '../../components/total/TotalInfo'
 import NetworkStatus from '../../components/networkStatus'
@@ -11,7 +11,7 @@ import Markdown from '../../components/markdown'
 import introContent from './intro.md'
 import { getTransitionTypeString, fetchHandlerSuccess, fetchHandlerError } from '../../util'
 import { ErrorMessageBlock } from '../../components/Errors'
-import { LoadingBlock, LoadingList } from '../../components/loading'
+import { LoadingList } from '../../components/loading'
 
 import {
   Box,
@@ -20,21 +20,12 @@ import {
   Flex
 } from '@chakra-ui/react'
 
-const transactionsChartConfig = {
-  timespan: {
-    default: '1w',
-    values: ['1h', '24h', '3d', '1w']
-  }
-}
-
 function Home () {
   const [status, setStatus] = useState({ data: {}, loading: true, error: false })
   const [dataContracts, setDataContracts] = useState({ data: {}, props: { printCount: 5 }, loading: true, error: false })
   const [transactions, setTransactions] = useState({ data: {}, props: { printCount: 8 }, loading: true, error: false })
   const [richestIdentities, setRichestIdentities] = useState({ data: {}, props: { printCount: 5 }, loading: true, error: false })
   const [trendingIdentities, setTrendingIdentities] = useState({ data: {}, props: { printCount: 5 }, loading: true, error: false })
-  const [transactionsHistory, setTransactionsHistory] = useState({ data: {}, props: { printCount: 5 }, loading: true, error: false })
-  const [transactionsTimespan, setTransactionsTimespan] = useState(transactionsChartConfig.timespan.default)
   const richListContainer = createRef()
   const richListRef = createRef()
   const trendingIdentitiesContainer = createRef()
@@ -68,12 +59,6 @@ function Home () {
   }
 
   useEffect(fetchData, [])
-
-  useEffect(() => {
-    Api.getTransactionsHistory(transactionsTimespan)
-      .then(res => fetchHandlerSuccess(setTransactionsHistory, { resultSet: res }))
-      .catch(err => fetchHandlerError(setTransactionsHistory, err))
-  }, [transactionsTimespan])
 
   function adaptList (container, listBlock, data, setDataFunc) {
     if (!container || !listBlock || data?.props.printCount >= data?.data?.resultSet?.length) return
@@ -184,71 +169,9 @@ function Home () {
                   wrap={['wrap', 'wrap', 'wrap', 'nowrap']}
                   mb={5}
               >
-                  <Flex
-                      className={'ChartBlock'}
-                      maxW={'none'}
-                      width={'100%'}
-                      mb={5}
-                      borderWidth={'1px'} borderRadius={'lg'}
-                      direction={'column'}
-                      p={3}
-                      pb={2}
-                  >
-                      <div className={'ChartBlock__Head'}>
-                          <Heading className={'ChartBlock__Title'} as={'h2'} size={'sm'}>Transactions history</Heading>
-
-                          <div className={'ChartBlock__TimeframeContainer'}>
-                              <span>Timeframe: </span>
-                              <select
-                                  className={'ChartBlock__TimeframeSelector'}
-                                  onChange={(e) => setTransactionsTimespan(e.target.value)}
-                                  value={transactionsTimespan}
-                              >
-                                  {transactionsChartConfig.timespan.values.map(timespan => {
-                                    return <option value={timespan} key={'ts' + timespan}>{timespan}</option>
-                                  })}
-                              </select>
-                          </div>
-                      </div>
-
-                      <Flex
-                          minH={'220px'}
-                          height={['300px', '300px', '300px', 'auto']}
-                          maxW={'none'}
-                          flexGrow={'1'}
-                          mt={2}
-                          mb={4}
-                          p={0}
-                          flexDirection={'column'}
-                      >
-                        <LoadingBlock loading={transactionsHistory.loading}>
-                          {(!transactionsHistory.error && transactionsHistory.data?.resultSet?.length)
-                            ? <LineChart
-                                  data={transactionsHistory.data.resultSet.map((item) => ({
-                                    x: new Date(item.timestamp),
-                                    y: item.data.txs
-                                  }))}
-                                  timespan={transactionsTimespan}
-                                  xAxis={{
-                                    type: (() => {
-                                      if (transactionsTimespan === '1h') return { axis: 'time' }
-                                      if (transactionsTimespan === '24h') return { axis: 'time' }
-                                      if (transactionsTimespan === '3d') return { axis: 'date', tooltip: 'datetime' }
-                                      if (transactionsTimespan === '1w') return { axis: 'date' }
-                                    })(),
-                                    abbreviation: '',
-                                    title: ''
-                                  }}
-                                  yAxis={{
-                                    type: 'number',
-                                    title: '',
-                                    abbreviation: 'txs'
-                                  }}
-                              />
-                            : <ErrorMessageBlock/>}
-                        </LoadingBlock>
-                      </Flex>
-                  </Flex>
+                  <Container mb={5} p={0} maxW={['100%', '100%', '100%', 'calc(50% - 20px)']}>
+                    <TransactionsHistory/>
+                  </Container>
 
                   <Box flexShrink={'0'} w={10} h={[0, 0, 0, 10]} />
 
