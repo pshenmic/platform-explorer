fbe961931b779f2de5c3a7230f2226947ce338c5
owl352 <kokosinca123@gmail.com>
2024-07-09 23:05:17 +0500

    BLOCK_TIME remove

--- packages/api/src/constants.js
+++ packages/api/src/constants.js
@@ -4,7 +4,6 @@ let genesisTime
 
 module.exports = {
   EPOCH_CHANGE_TIME: Number(process.env.EPOCH_CHANGE_TIME),
-  BLOCK_TIME: 5000,
   get genesisTime () {
     if (!genesisTime) {
       return TenderdashRPC.getGenesis().then((genesis) => {
