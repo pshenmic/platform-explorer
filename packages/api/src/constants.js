ae2448dd14eb19b39fdd10c1360e359142445c66
pshenmic <pshenmic@gmail.com>
2024-07-10 09:16:27 +0700

    Revert "Add average TPS per epoch endpoint"

--- packages/api/src/constants.js
+++ packages/api/src/constants.js
@@ -1,16 +1,13 @@
-const TenderdashRPC = require('./tenderdashRpc')
-
-let genesisTime
-
 module.exports = {
-  EPOCH_CHANGE_TIME: Number(process.env.EPOCH_CHANGE_TIME),
-  get genesisTime () {
-    if (!genesisTime) {
-      return TenderdashRPC.getGenesis().then((genesis) => {
-        genesisTime = genesis.genesis_time
-        return genesis.genesis_time
-      }).catch(() => { throw new Error('Could not load genesis time') })
-    }
-    return genesisTime
+  BLOCK_TIME: 5000,
+  StateTransitionEnum: {
+    DATA_CONTRACT_CREATE: 0,
+    DOCUMENTS_BATCH: 1,
+    IDENTITY_CREATE: 2,
+    IDENTITY_TOP_UP: 3,
+    DATA_CONTRACT_UPDATE: 4,
+    IDENTITY_UPDATE: 5,
+    IDENTITY_CREDIT_WITHDRAWAL: 6,
+    IDENTITY_CREDIT_TRANSFER: 7
   }
 }
--- packages/api/src/controllers/MainController.js
+++ packages/api/src/controllers/MainController.js
@@ -4,8 +4,6 @@ const TransactionsDAO = require('../dao/TransactionsDAO')
 const DocumentsDAO = require('../dao/DocumentsDAO')
 const IdentitiesDAO = require('../dao/IdentitiesDAO')
 const TenderdashRPC = require('../tenderdashRpc')
-const Epoch = require('../models/Epoch')
-const Constants = require('../constants')
 
 const API_VERSION = require('../../package.json').version
 const PLATFORM_VERSION = '1' + require('../../package.json').dependencies.dash.substring(1)
@@ -20,18 +18,27 @@ class MainController {
   }
 
   getStatus = async (request, response) => {
-    const [blocks, stats, tdStatus] = (await Promise.allSettled([
+    const [blocks, stats, genesis, tdStatus] = (await Promise.allSettled([
       this.blocksDAO.getBlocks(1, 1, 'desc'),
       this.blocksDAO.getStats(),
+      TenderdashRPC.getGenesis(),
       TenderdashRPC.getStatus()
     ])).map((e) => e.value ?? null)
 
     const [currentBlock] = blocks.resultSet
 
-    const epoch = Epoch.fromObject({
-      timestamp: currentBlock.header.timestamp,
-      genesisTime: await Constants.genesisTime
-    })
+    const currentBlocktime = currentBlock.header.timestamp.getTime()
+    const epochChangeTime = Number(process.env.EPOCH_CHANGE_TIME)
+    const genesisTime = new Date(genesis?.genesis_time).getTime()
+    const epochIndex = Math.floor((currentBlocktime - genesisTime) / epochChangeTime)
+    const startEpochTime = Math.floor(genesisTime + epochChangeTime * epochIndex)
+    const nextEpochTime = Math.floor(genesisTime + epochChangeTime * (epochIndex + 1))
+
+    const epoch = {
+      index: epochIndex,
+      startTime: new Date(startEpochTime),
+      endTime: new Date(nextEpochTime)
+    }
 
     response.send({
       epoch,
