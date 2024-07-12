6c6741de1be9b05181fa8ef1f040903bca11b4d4
owl352 <kokosinca123@gmail.com>
2024-07-13 02:36:13 +0500

    query optimization

--- packages/api/src/dao/EpochDAO.js
+++ packages/api/src/dao/EpochDAO.js
@@ -5,7 +5,7 @@ const Constants = require('../constants')
 const EpochData = require('../models/EpochData')
 
 module.exports = class EpochDAO {
-  constructor (knex) {
+  constructor(knex) {
     this.blocksDAO = new BlocksDAO(knex)
     this.transactionsDAO = new TransactionsDAO(knex)
     this.knex = knex
@@ -23,32 +23,25 @@ module.exports = class EpochDAO {
       .leftJoin('validators', 'blocks.validator', 'validators.pro_tx_hash')
       .where('blocks.timestamp', '>', epoch.startTime.toISOString())
       .andWhere('blocks.timestamp', '<=', epoch.endTime.toISOString())
-      .groupBy('validators.pro_tx_hash')
-      .count('* as tx_count')
+      .groupBy('validators.pro_tx_hash', 'gas_used')
       .sum('gas_used as collected_fees')
-      .select('validators.pro_tx_hash',
-        this.knex.raw('ROW_NUMBER() OVER (ORDER BY COUNT(blocks.hash) DESC) as row_num')
-      )
-
-    const subqueryWithTotalFees = this.knex
+      .count('* as tx_count')
       .select(
-        'pro_tx_hash',
-        'tx_count',
-        'collected_fees',
-        'row_num',
-        this.knex.raw('SUM(collected_fees) OVER () as total_collected_fees')
+        'validators.pro_tx_hash',
+        this.knex.raw('ROW_NUMBER() OVER (ORDER BY COUNT(blocks.hash) DESC) as row_num'),
       )
-      .from(subquery)
 
     const [row] = await this.knex
       .select(
-        this.knex.raw(`SUM(tx_count) OVER () * 1.0 / ${Constants.EPOCH_CHANGE_TIME / 1000} as tps`),
         'pro_tx_hash as best_validator',
-        'total_collected_fees as total_collected_fees'
+        'tx_count',
+        'collected_fees',
+        'row_num',
+        this.knex.raw('SUM(collected_fees) OVER () as total_collected_fees'),
+        this.knex.raw(`SUM(tx_count) OVER () * 1.0 / ${Constants.EPOCH_CHANGE_TIME / 1000} as tps`),
       )
-      .groupBy('pro_tx_hash', 'row_num', 'collected_fees', 'total_collected_fees', 'tx_count')
       .orderBy('row_num', 'asc')
-      .from(subqueryWithTotalFees)
+      .from(subquery)
 
     return EpochData.fromObject({ epoch, ...row })
   }
