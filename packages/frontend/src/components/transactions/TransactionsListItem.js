2b4753fe9a4096a9024db08a748fd508c198084b
alexeyandreevsky <andreewsky.alexey@gmail.com>
2024-07-14 01:12:36 +0300

    Add a tooltip to transaction status

--- packages/frontend/src/components/transactions/TransactionsListItem.js
+++ packages/frontend/src/components/transactions/TransactionsListItem.js
@@ -3,15 +3,16 @@
 import Link from 'next/link'
 import { getTransitionTypeString } from '../../util/index'
 import { CheckCircleIcon, WarningTwoIcon } from '@chakra-ui/icons'
+import { Tooltip, Box } from '@chakra-ui/react'
 import './TransactionsListItem.scss'
 
 function TransactionsListItem ({ transaction }) {
   const hash = typeof transaction === 'object' ? transaction.hash : transaction
   const { timestamp, type } = transaction
 
-  const statusIcon = transaction.status === 'SUCCESS'
-    ? <CheckCircleIcon color={'green.500'} mr={4}/>
-    : <WarningTwoIcon color={'yellow.400'} mr={4}/>
+  const StatusIcon = transaction.status === 'SUCCESS'
+    ? <CheckCircleIcon color={'green.500'}/>
+    : <WarningTwoIcon color={'yellow.400'}/>
 
   return (
     <Link
@@ -20,7 +21,19 @@ function TransactionsListItem ({ transaction }) {
     >
         {typeof timestamp === 'string' &&
             <div className={'TransactionsListItem__Timestamp'}>
-                {statusIcon}
+                  <Tooltip
+                      label={transaction.status === 'SUCCESS'
+                        ? 'Transaction processed successfully'
+                        : 'Transaction processed with error'
+                      }
+                      aria-label={'A tooltip'}
+                      placement={'top'}
+                      hasArrow
+                      bg={'gray.700'}
+                      color={'white'}
+                  >
+                    <Box mr={4} display={'inline-block'}>{StatusIcon}</Box>
+                  </Tooltip>
                 {new Date(timestamp).toLocaleString()}
             </div>
         }
