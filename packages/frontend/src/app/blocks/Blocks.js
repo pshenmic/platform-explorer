fe101e90979d559711a2c59c43759eff569a49f3
alexeyandreevsky <andreewsky.alexey@gmail.com>
2024-07-31 02:38:59 +0300

    Fix pagination urls on the Blocks page

--- packages/frontend/src/app/blocks/Blocks.js
+++ packages/frontend/src/app/blocks/Blocks.js
@@ -53,6 +53,12 @@ function Blocks ({ defaultPage = 1, defaultPageSize }) {
   useEffect(() => fetchData(currentPage + 1, pageSize), [pageSize, currentPage])
 
   useEffect(() => {
+    const page = parseInt(searchParams.get('page')) || paginateConfig.defaultPage
+    setCurrentPage(Math.max(page - 1, 0))
+    setPageSize(parseInt(searchParams.get('page-size')) || paginateConfig.pageSize.default)
+  }, [searchParams, pathname])
+
+  useEffect(() => {
     const urlParameters = new URLSearchParams(Array.from(searchParams.entries()))
 
     if (currentPage + 1 === paginateConfig.defaultPage && pageSize === paginateConfig.pageSize.default) {
@@ -118,7 +124,7 @@ function Blocks ({ defaultPage = 1, defaultPageSize }) {
                     />
                     <PageSizeSelector
                         PageSizeSelectHandler={(e) => setPageSize(Number(e.target.value))}
-                        defaultValue={pageSize}
+                        value={pageSize}
                         items={paginateConfig.pageSize.values}
                     />
                 </div>
