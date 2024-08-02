7602835a752eb3d9aa29e95bfe00f5658821d274
owl352 <kokosinca123@gmail.com>
2024-08-02 22:17:35 +0500

    default value fix

--- packages/api/src/dashcoreRpc.js
+++ packages/api/src/dashcoreRpc.js
@@ -26,7 +26,7 @@ class DashCoreRPC {
     }
   }
 
-  static async getProTxInfo (proTxHash, blockHash = NaN) {
+  static async getProTxInfo (proTxHash, blockHash = undefined) {
     try {
       const args = ['info', proTxHash]
       if (blockHash) args.push(blockHash)
