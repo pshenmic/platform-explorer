4d92aee0e6e6bb930b98de88474d6bbe4dde9e8b
owl352 <kokosinca123@gmail.com>
2024-08-11 20:50:05 +0500

    lint

--- packages/api/src/models/EpochData.js
+++ packages/api/src/models/EpochData.js
@@ -4,7 +4,7 @@ module.exports = class EpochData {
   totalCollectedFees
   bestValidator
 
-  constructor(epoch, tps, totalCollectedFees, bestValidator) {
+  constructor (epoch, tps, totalCollectedFees, bestValidator) {
     this.epoch = epoch ?? null
     this.tps = tps ?? null
     this.totalCollectedFees = totalCollectedFees ?? null
@@ -12,7 +12,7 @@ module.exports = class EpochData {
   }
 
   // eslint-disable-next-line camelcase
-  static fromObject({ epoch, tps, total_collected_fees, best_validator }) {
+  static fromObject ({ epoch, tps, total_collected_fees, best_validator }) {
     // eslint-disable-next-line camelcase
     return new EpochData(epoch, Number(tps ?? 0), Number(total_collected_fees ?? 0), best_validator)
   }
