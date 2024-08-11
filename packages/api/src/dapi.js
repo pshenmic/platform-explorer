d198cd4b4a4d93c572ba1b12f4f3c67cb2ee172a
owl352 <kokosinca123@gmail.com>
2024-08-12 00:00:55 +0500

    default values fix

--- packages/api/src/dapi.js
+++ packages/api/src/dapi.js
@@ -3,9 +3,10 @@ const DAPIClient = require('@dashevo/dapi-client')
 const client = new DAPIClient(
   {
     dapiAddresses: [
-      process.env.DAPI_URL
+      process.env.DAPI_URL,
+      '127.0.0.1:9901'
     ],
-    network: process.env.DAPI_NETWORK
+    network: process.env.DAPI_NETWORK ?? 'local'
   }
 )
 
