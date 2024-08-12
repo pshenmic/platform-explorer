9ac933f0dd0d36e933e7368e434e5b23f5994538
Alexey <andreewsky.alexey@gmail.com>
2024-08-12 19:00:20 +0300

    Update TotalInfo styles

--- packages/frontend/src/components/total/TotalInfo.js
+++ packages/frontend/src/components/total/TotalInfo.js
@@ -1,71 +1,48 @@
 import './TotalInfo.scss'
 import './TotalInfoItem.scss'
-
-import {
-  Box,
-  Container,
-  Flex
-} from '@chakra-ui/react'
+import { Container, Flex } from '@chakra-ui/react'
 
 export default function TotalInfo ({ blocks, transactions, dataContracts, documents, transfers, loading }) {
   return (
-        <Container
-            className={'TotalInfo'}
-            maxW={'none'}
-            borderColor={'gray.800'}
-            py={0}
-            mt={8}
-            mb={8}
-        >
-            <Flex
-                className={'TotalInfo__ContentContainer'}
-                justify={'space-around'}
-                maxW={'container.xl'}
-                wrap={'wrap'}
-            >
-                <div className='TotalInfo__Item'>
-                    <div className={'TotalInfoItem TotalInfoItem--Blocks'}>
-                        <div className={'TotalInfoItem__Title'}>Blocks</div>
-                        <div className={`TotalInfoItem__Value ${loading ? 'TotalInfoItem__Value--Loading' : ''}`}>{`${blocks || '-'}`}</div>
-                    </div>
-                </div>
+    <Container
+      className={'TotalInfo'}
+      maxW={'none'}
+      borderColor={'gray.800'}
+      py={0}
+      mt={8}
+      mb={8}
+    >
+      <Flex
+        className={'TotalInfo__ContentContainer'}
+        justify={'space-between'}
+        maxW={'container.xl'}
+        wrap={'wrap'}
+      >
+        <div className={'InfoBlock TotalInfo__Item TotalInfoItem TotalInfoItem--Blocks'}>
+          <div className={'TotalInfoItem__Title'}>Blocks</div>
+          <div className={`TotalInfoItem__Value ${loading ? 'TotalInfoItem__Value--Loading' : ''}`}>{`${blocks || '-'}`}</div>
+        </div>
 
-                <Box className={'TotalInfo__Divider'}></Box>
+        <div className={'InfoBlock TotalInfo__Item TotalInfoItem TotalInfoItem--Transactions'}>
+          <div className={'TotalInfoItem__Title'}>Transactions</div>
+          <div className={`TotalInfoItem__Value ${loading ? 'TotalInfoItem__Value--Loading' : ''}`}>{`${transactions || '-'}`}</div>
+        </div>
 
-                <div className='TotalInfo__Item'>
-                    <div className={'TotalInfoItem TotalInfoItem--Transactions'}>
-                        <div className={'TotalInfoItem__Title'}>Transactions</div>
-                        <div className={`TotalInfoItem__Value ${loading ? 'TotalInfoItem__Value--Loading' : ''}`}>{`${transactions || '-'}`}</div>
-                    </div>
-                </div>
+        <div className={'InfoBlock TotalInfo__Item TotalInfoItem TotalInfoItem--DataContracts'}>
+          <div className={'TotalInfoItem__Title'}>Data Contracts</div>
+          <div className={`TotalInfoItem__Value ${loading ? 'TotalInfoItem__Value--Loading' : ''}`}>{`${dataContracts || '-'}`}</div>
+        </div>
 
-                <Box className={'TotalInfo__Divider'} display={['none', 'none', 'block']}></Box>
+        <div className={'InfoBlock TotalInfo__Item TotalInfoItem TotalInfoItem--Documents'}>
+          <div className={'TotalInfoItem__Title'}>Documents</div>
+          <div className={`TotalInfoItem__Value ${loading ? 'TotalInfoItem__Value--Loading' : ''}`}>{`${documents || '-'}`}</div>
+        </div>
 
-                <div className='TotalInfo__Item'>
-                    <div className={'TotalInfoItem TotalInfoItem--DataContracts'}>
-                        <div className={'TotalInfoItem__Title'}>Data Contracts</div>
-                        <div className={`TotalInfoItem__Value ${loading ? 'TotalInfoItem__Value--Loading' : ''}`}>{`${dataContracts || '-'}`}</div>
-                    </div>
-                </div>
-
-                <Box className={'TotalInfo__Divider'} display={['block', 'block', 'block']}></Box>
-
-                <div className='TotalInfo__Item'>
-                    <div className={'TotalInfoItem TotalInfoItem--Documents'}>
-                        <div className={'TotalInfoItem__Title'}>Documents</div>
-                        <div className={`TotalInfoItem__Value ${loading ? 'TotalInfoItem__Value--Loading' : ''}`}>{`${documents || '-'}`}</div>
-                    </div>
-                </div>
-
-                <Box className={'TotalInfo__Divider'} display={['none', 'none', 'block']}></Box>
-
-                <div className='TotalInfo__Item'>
-                    <div className={'TotalInfoItem TotalInfoItem--Transfers'}>
-                        <div className={'TotalInfoItem__Title'}>Transfers</div>
-                        <div className={`TotalInfoItem__Value ${loading ? 'TotalInfoItem__Value--Loading' : ''}`}>{`${transfers || '-'}`}</div>
-                    </div>
-                </div>
-            </Flex>
-        </Container>
+        <div className={'InfoBlock TotalInfo__Item TotalInfoItem TotalInfoItem--Identities'}>
+          <div className={'TotalInfoItem__Title'}>Identities</div>
+          <div className={`TotalInfoItem__Value ${loading ? 'TotalInfoItem__Value--Loading' : ''}`}>{`${transfers || '-'}`}</div>
+        </div>
+      </Flex>
+    </Container>
   )
 }
