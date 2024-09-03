9e489b5b331e7bf45920dc8cce5b85ca8c068d98
owl352 <kokosinca123@gmail.com>
2024-09-03 17:20:30 +0300

    fix

--- packages/api/src/controllers/EpochController.js
+++ packages/api/src/controllers/EpochController.js
@@ -1,5 +1,6 @@
 const EpochDAO = require('../dao/EpochDAO')
 const BlocksDAO = require('../dao/BlocksDAO')
+const Constants = require("../constants");
 
 class EpochController {
   epochDAO
@@ -14,20 +15,19 @@ class EpochController {
 
   getEpochByIndex = async (request, response) => {
     const { index } = request.params
-    //
-    // const epochChangeTime = Constants.EPOCH_CHANGE_TIME
-    // const genesisTime = await Constants.genesisTime
-    //
-    // const [currentBlock] = (await this.blocksDAO.getBlocks(1, 1, 'desc')).resultSet
-    //
-    // if (genesisTime.getTime() + index * epochChangeTime > currentBlock.header.timestamp.getTime()) {
-    //   return response.status(400).send({ message: 'Invalid epoch date' })
-    // }
-    //
-    // const epochInfo = await this.epochDAO.getEpochByIndex(index, currentBlock)
 
-    const bb = await this.dapi.getEpochsInfo()
-    response.send(bb)
+    const epochChangeTime = Constants.EPOCH_CHANGE_TIME
+    const genesisTime = await Constants.genesisTime
+
+    const [currentBlock] = (await this.blocksDAO.getBlocks(1, 1, 'desc')).resultSet
+
+    if (genesisTime.getTime() + index * epochChangeTime > currentBlock.header.timestamp.getTime()) {
+      return response.status(400).send({ message: 'Invalid epoch date' })
+    }
+
+    const epochInfo = await this.epochDAO.getEpochByIndex(index, currentBlock)
+
+    response.send(epochInfo)
   }
 }
 
