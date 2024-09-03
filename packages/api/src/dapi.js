cf180755d9118b375bf0e667e0b85275b7c5374e
owl352 <kokosinca123@gmail.com>
2024-09-03 17:19:28 +0300

    fix

--- packages/api/src/dapi.js
+++ packages/api/src/dapi.js
@@ -1,18 +1,32 @@
 const { Identifier } = require('@dashevo/wasm-dpp')
-const { getDapi } = require('./utils')
+const DAPIClient = require('@dashevo/dapi-client')
 
 class DAPI {
   dapi
   dpp
 
   constructor (dpp) {
-    this.dapi = getDapi()
+    this.dapi = this.getDapi()
     this.dpp = dpp
   }
 
   async getIdentityBalance (identifier) {
     const GRPCIdentity = await this.dapi.platform.getIdentity(Identifier.from(identifier))
     return this.dpp.identity.createFromBuffer(GRPCIdentity.getIdentity()).balance
+  }
+
+  getDapi () {
+    return this.dapi ?? new DAPIClient({
+      dapiAddresses: [
+        {
+          host: process.env.DAPI_HOST,
+          port: process.env.DAPI_PORT,
+          retries: process.env.DAPI_RETRIES,
+          protocol: process.env.DAPI_PROTOCOL
+        }
+      ],
+      retries: -1
+    })
   }
 }
 
