cea313ed5a03bf642011502068e1fe660ce6da92
Alexey <andreewsky.alexey@gmail.com>
2024-09-12 16:47:10 +0300

    Update copy button styles and handler

--- packages/frontend/src/components/ui/Buttons/CopyButton.js
+++ packages/frontend/src/components/ui/Buttons/CopyButton.js
@@ -28,7 +28,15 @@ function CopyButton ({ text, className }) {
   }
 
   return (
-    <Button size={'sm'} onClick={() => copyToClipboard(text, showMessage)} className={`CopyButton ${className}`}>
+    <Button
+      bg={'transparent'}
+      size={'sm'}
+      onClick={event => {
+        event.stopPropagation()
+        event.preventDefault()
+        copyToClipboard(text, showMessage)
+      }}
+      className={`CopyButton ${className}`}>
       <Tooltip
         label={messageState.text}
         aria-label={'A tooltip'}
@@ -39,7 +47,7 @@ function CopyButton ({ text, className }) {
         isDisabled={!messageState.active}
         isOpen={messageState.active}
       >
-        <CopyIcon/>
+        <CopyIcon w={4} h={4} color={'gray.250'}/>
       </Tooltip>
     </Button>
   )
