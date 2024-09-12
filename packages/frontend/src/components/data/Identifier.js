99c9ea36630867f1bc272581b8869b98d33b26d5
Alexey <andreewsky.alexey@gmail.com>
2024-09-12 16:45:13 +0300

    Update Identitfier component styles

--- packages/frontend/src/components/data/Identifier.js
+++ packages/frontend/src/components/data/Identifier.js
@@ -2,9 +2,15 @@ import './Identifier.scss'
 import ImageGenerator from '../imageGenerator'
 import { CopyButton } from '../ui/Buttons'
 
-export default function Identifier ({ children, ellipsis = true, avatar, copyButton }) {
+export default function Identifier ({ children, ellipsis = true, avatar, styles = [], copyButton }) {
   return (
-    <div className={`Identifier ${ellipsis && 'Identifier--Ellipsis'}`}>
+    <div
+      className={`Identifier ${ellipsis && 'Identifier--Ellipsis'} ` + 
+        `${styles.includes('dim') && 'Identifier--Dim'} ` +
+        `${styles.includes('gradient-start') && 'Identifier--GradientStart'} ` +
+        `${styles.includes('gradient-both') && 'Identifier--GradientBoth'} `
+      }
+    >
       {avatar && (
         <ImageGenerator className={'Identifier__Avatar'} username={children} lightness={50} saturation={50} width={28} height={28} />
       )}
