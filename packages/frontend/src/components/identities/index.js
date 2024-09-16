dec5b2d1d206e8e210a6683cfe9cb843f718296e
Alexey <andreewsky.alexey@gmail.com>
2024-09-16 16:24:19 +0300

    Update Identities intro page

--- packages/frontend/src/components/identities/index.js
+++ packages/frontend/src/components/identities/index.js
@@ -1,10 +1,12 @@
 import RichList from './RichList'
 import IdentitiesList from './IdentitiesList'
 import { IdentitiesCards, IdentityCard } from './Cards'
+import TopIdentities from './TopIdentities'
 
 export {
   RichList,
   IdentitiesList,
   IdentitiesCards,
-  IdentityCard
+  IdentityCard,
+  TopIdentities
 }
