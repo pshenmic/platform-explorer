001a99e6c605cd044e219a17108e245088cf47fc
Alexey <andreewsky.alexey@gmail.com>
2024-09-16 22:50:46 +0300

    Update pagination styles

--- packages/frontend/src/components/pagination/index.js
+++ packages/frontend/src/components/pagination/index.js
@@ -7,22 +7,23 @@ function Pagination ({ onPageChange, pageCount, forcePage }) {
 
   return (
     <ReactPaginate
-      breakLabel="..."
-      nextLabel=">"
+      breakLabel={'...'}
+      nextLabel={'>'}
       onPageChange={onPageChange}
       pageRangeDisplayed={2}
       marginPagesDisplayed={1}
       pageCount={pageCount}
-      previousLabel="<"
-      pageClassName="page-item"
-      pageLinkClassName="page-link"
-      previousClassName="page-item page-item--previous"
-      previousLinkClassName="page-link"
-      nextClassName="page-item page-item--next"
-      nextLinkClassName="page-link"
-      breakClassName="page-item  page-item--break-link"
-      containerClassName="pagination"
-      activeClassName="active"
+      previousLabel={'<'}
+      pageClassName={'PageItem'}
+      pageLinkClassName={'PageLink'}
+      previousClassName={'PageItem PageItem--Previous'}
+      previousLinkClassName={'PageLink'}
+      nextClassName={'PageItem PageItem--Next'}
+      nextLinkClassName={'PageLink'}
+      breakClassName={'PageItem PageItem--BreakLink'}
+      breakLinkClassName={'PageLink PageLink--Break'}
+      containerClassName={'Pagination'}
+      activeClassName={'active'}
       renderOnZeroPageCount={true}
       forcePage={forcePage}
     />
