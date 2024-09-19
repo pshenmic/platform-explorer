76fd77ae103465ccef9544ee3b910bd5518517e5
Alexey <andreewsky.alexey@gmail.com>
2024-09-19 13:49:21 +0300

    Update scroll handler in Slider plugin

--- packages/frontend/src/components/ui/Slider/plugins/WheelControls.js
+++ packages/frontend/src/components/ui/Slider/plugins/WheelControls.js
@@ -46,11 +46,21 @@ const WheelControls = (slider) => {
     }, 50)
   }
 
-  slider.on('created', () => {
-    slider.container.addEventListener('wheel', eventWheel, {
-      passive: false
-    })
-  })
+  const initPlugin = () => {
+    if (!slider) return
+
+    const totalSlides = slider?.slides?.length
+    const visibleSlides = slider?.options?.slides?.perView || 1
+
+    if (totalSlides > visibleSlides) {
+      slider.container.addEventListener('wheel', eventWheel, { passive: false })
+    } else {
+      slider.container.removeEventListener('wheel', eventWheel)
+    }
+  }
+
+  slider.on('created', initPlugin)
+  slider.on('optionsChanged', initPlugin)
 }
 
 export default WheelControls
