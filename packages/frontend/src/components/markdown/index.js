1291aeca1ef24fe6f0d2fec85f00faeca2f39f81
Alexey <andreewsky.alexey@gmail.com>
2024-09-20 17:00:49 +0300

    Add rehype-raw plugin to markdown render and update markdown styles

--- packages/frontend/src/components/markdown/index.js
+++ packages/frontend/src/components/markdown/index.js
@@ -1,22 +1,24 @@
 import Markdown from 'react-markdown'
+import rehypeRaw from 'rehype-raw'
 import HeadingRenderer from './HeadingRenderer'
 import './Markdown.scss'
 
 export default function CustomMarkdown ({ children }) {
   return (
-      <Markdown
-          className={'Markdown'}
-          components={{
-            h1: HeadingRenderer,
-            h2: HeadingRenderer,
-            h3: HeadingRenderer,
-            h4: HeadingRenderer,
-            h5: HeadingRenderer,
-            h6: HeadingRenderer
-          }}
-      >
-        {children}
-      </Markdown>
+    <Markdown
+      className={'Markdown'}
+      rehypePlugins={[rehypeRaw]}
+      components={{
+        h1: HeadingRenderer,
+        h2: HeadingRenderer,
+        h3: HeadingRenderer,
+        h4: HeadingRenderer,
+        h5: HeadingRenderer,
+        h6: HeadingRenderer
+      }}
+    >
+      {children}
+    </Markdown>
   )
 }
 
