10df8207239a27a5e69afc55af4f6f9ebefaf19c
Alexey <andreewsky.alexey@gmail.com>
2024-10-01 21:39:37 +0300

    Update TotalInfo cards styles

--- packages/frontend/src/components/total/TotalInfo.js
+++ packages/frontend/src/components/total/TotalInfo.js
@@ -19,28 +19,28 @@ export default function TotalInfo ({ blocks, transactions, dataContracts, docume
         wrap={'wrap'}
       >
         <Link href={'/blocks/'} className={'InfoBlock TotalInfo__Item TotalInfoItem TotalInfoItem--Blocks'}>
-          <div className={'TotalInfoItem__Title'}>Blocks</div>
           <div className={`TotalInfoItem__Value ${loading ? 'TotalInfoItem__Value--Loading' : ''}`}>{`${blocks || '-'}`}</div>
+          <div className={'TotalInfoItem__Title'}>Blocks</div>
         </Link>
 
         <Link href={'/transactions/'} className={'InfoBlock TotalInfo__Item TotalInfoItem TotalInfoItem--Transactions'}>
-          <div className={'TotalInfoItem__Title'}>Transactions</div>
           <div className={`TotalInfoItem__Value ${loading ? 'TotalInfoItem__Value--Loading' : ''}`}>{`${transactions || '-'}`}</div>
+          <div className={'TotalInfoItem__Title'}>Transactions</div>
         </Link>
 
         <Link href={'/dataContracts/'} className={'InfoBlock TotalInfo__Item TotalInfoItem TotalInfoItem--DataContracts'}>
-          <div className={'TotalInfoItem__Title'}>Data Contracts</div>
           <div className={`TotalInfoItem__Value ${loading ? 'TotalInfoItem__Value--Loading' : ''}`}>{`${dataContracts || '-'}`}</div>
+          <div className={'TotalInfoItem__Title'}>Data Contracts</div>
         </Link>
 
         <div className={'InfoBlock TotalInfo__Item TotalInfoItem TotalInfoItem--Documents'}>
-          <div className={'TotalInfoItem__Title'}>Documents</div>
           <div className={`TotalInfoItem__Value ${loading ? 'TotalInfoItem__Value--Loading' : ''}`}>{`${documents || '-'}`}</div>
+          <div className={'TotalInfoItem__Title'}>Documents</div>
         </div>
 
         <Link href={'/identities/'} className={'InfoBlock TotalInfo__Item TotalInfoItem TotalInfoItem--Identities'}>
-          <div className={'TotalInfoItem__Title'}>Identities</div>
           <div className={`TotalInfoItem__Value ${loading ? 'TotalInfoItem__Value--Loading' : ''}`}>{`${identities || '-'}`}</div>
+          <div className={'TotalInfoItem__Title'}>Identities</div>
         </Link>
       </Flex>
     </Container>
