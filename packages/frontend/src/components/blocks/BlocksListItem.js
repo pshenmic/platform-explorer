a2f71b16be0cd66f0087c406a9c95af9e43faab6
Alexey <andreewsky.alexey@gmail.com>
2024-10-01 20:28:51 +0300

    Update blocks list styles

--- packages/frontend/src/components/blocks/BlocksListItem.js
+++ packages/frontend/src/components/blocks/BlocksListItem.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { Identifier } from '../data'
 import './BlocksListItem.scss'
 
 function BlocksListItem ({ block }) {
@@ -6,32 +7,26 @@ function BlocksListItem ({ block }) {
   const { hash, height, timestamp } = header
 
   return (
-    <Link
-        href={`/block/${hash}`}
-        className={'BlocksListItem'}
-    >
-        {(typeof height === 'number' &&
-            <span className={'BlocksListItem__Height'}>{height}</span>
-        )}
+    <Link href={`/block/${hash}`} className={'BlocksListItem'}>
+      {(typeof height === 'number' &&
+        <span className={'BlocksListItem__Height'}>{height}</span>
+      )}
 
-        {typeof timestamp === 'string' &&
-            <span className={'BlocksListItem__Timestamp'}>
-                {new Date(timestamp).toLocaleString()}
-            </span>
-        }
+      {typeof timestamp === 'string' &&
+        <span className={'BlocksListItem__Timestamp'}>
+          {new Date(timestamp).toLocaleString()}
+        </span>
+      }
 
-        {typeof hash === 'string' &&
-            <span className={'BlocksListItem__Hash'}>
-                {hash}
-            </span>
-        }
+      {typeof hash === 'string' &&
+        <Identifier className={'BlocksListItem__Hash'} styles={['highlight-both']}>
+          {hash}
+        </Identifier>
+      }
 
-        {(typeof txs.length === 'number') &&
-            <span className={'BlocksListItem__Txs'}>
-                ({txs.length} txs)
-            </span>
-        }
-
+      {(typeof txs.length === 'number') &&
+        <span className={'BlocksListItem__Txs'}>({txs.length} txs)</span>
+      }
     </Link>
   )
 }
