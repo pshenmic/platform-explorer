ea4913cb2dc3b9bd63ee0a4de58138834bb45675
Alexey <andreewsky.alexey@gmail.com>
2024-10-02 20:18:49 +0300

    Initializing the validator page structure

--- packages/frontend/src/app/validator/[hash]/Validator.js
+++ packages/frontend/src/app/validator/[hash]/Validator.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useCallback } from 'react'
 import * as Api from '../../../util/Api'
-import { fetchHandlerSuccess, fetchHandlerError } from '../../../util'
+import { fetchHandlerSuccess, fetchHandlerError, getTimeDelta } from '../../../util'
 import { LoadingLine, LoadingList } from '../../../components/loading'
 import Pagination from '../../../components/pagination'
 import PageSizeSelector from '../../../components/pageSizeSelector/PageSizeSelector'
@@ -11,6 +11,7 @@ import BlocksList from '../../../components/blocks/BlocksList'
 import ImageGenerator from '../../../components/imageGenerator'
 import BlocksChart from './BlocksChart'
 import Link from 'next/link'
+import { Identifier } from '../../../components/data'
 import {
   Container,
   TableContainer, Table, Thead, Tbody, Tr, Th, Td,
@@ -25,6 +26,79 @@ const paginateConfig = {
     values: [10, 25, 50, 75, 100]
   },
   defaultPage: 1
+}
+
+function InfoLine ({ title, value, className }) {
+  return (
+    <div className={`InfoLine ${className}`}>
+      <div className={'InfoLine__Title'}>{title}:</div>
+      <div className={'InfoLine__Value'}>{value}</div>
+    </div>
+  )
+}
+
+function Credits ({ credits, usd, format }) {
+  return (
+    <div>
+      <span>{credits} CREDITS</span>
+      <span>({credits / 1000} DASH)</span>
+      <span>~{usd}$</span>
+    </div>
+  )
+}
+
+function DateBlock ({ timestamp }) {
+  const date = new Date(timestamp)
+
+  if (String(date) === 'Invalid Date') return null
+
+  const options = {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric'
+  }
+
+  const formattedDate = date.toLocaleDateString('en-GB', options)
+
+  return (
+    <div>
+      <div>{formattedDate}</div>
+      <div>
+        {getTimeDelta(new Date(), date)}
+      </div>
+    </div>
+  )
+}
+
+function HorisontalSeparator () {
+  return <div className={'HorisontalSeparator'}></div>
+}
+
+function Endpoint ({ value, status, link }) {
+  const statusClass = (() => {
+    if (status === 'active') return 'Endpoint__Status--Active'
+    if (status === 'warining') return 'Endpoint__Status--Warning'
+    if (status === 'error') return 'Endpoint__Status--Error'
+    return ''
+  })()
+
+  return (
+    <div className={'Endpoint'}>
+      <div className={'Endpoint__Value'}>{value}</div>
+      {status !== undefined &&
+        <div className={`Endpoint__Status ${statusClass}`}></div>
+      }
+    </div>
+  )
+}
+
+function IpAddress ({ address, port, className }) {
+  return (
+    <div className={`IpAddress ${className}`}>
+      <span className={'IpAddress__Ip'}>{address}</span>
+      <span className={'IpAddress__Port'}>{port}</span>
+    </div>
+  )
 }
 
 function Validator ({ hash }) {
@@ -62,6 +136,156 @@ function Validator ({ hash }) {
     setCurrentPage(0)
     handlePageClick({ selected: 0 })
   }, [pageSize, handlePageClick])
+
+  return (
+    <Container
+      className={'PageInfoContainer'}
+      maxW={'container.xl'}
+      p={3}
+      mt={8}
+      bg={'gray'}
+    >
+      <div className={'PageInfoContainer__Header'}>
+        <div className={'PageInfoContainer__BackLink'}>{'<'}</div>
+        <div className={'PageInfoContainer__Title'}>Validator Info</div>
+      </div>
+
+      <div>
+        <div className={'PageInfoColumn'}>
+          <div>
+            <div className={'ValidatorCard'}>
+              <div className={'ValidatorCard__Header'}>
+                <div className={'ValidatorCard__Avatar'}></div>
+
+                <InfoLine
+                  title={'Pro TX Hash'}
+                  value={(
+                    <Identifier
+                      className={''}
+                      copyButton={true}
+                      styles={['gradient-both']}
+                    >
+                      {hash}
+                    </Identifier>
+                  )}
+                />
+
+                <InfoLine
+                  title={'Balance'}
+                  value={<Credits credits={85800000} usd={'209.15'} />}
+                />
+              </div>
+
+              <HorisontalSeparator/>
+
+              <InfoLine
+                title={'Creation Date'}
+                value={<DateBlock timestamp={1727887511000}/>}
+              />
+
+              <InfoLine
+                title={'Block Height'}
+                value={'#10225'}
+              />
+
+              <InfoLine
+                title={'Identity Address'}
+                value={(
+                  <Identifier
+                    className={''}
+                    copyButton={true}
+                    styles={['gradient-both']}
+                  >
+                    23975732199C674FD2133FA9F08454D809561DC24E6E941D78FF414C528ABA67
+                  </Identifier>
+                )}
+              />
+
+              <InfoLine
+                title={'Node ID'}
+                value={(
+                  <Identifier
+                    className={''}
+                    copyButton={true}
+                    styles={['gradient-both']}
+                  >
+                    50d847734406592420320c864eb572fb900e5c36
+                  </Identifier>
+                )}
+              />
+            </div>
+
+            {/* Endpoints */}
+
+            <InfoLine
+              title={'CORE P2P'}
+              value={(
+                <Identifier
+                  className={''}
+                  copyButton={true}
+                  styles={['gradient-both']}
+                >
+                  <Endpoint
+                    value={<IpAddress address={'192.168.0.1'} port={'9999'}/>}
+                    status={'active'}
+                  />
+                </Identifier>
+              )}
+            />
+
+            <InfoLine
+              title={'Platform P2P'}
+              value={(
+                <Identifier
+                  className={''}
+                  copyButton={true}
+                  styles={['gradient-both']}
+                >
+                  <Endpoint
+                    value={<IpAddress address={'192.168.0.1'} port={'9999'}/>}
+                    status={'active'}
+                  />
+                </Identifier>
+              )}
+            />
+
+            <InfoLine
+              title={'Platform GRPC'}
+              value={(
+                <Identifier
+                  className={''}
+                  copyButton={true}
+                  styles={['gradient-both']}
+                >
+                  <Endpoint
+                    value={<IpAddress address={'192.168.0.1'} port={'9999'}/>}
+                    status={'active'}
+                  />
+                </Identifier>
+              )}
+            />
+          </div>
+
+          <HorisontalSeparator/>
+
+          <div>
+            {/* Status*/}
+          </div>
+
+          <HorisontalSeparator/>
+
+          <div>
+            {/* PoSe Score and etc. */}
+          </div>
+        </div>
+        <div className={'PageInfoColumn'}> {/* Npobg */}
+          {/* Chart */}
+          {/* Proposed Blocks */}
+        </div>
+      </div>
+
+    </Container>
+  )
 
   return (
     <Container
