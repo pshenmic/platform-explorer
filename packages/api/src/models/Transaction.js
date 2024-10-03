97f7f50eccb2cd03b4bfbd6e0639380086ae5a28
owl352 <kokosinca123@gmail.com>
2024-10-04 02:52:09 +0300

    Revert "initial commit"
    This reverts commit e22a12e2c7ff274cfd1b6ea8c86cfe4f06de5fe1.

--- packages/api/src/models/Transaction.js
+++ packages/api/src/models/Transaction.js
@@ -1,7 +1,3 @@
-const cbor = require('cbor')
-
-const { deserializeConsensusError } = require('@dashevo/wasm-dpp')
-
 module.exports = class Transaction {
   hash
   index
@@ -29,17 +25,6 @@ module.exports = class Transaction {
 
   // eslint-disable-next-line camelcase
   static fromRow ({ tx_hash, index, block_hash, block_height, type, data, timestamp, gas_used, status, error }) {
-    let decodedError = null
-
-    try {
-      if (typeof error === 'string') {
-        const { serializedError } = cbor.decode(Buffer.from(error, 'base64'))?.data
-        decodedError = deserializeConsensusError(serializedError).message
-      }
-    } catch (e) {
-      console.error(e)
-    }
-
-    return new Transaction(tx_hash, index, block_hash, block_height, type, data, timestamp, parseInt(gas_used), status, decodedError ?? error)
+    return new Transaction(tx_hash, index, block_hash, block_height, type, data, timestamp, parseInt(gas_used), status, error)
   }
 }
