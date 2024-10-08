1005c4e69faf7ecb6ea156ae4e535f240fc03bc5
Alexey <andreewsky.alexey@gmail.com>
2024-10-08 18:03:23 +0300

    Remove unused import

--- packages/frontend/src/components/ui/containers/index.js
+++ packages/frontend/src/components/ui/containers/index.js
@@ -1,11 +1,9 @@
 import ValueContainer from './ValueContainer'
 import PageDataContainer from './PageDataContainer'
-import TabsBlock from './TabsBlock'
 import InfoContainer from './InfoContainer'
 
 export {
   ValueContainer,
   PageDataContainer,
-  TabsBlock,
   InfoContainer
 }
