0bd01c6f1445521948c011beac8be25d9f3da41c
Alexey <andreewsky.alexey@gmail.com>
2024-10-09 21:12:02 +0300

    Update Value Container

--- packages/frontend/src/components/ui/containers/ValueContainer.js
+++ packages/frontend/src/components/ui/containers/ValueContainer.js
@@ -1,8 +1,8 @@
 import './ValueContainer.scss'
 
-function ValueContainer ({ children, className }) {
+function ValueContainer ({ children, clickable, className, ...props }) {
   return (
-    <div className={`ValueContainer ${className || ''}`}>
+    <div className={`ValueContainer ${clickable ? 'ValueContainer--Clickable' : ''} ${className || ''}`} {...props}>
       {children}
     </div>
   )
