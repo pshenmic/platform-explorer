043e102b2f96d7061abbe19f803fddccd60a7600
Alexey <andreewsky.alexey@gmail.com>
2024-10-14 17:44:40 +0300

    Implement list fetches on the validator page

--- packages/frontend/src/app/validator/[hash]/Validator.js
+++ packages/frontend/src/app/validator/[hash]/Validator.js
@@ -2,13 +2,13 @@
 
 import { useState, useEffect, useCallback } from 'react'
 import * as Api from '../../../util/Api'
-import { fetchHandlerSuccess, fetchHandlerError } from '../../../util'
+import { fetchHandlerSuccess, fetchHandlerError, getTimeDelta } from '../../../util'
 import { LoadingList } from '../../../components/loading'
 import Pagination from '../../../components/pagination'
-// import PageSizeSelector from '../../../components/pageSizeSelector/PageSizeSelector'
 import { ErrorMessageBlock } from '../../../components/Errors'
 import BlocksList from '../../../components/blocks/BlocksList'
-// import ImageGenerator from '../../../components/imageGenerator'
+import TransactionsList from '../../../components/transactions/TransactionsList'
+import TransfersList from '../../../components/transfers/TransfersList'
 import BlocksChart from './BlocksChart'
 import Link from 'next/link'
 import { Identifier, DateBlock, Endpoint, IpAddress, InfoLine } from '../../../components/data'
@@ -16,7 +16,6 @@ import { ValueContainer, PageDataContainer, InfoContainer } from '../../../compo
 import { HorisontalSeparator } from '../../../components/ui/separators'
 import { ValidatorCard } from '../../../components/validators'
 import { CircleIcon } from '../../../components/ui/icons'
-import { getTimeDelta } from '../../../util'
 import './ValidatorPage.scss'
 import {
   Container,
@@ -24,51 +23,69 @@ import {
   Tabs, TabList, TabPanels, Tab, TabPanel
 } from '@chakra-ui/react'
 
-// const paginateConfig = {
-//   pageSize: {
-//     default: 10,
-//     values: [10, 25, 50, 75, 100]
-//   },
-//   defaultPage: 1
-// }
-
 function Validator ({ hash }) {
   const [validator, setValidator] = useState({ data: {}, loading: true, error: false })
   const [status, setStatus] = useState({ data: {}, loading: true, error: false })
-  const [proposedBlocks, setProposedBlocks] = useState({ data: {}, loading: true, error: false })
-  const [totalBlocks, setTotalBlocks] = useState(1)
-  // const tdTitleWidth = 250
-  // const [pageSize, setPageSize] = useState(paginateConfig.pageSize.default)
+  const [proposedBlocks, setProposedBlocks] = useState({ data: {}, props: { currentPage: 0 }, loading: true, error: false })
   const pageSize = 13
   const [currentPage, setCurrentPage] = useState(1)
-  const pageCount = Math.ceil(totalBlocks / pageSize) ? Math.ceil(totalBlocks / pageSize) : 1
+  const [transactions, setTransactions] = useState({ data: {}, props: { currentPage: 0 }, loading: true, error: false })
+  const [transfers, setTransfers] = useState({ data: {}, props: { currentPage: 0 }, loading: true, error: false })
   const [activeChartTab, setActiveChartTab] = useState(0)
 
-  const fetchData = (page, count) => {
-    setProposedBlocks(state => ({ ...state, loading: true }))
-
+  const fetchData = () => {
     Api.getValidatorByProTxHash(hash)
       .then(res => fetchHandlerSuccess(setValidator, res))
       .catch(err => fetchHandlerError(setValidator, err))
 
-    Api.getBlocksByValidator(hash, page, count, 'desc')
-      .then((res) => {
-        fetchHandlerSuccess(setProposedBlocks, res)
-        setTotalBlocks(res?.pagination?.total)
-      })
-      .catch(err => fetchHandlerError(setProposedBlocks, err))
-
     Api.getStatus()
       .then(res => fetchHandlerSuccess(setStatus, res))
       .catch(err => fetchHandlerError(setStatus, err))
-
-    
   }
 
-  console.log('status', status)
+  useEffect(() => fetchData(), [hash])
 
-  // useEffect(() => fetchData(paginateConfig.defaultPage, paginateConfig.pageSize.default), [hash])
-  useEffect(() => fetchData(1, pageSize), [hash])
+  useEffect(() => {
+    setProposedBlocks(state => ({ ...state, loading: true }))
+
+    Api.getBlocksByValidator(hash, proposedBlocks.props.currentPage + 1, pageSize, 'desc')
+      .then((res) => fetchHandlerSuccess(setProposedBlocks, res))
+      .catch(err => fetchHandlerError(setProposedBlocks, err))
+  }, [proposedBlocks.props.currentPage])
+
+  useEffect(() => {
+    if (!validator.data?.identity) return
+    setTransactions(state => ({ ...state, loading: true }))
+
+    const identifier = validator.data.identity
+    // const identifier = 'HVfqSPfdmiHsrajx7EmErGnV597uYdH3JGhvwpVDcdAT' // test
+
+    Api.getTransactionsByIdentity(identifier, transactions.props.currentPage + 1, pageSize, 'desc')
+      .then(paginatedTransactions => fetchHandlerSuccess(setTransactions, paginatedTransactions))
+      .catch(err => fetchHandlerError(setTransactions, err))
+  }, [validator, transactions.props.currentPage])
+
+  useEffect(() => {
+    if (!validator.data?.identity) return
+    setTransfers(state => ({ ...state, loading: true }))
+
+    const identifier = validator.data.identity
+    // const identifier = 'HVfqSPfdmiHsrajx7EmErGnV597uYdH3JGhvwpVDcdAT' // test
+
+    Api.getTransfersByIdentity(identifier, transfers.props.currentPage + 1, pageSize, 'desc')
+      .then(paginatedTransactions => fetchHandlerSuccess(setTransfers, paginatedTransactions))
+      .catch(err => fetchHandlerError(setTransfers, err))
+  }, [validator, transfers.props.currentPage])
+
+  function paginationHandler (setter, currentPage) {
+    setter(state => ({
+      ...state,
+      props: {
+        ...state.props,
+        currentPage
+      }
+    }))
+  }
 
   const handlePageClick = useCallback(({ selected }) => {
     setCurrentPage(selected)
@@ -79,8 +96,6 @@ function Validator ({ hash }) {
     setCurrentPage(0)
     handlePageClick({ selected: 0 })
   }, [pageSize, handlePageClick])
-
-  // console.log('validator', validator.data)
 
   return (
     <PageDataContainer
@@ -341,142 +356,49 @@ function Validator ({ hash }) {
                   }
 
                   {proposedBlocks.data?.resultSet?.length > 0 &&
-                    <Pagination
-                      onPageChange={handlePageClick}
-                      pageCount={pageCount}
-                      forcePage={currentPage}
-                    />
+                    <div className={'ValidatorPage__ListPagination'}>
+                      <Pagination
+                        onPageChange={pagination => paginationHandler(setProposedBlocks, pagination.selected)}
+                        pageCount={Math.ceil(proposedBlocks.data?.pagination?.total / pageSize) || 1}
+                        forcePage={currentPage}
+                      />
+                    </div>
                   }
                 </TabPanel>
                 <TabPanel className={'ValidatorPage__ListContainer'}>
-                  Transactions
+                  {!transactions.error
+                    ? <div className={'ValidatorPage__List'}>
+                        {!transactions.loading
+                          ? <TransactionsList transactions={transactions.data.resultSet} size={'m'}/>
+                          : <LoadingList itemsCount={pageSize}/>}
+                      </div>
+                    : <Container h={20}><ErrorMessageBlock/></Container>}
+
+                  {transactions.data?.resultSet?.length > 0 &&
+                    <div className={'ValidatorPage__ListPagination'}>
+                      <Pagination
+                        className={'ValidatorPage__ListPagination'}
+                        onPageChange={pagination => paginationHandler(setTransactions, pagination.selected)}
+                        pageCount={Math.ceil(transactions.data?.pagination?.total / pageSize) || 1}
+                        forcePage={currentPage}
+                      />
+                    </div>
+                  }
                 </TabPanel>
                 <TabPanel className={'ValidatorPage__ListContainer'}>
-                  Withdrawals
+                  {!transfers.error
+                    ? !transfers.loading
+                        ? <TransfersList transfers={transfers.data.resultSet} identityId={validator.data?.identity}/>
+                        : <LoadingList itemsCount={pageSize}/>
+                    : <Container h={20}><ErrorMessageBlock/></Container>}
                 </TabPanel>
               </TabPanels>
             </Tabs>
           </InfoContainer>
-
         </div>
       </div>
     </PageDataContainer>
   )
-
-  // return (
-  //   <Container
-  //     maxW={'container.lg'}
-  //     p={3}
-  //     mt={8}
-  //   >
-  //     <Grid
-  //       w={'100%'}
-  //       mb={5}
-  //       gap={5}
-  //       templateColumns={['1fr', '1fr', '1fr', '1fr 1fr']}
-  //     >
-  //       <GridItem p={0} height={'100%'}>
-  //         <TableContainer
-  //           width={'100%'}
-  //           height={'100%'}
-  //           maxW={'none'}
-  //           borderWidth={'1px'} borderRadius={'block'}
-  //           m={0}
-  //           display={'block'}
-  //         >
-  //           {!validator.error
-  //             ? <Table variant={'simple'} className={'Table'}>
-  //                 <Thead>
-  //                   <Tr>
-  //                     <Th pr={0}>validator info</Th>
-  //                     <Th className={'TableHeader TableHeader--Name'}>
-  //                       {hash
-  //                         ? <div className={'TableHeader__Content'}>
-  //                             <ImageGenerator className={'TableHeader__Avatar'} username={hash} lightness={50} saturation={50} width={32} height={32}/>
-  //                           </div>
-  //                         : <Box w={'32px'} h={'32px'} />
-  //                       }
-  //                     </Th>
-  //                   </Tr>
-  //                 </Thead>
-  //                 <Tbody>
-  //                 <Tr>
-  //                     <Td w={tdTitleWidth}>ProTxHash</Td>
-  //                     <Td isNumeric className={'Table__Cell--BreakWord Table__Cell--Mono'}>
-  //                       <LoadingLine loading={validator.loading}>
-  //                         <div>{hash}</div>
-  //                       </LoadingLine>
-  //                     </Td>
-  //                   </Tr>
-  //                   <Tr>
-  //                     <Td w={tdTitleWidth}>Identity</Td>
-  //                     <Td isNumeric className={'Table__Cell--BreakWord Table__Cell--Mono'}>
-  //                       <LoadingLine loading={validator.loading}>
-  //                         {validator?.data?.identity
-  //                           ? <Link href={`/identity/${validator.data.identity}`}>{validator.data.identity}</Link>
-  //                           : '-'}
-  //                       </LoadingLine>
-  //                     </Td>
-  //                   </Tr>
-  //                   <Tr>
-  //                     <Td w={tdTitleWidth}>Status</Td>
-  //                     <Td isNumeric>
-  //                       <LoadingLine loading={validator.loading}>{validator?.data?.isActive ? 'Active' : 'Inactive'}</LoadingLine>
-  //                     </Td>
-  //                   </Tr>
-  //                   <Tr>
-  //                     <Td w={tdTitleWidth}>Proposed blocks</Td>
-  //                     <Td isNumeric>
-  //                       <LoadingLine loading={validator.loading}>{validator?.data?.proposedBlocksAmount || '-'}</LoadingLine>
-  //                     </Td>
-  //                   </Tr>
-  //                 </Tbody>
-  //               </Table>
-  //             : <Container h={60}><ErrorMessageBlock/></Container>}
-  //         </TableContainer>
-  //       </GridItem>
-
-  //       <GridItem height={'100%'} p={0}>
-  //         <BlocksChart hash={hash}/>
-  //       </GridItem>
-  //     </Grid>
-
-  //     <Container
-  //       width={'100%'}
-  //       maxW={'none'}
-  //       mt={5}
-  //       className={'InfoBlock'}
-  //     >
-  //       <Heading className={'InfoBlock__Title'} as={'h1'}>Proposed blocks</Heading>
-
-  //       {!proposedBlocks.error
-  //         ? <>
-  //             {!proposedBlocks.loading
-  //               ? <BlocksList blocks={proposedBlocks?.data?.resultSet}/>
-  //               : <LoadingList itemsCount={pageSize}/>
-  //             }
-  //           </>
-  //         : <Container h={20}><ErrorMessageBlock/></Container>
-  //       }
-
-  //       {proposedBlocks.data?.resultSet?.length > 0 &&
-  //         <div className={'ListNavigation'}>
-  //             <Box width={'155px'}/>
-  //             <Pagination
-  //               onPageChange={handlePageClick}
-  //               pageCount={pageCount}
-  //               forcePage={currentPage}
-  //             />
-  //             <PageSizeSelector
-  //               PageSizeSelectHandler={(e) => setPageSize(Number(e.target.value))}
-  //               defaultValue={paginateConfig.pageSize.default}
-  //               items={paginateConfig.pageSize.values}
-  //             />
-  //         </div>
-  //       }
-  //     </Container>
-  //   </Container>
-  // )
 }
 
 export default Validator
