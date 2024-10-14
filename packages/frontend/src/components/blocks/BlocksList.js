9a547fb56e6ac42c61917d379a2890cbcb45e5a0
Alexey <andreewsky.alexey@gmail.com>
2024-10-14 18:04:49 +0300

    Add titles to blocks list

--- packages/frontend/src/components/blocks/BlocksList.js
+++ packages/frontend/src/components/blocks/BlocksList.js
@@ -1,5 +1,6 @@
 import BlocksListItem from './BlocksListItem'
 import { EmptyListMessage } from '../ui/lists'
+import { Grid, GridItem } from '@chakra-ui/react'
 import './BlocksList.scss'
 
 function BlocksList ({ blocks = [], columnsCount = 1, size = 'l' }) {
@@ -10,17 +11,32 @@ function BlocksList ({ blocks = [], columnsCount = 1, size = 'l' }) {
         columnCount: blocks.length > 1 ? columnsCount : 1
       }}
     >
-        {blocks.map((block, i) =>
-            <BlocksListItem
-                key={i}
-                block={block}
-                size={size}
-            />
-        )}
+      <Grid className={'BlocksList__ColumnTitles'}>
+        <GridItem className={'BlocksList__ColumnTitle'}>
+          Height
+        </GridItem>
+        <GridItem className={'BlocksList__ColumnTitle'}>
+          Time
+        </GridItem>
+        <GridItem className={'BlocksList__ColumnTitle BlocksList__ColumnTitle--Hash'}>
+          Block HASH
+        </GridItem>
+        <GridItem className={'BlocksList__ColumnTitle'}>
+          Txs count
+        </GridItem>
+      </Grid>
 
-        {blocks.length === 0 &&
-            <EmptyListMessage>There are no blocks created yet.</EmptyListMessage>
-        }
+      {blocks.map((block, i) =>
+        <BlocksListItem
+          key={i}
+          block={block}
+          size={size}
+        />
+      )}
+
+      {blocks.length === 0 &&
+        <EmptyListMessage>There are no blocks created yet.</EmptyListMessage>
+      }
     </div>
   )
 }
