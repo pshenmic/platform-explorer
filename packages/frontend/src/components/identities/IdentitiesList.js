14f479b29350009f89713f7c6c91db3333839ad4
Alexey <andreewsky.alexey@gmail.com>
2024-10-14 14:36:37 +0300

    Add mock identities  alias to /identities page

--- packages/frontend/src/components/identities/IdentitiesList.js
+++ packages/frontend/src/components/identities/IdentitiesList.js
@@ -4,18 +4,18 @@ import './IdentitiesList.scss'
 
 function IdentitiesList ({ identities }) {
   return (
-        <div className={'IdentitiesList'}>
-            {identities.map((identity, key) =>
-                <IdentitiesListItem
-                    key={key}
-                    identity={identity}
-                />
-            )}
+    <div className={'IdentitiesList'}>
+      {identities.map((identity, key) =>
+        <IdentitiesListItem
+          key={key}
+          identity={identity}
+        />
+      )}
 
-            {identities.length === 0 &&
-                <EmptyListMessage>There are no identities created yet.</EmptyListMessage>
-            }
-        </div>
+      {identities.length === 0 &&
+        <EmptyListMessage>There are no identities created yet.</EmptyListMessage>
+      }
+    </div>
   )
 }
 
--- packages/frontend/src/components/identities/IdentitiesListItem.js
+++ packages/frontend/src/components/identities/IdentitiesListItem.js
@@ -1,27 +1,40 @@
 import Link from 'next/link'
 import ImageGenerator from '../imageGenerator'
+import { Identifier } from '../data'
 import './IdentitiesListItem.scss'
 
 function IdentitiesListItem ({ identity }) {
   const { identifier, timestamp, isSystem } = identity
 
+  const alias = Math.floor(Math.random() * 2) ? 'alias' : null
+
   return (
     <Link
-        href={`/identity/${identifier}`}
-        className={'IdentitiesListItem'}
+      href={`/identity/${identifier}`}
+      className={'IdentitiesListItem'}
     >
-        <div className={'IdentitiesListItem__IdentifierContainer'}>
-            <ImageGenerator className={'IdentitiesListItem__Avatar'} username={identifier} lightness={50} saturation={50} width={28} height={28}/>
-            <div className={'IdentitiesListItem__Identifier'}>{identifier}</div>
-        </div>
+      <div className={'IdentitiesListItem__IdentifierContainer'}>
+        <ImageGenerator className={'IdentitiesListItem__Avatar'} username={identifier} lightness={50} saturation={50} width={28} height={28}/>
 
-        {isSystem && <div>SYSTEM</div>}
-
-        {(typeof timestamp === 'string') &&
-            <div className={'IdentitiesListItem__Timestamp'}>
-                {new Date(timestamp).toLocaleString()}
-            </div>
+        {alias
+          ? <div className={'IdentitiesListItem__Alias'}>{alias}</div>
+          : <Identifier
+              className={'IdentitiesListItem__Identifier'}
+              copyButton={true}
+              styles={['highlight-both']}
+            >
+              {identifier}
+            </Identifier>
         }
+      </div>
+
+      {isSystem && <div>SYSTEM</div>}
+
+      {(typeof timestamp === 'string') &&
+        <div className={'IdentitiesListItem__Timestamp'}>
+          {new Date(timestamp).toLocaleString()}
+        </div>
+      }
     </Link>
   )
 }
