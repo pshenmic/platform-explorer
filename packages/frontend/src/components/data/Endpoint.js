0e0e1cb3bd2f9d3bd2efa0da7b418b1fe080d29d
Alexey <andreewsky.alexey@gmail.com>
2024-10-15 16:47:49 +0300

    Update Endpoint styles

--- packages/frontend/src/components/data/Endpoint.js
+++ packages/frontend/src/components/data/Endpoint.js
@@ -2,12 +2,11 @@ import { CircleIcon, ArrowCornerIcon } from '../ui/icons'
 import './Endpoint.scss'
 
 function Endpoint ({ value, status, link }) {
-  const iconColor = (() => {
-    if (status === 'active') return 'green.default'
-    if (status === 'warining') return 'yellow.default'
-    if (status === 'error') return 'red.default'
-    return ''
-  })()
+  const iconColors = {
+    active: 'green.default',
+    warining: 'yellow.default',
+    error: 'red.default'
+  }
 
   const Wrapper = ({ children, ...props }) => {
     if (link) return <a href={link} target={'_blank'} rel={'noopener noreferrer'} {...props}>{children}</a>
@@ -19,7 +18,7 @@ function Endpoint ({ value, status, link }) {
       {link && <ArrowCornerIcon color={'brand.normal'} w={'10px'} h={'10px'} mr={'10px'}/>}
       <div className={'Endpoint__Value'}>{value}</div>
       {status !== undefined &&
-        <CircleIcon className={'Endpoint__Status'} w={'8px'} h={'8px'} color={iconColor}/>
+        <CircleIcon className={'Endpoint__Status'} w={'8px'} h={'8px'} color={iconColors[status] || ''}/>
       }
     </Wrapper>
   )
