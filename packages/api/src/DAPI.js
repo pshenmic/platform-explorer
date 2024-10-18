2618b77fc28721385bcf3980c320bee9eb79fce0
owl352 <kokosinca123@gmail.com>
2024-10-18 19:03:25 +0300

    lint

--- packages/api/src/DAPI.js
+++ packages/api/src/DAPI.js
@@ -1,26 +1,26 @@
-const {Identifier} = require('dash').PlatformProtocol
+const { Identifier } = require('dash').PlatformProtocol
 
 class DAPI {
   dapi
   dpp
 
-  constructor(dapi, dpp) {
+  constructor (dapi, dpp) {
     this.dapi = dapi
     this.dpp = dpp
   }
 
-  async getIdentityBalance(identifier) {
-    const {balance} = await this.dapi.platform.getIdentityBalance(Identifier.from(identifier))
+  async getIdentityBalance (identifier) {
+    const { balance } = await this.dapi.platform.getIdentityBalance(Identifier.from(identifier))
     return balance
   }
 
-  async getTotalCredits() {
-    const {totalCreditsInPlatform} = await this.dapi.platform.getTotalCreditsInPlatform()
+  async getTotalCredits () {
+    const { totalCreditsInPlatform } = await this.dapi.platform.getTotalCreditsInPlatform()
     return totalCreditsInPlatform
   }
 
-  async getEpochsInfo(count, start, ascending) {
-    const {epochsInfo} = await this.dapi.platform.getEpochsInfo(start, count, {ascending})
+  async getEpochsInfo (count, start, ascending) {
+    const { epochsInfo } = await this.dapi.platform.getEpochsInfo(start, count, { ascending })
     return epochsInfo
   }
 }
--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -47,7 +47,6 @@ module.exports = {
   start: async () => {
     client = new Dash.Client()
 
-
     await loadWasmDpp()
     await client.platform.initialize()
 
