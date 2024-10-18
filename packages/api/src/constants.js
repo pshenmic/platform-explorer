c0bf65e8ce7c6b6264e3972d0bd88aa321dbb8ff
owl352 <kokosinca123@gmail.com>
2024-10-18 23:45:31 +0300

    tiny fix

--- packages/api/src/constants.js
+++ packages/api/src/constants.js
@@ -4,7 +4,7 @@ let genesisTime
 
 module.exports = {
   EPOCH_CHANGE_TIME: Number(process.env.EPOCH_CHANGE_TIME),
-  TCP_CONNECT_TIMEOUT: Number(process.env.TCP_CONNECT_TIMEOUT ?? 400),
+  TCP_CONNECT_TIMEOUT: Number(process.env.TCP_CONNECT_TIMEOUT),
   get genesisTime () {
     if (!genesisTime || isNaN(genesisTime)) {
       return TenderdashRPC.getBlockByHeight(1).then((blockInfo) => {
