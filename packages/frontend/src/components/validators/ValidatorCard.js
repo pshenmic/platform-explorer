005eb96c002eb914f11cf33c309760c37206005d
pshenmic <pshenmic@gmail.com>
2024-10-22 00:31:09 +0700

    Fix lint

--- packages/frontend/src/components/validators/ValidatorCard.js
+++ packages/frontend/src/components/validators/ValidatorCard.js
@@ -5,7 +5,6 @@ import { HorisontalSeparator } from '../ui/separators'
 import Link from 'next/link'
 
 export default function ValidatorCard ({ validator, rate, className }) {
-
   return (
     <div className={`InfoBlock InfoBlock--Gradient ValidatorCard ${validator.loading ? 'ValidatorCard--Loading' : ''} ${className || ''}`}>
       <div className={'ValidatorCard__Header'}>
