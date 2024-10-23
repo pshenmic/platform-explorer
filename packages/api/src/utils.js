152f3aa4b66c039a4720ed3ffba86dcd73741c0f
owl352 <kokosinca123@gmail.com>
2024-10-23 16:27:48 +0300

    fix

--- packages/api/src/utils.js
+++ packages/api/src/utils.js
@@ -124,7 +124,7 @@ const checkTcpConnect = (port, host) => {
 
       connection.once('timeout', async () => {
         await connection.destroy()
-        resolve('ERR_CONNECTION_REFUSED')
+        resolve('ERR_CONNECTION_TIMEOUT')
       })
     } catch (e) {
       console.error(e)
