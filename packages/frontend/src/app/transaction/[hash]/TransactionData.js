fe622e1f7a6b7b00450b000a20c496f60a3f4b1a
Alexey <andreewsky.alexey@gmail.com>
2024-11-15 01:16:08 +0300

    Remove old commented code

--- packages/frontend/src/app/transaction/[hash]/TransactionData.js
+++ packages/frontend/src/app/transaction/[hash]/TransactionData.js
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+// import Link from 'next/link'
 import { StateTransitionEnum } from '../../../enums/state.transition.type'
 // import { Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react'
 import { ValueCard } from '../../../components/cards'
@@ -18,7 +18,7 @@ function TransactionData ({ data }) {
         value={(
           <ValueCard>
             <Identifier copyButton={true} ellipsis={true} styles={['highlight-both']}>
-              {/*{data?.proTxHash}*/}
+              {/* {data?.proTxHash} */}
             </Identifier>
           </ValueCard>
         )}
@@ -62,25 +62,7 @@ function TransactionData ({ data }) {
   }
 
   if (data.type === StateTransitionEnum.DATA_CONTRACT_CREATE) {
-    return (<>
-      {/*<Thead>*/}
-      {/*  <Tr>*/}
-      {/*    <Th>Created data contract</Th>*/}
-      {/*    <Th></Th>*/}
-      {/*  </Tr>*/}
-      {/*</Thead>*/}
-
-      {/*<Tbody>*/}
-      {/*  <Tr>*/}
-      {/*    <Td>Data contract</Td>*/}
-      {/*    <Td><Link href={`/dataContract/${data.dataContractId}`}>{data.dataContractId}</Link></Td>*/}
-      {/*  </Tr>*/}
-      {/*  <Tr>*/}
-      {/*    <Td>Owner</Td>*/}
-      {/*    <Td><Link href={`/identity/${data.identityId}`}>{data.identityId}</Link></Td>*/}
-      {/*  </Tr>*/}
-      {/*</Tbody>*/}
-    </>)
+    return (<>DATA_CONTRACT_CREATE</>)
   }
 
   if (data.type === StateTransitionEnum.DOCUMENTS_BATCH) {
@@ -110,26 +92,6 @@ function TransactionData ({ data }) {
         // loading={transaction.loading}
         error={data?.transitions === undefined}
       />
-
-      {/*<Thead>*/}
-      {/*  <Tr>*/}
-      {/*    <Th>Changed documents</Th>*/}
-      {/*    <Th></Th>*/}
-      {/*  </Tr>*/}
-      {/*</Thead>*/}
-
-      {/*{data.transitions.map((transition, key) =>*/}
-      {/*  <Tbody className='TransactionData__DocumentsBatch' key={'dc' + key}>*/}
-      {/*    <Tr>*/}
-      {/*      <Td>Data contract</Td>*/}
-      {/*      <Td><Link href={`/dataContract/${transition.dataContractId}`}>{transition.dataContractId}</Link></Td>*/}
-      {/*    </Tr>*/}
-      {/*    <Tr>*/}
-      {/*      <Td>Document</Td>*/}
-      {/*      <Td><Link href={`/document/${transition.id}`}>{transition.id}</Link></Td>*/}
-      {/*    </Tr>*/}
-      {/*  </Tbody>*/}
-      {/*)}*/}
     </>)
   }
 
@@ -152,133 +114,23 @@ function TransactionData ({ data }) {
   }
 
   if (data.type === StateTransitionEnum.IDENTITY_TOP_UP) {
-    return (<>
-        {/*<Thead>*/}
-        {/*  <Tr>*/}
-        {/*    <Th>Credit top up</Th>*/}
-        {/*    <Th></Th>*/}
-        {/*  </Tr>*/}
-        {/*</Thead>*/}
-
-        {/*<Tbody>*/}
-        {/*  <Tr>*/}
-        {/*    <Td>Amount</Td>*/}
-        {/*    <Td>{data.amount} Credits</Td>*/}
-        {/*  </Tr>*/}
-        {/*  <Tr>*/}
-        {/*    <Td>Identity</Td>*/}
-        {/*    <Td><Link href={`/identity/${data.identityId}`}>{data.identityId}</Link></Td>*/}
-        {/*  </Tr>*/}
-        {/*</Tbody>*/}
-    </>)
+    return (<>IDENTITY_TOP_UP</>)
   }
 
   if (data.type === StateTransitionEnum.DATA_CONTRACT_UPDATE) {
-    return (<>
-      {/*<Thead>*/}
-      {/*    <Tr>*/}
-      {/*        <Th>Updated data contract</Th>*/}
-      {/*        <Th></Th>*/}
-      {/*    </Tr>*/}
-      {/*</Thead>*/}
-
-      {/*<Tbody>*/}
-      {/*  <Tr>*/}
-      {/*    <Td>Data contract</Td>*/}
-      {/*    <Td><Link href={`/dataContract/${data.dataContractId}`}>{data.dataContractId}</Link></Td>*/}
-      {/*  </Tr>*/}
-      {/*  <Tr>*/}
-      {/*    <Td>Owner</Td>*/}
-      {/*    <Td><Link href={`/identity/${data.identityId}`}>{data.identityId}</Link></Td>*/}
-      {/*  </Tr>*/}
-      {/*  <Tr>*/}
-      {/*    <Td>Version</Td>*/}
-      {/*    <Td>{data.version}</Td>*/}
-      {/*  </Tr>*/}
-      {/*</Tbody>*/}
-    </>)
+    return (<>DATA_CONTRACT_UPDATE</>)
   }
 
   if (data.type === StateTransitionEnum.IDENTITY_UPDATE) {
-    return (<>
-      {/*<Thead>*/}
-      {/*  <Tr>*/}
-      {/*    <Th>Updated identity</Th>*/}
-      {/*    <Th></Th>*/}
-      {/*  </Tr>*/}
-      {/*</Thead>*/}
-
-      {/*<Tbody>*/}
-      {/*  <Tr>*/}
-      {/*    <Td>Identity</Td>*/}
-      {/*    <Td><Link href={`/identity/${data.identityId}`}>{data.identityId}</Link></Td>*/}
-      {/*  </Tr>*/}
-      {/*  <Tr>*/}
-      {/*    <Td>Revision</Td>*/}
-      {/*    <Td>{data.revision}</Td>*/}
-      {/*  </Tr>*/}
-      {/*</Tbody>*/}
-    </>)
+    return (<>IDENTITY_UPDATE</>)
   }
 
   if (data.type === StateTransitionEnum.IDENTITY_CREDIT_WITHDRAWAL) {
-    return (<>
-      {/*<Thead>*/}
-      {/*  <Tr>*/}
-      {/*    <Th>Credit withdrawal</Th>*/}
-      {/*    <Th></Th>*/}
-      {/*  </Tr>*/}
-      {/*</Thead>*/}
-
-      {/*<Tbody>*/}
-      {/*  <Tr>*/}
-      {/*    <Td>Identity</Td>*/}
-      {/*    <Td><Link href={`/identity/${data.senderId}`}>{data.senderId}</Link></Td>*/}
-      {/*  </Tr>*/}
-      {/*  <Tr>*/}
-      {/*    <Td>Output script</Td>*/}
-      {/*    <Td>{data.outputScript}</Td>*/}
-      {/*  </Tr>*/}
-      {/*  <Tr>*/}
-      {/*    <Td>Amount</Td>*/}
-      {/*    <Td>{data.amount} Credits</Td>*/}
-      {/*  </Tr>*/}
-      {/*  <Tr>*/}
-      {/*    <Td>Core fee per byte</Td>*/}
-      {/*    <Td>{data.coreFeePerByte}</Td>*/}
-      {/*  </Tr>*/}
-      {/*  <Tr>*/}
-      {/*    <Td>Nonce</Td>*/}
-      {/*    <Td>{data.nonce}</Td>*/}
-      {/*  </Tr>*/}
-      {/*</Tbody>*/}
-    </>)
+    return (<>IDENTITY_CREDIT_WITHDRAWAL</>)
   }
 
   if (data.type === StateTransitionEnum.IDENTITY_CREDIT_TRANSFER) {
-    return (<>
-      {/*<Thead>*/}
-      {/*  <Tr>*/}
-      {/*    <Th>Credit transfer</Th>*/}
-      {/*    <Th></Th>*/}
-      {/*  </Tr>*/}
-      {/*</Thead>*/}
-
-      {/*<Tbody>*/}
-      {/*  <Tr>*/}
-      {/*    <Td>Amount</Td>*/}
-      {/*    <Td>{data.amount} Credits</Td>*/}
-      {/*  </Tr>*/}
-      {/*  <Tr>*/}
-      {/*    <Td>Sender</Td>*/}
-      {/*    <Td><Link href={`/identity/${data.senderId}`}>{data.senderId}</Link></Td>*/}
-      {/*  </Tr>*/}
-      {/*  <Tr>*/}
-      {/*    <Td>Recipient</Td>*/}
-      {/*    <Td><Link href={`/identity/${data.recipientId}`}>{data.recipientId}</Link></Td>*/}
-      {/*  </Tr>*/}
-      {/*</Tbody>*/}
-    </>)
+    return (<>IDENTITY_CREDIT_TRANSFER</>)
   }
 }
 
