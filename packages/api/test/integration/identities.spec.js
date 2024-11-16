eaf89a4fef2c2291f7d16eebe6603355a5de8233
owl352 <kokosinca123@gmail.com>
2024-11-16 06:43:57 +0300

    lint

--- packages/api/test/integration/identities.spec.js
+++ packages/api/test/integration/identities.spec.js
@@ -273,7 +273,7 @@ describe('Identities routes', () => {
     })
 
     it('should return 404 whe identity not exist', async () => {
-      await client.get(`/identity/1234123123PFdomuTVvNy3VRrvWgvkKPzqehEBpNf2nk6/withdrawals`)
+      await client.get('/identity/1234123123PFdomuTVvNy3VRrvWgvkKPzqehEBpNf2nk6/withdrawals')
         .expect(404)
         .expect('Content-Type', 'application/json; charset=utf-8')
     })
