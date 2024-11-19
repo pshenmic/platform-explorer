cbdfb4d38276446732e60fe5efa8a4b306160fc1
Alexey <andreewsky.alexey@gmail.com>
2024-11-19 18:44:07 +0300

    Add tooltip for locked alias

--- packages/frontend/src/components/data/Alias.js
+++ packages/frontend/src/components/data/Alias.js
@@ -1,4 +1,5 @@
 import './Alias.scss'
+import { Tooltip } from '../ui/Tooltips'
 
 export default function Alias ({ alias, status, children, ellipsis = true, className }) {
   alias = alias || children
@@ -7,20 +8,28 @@ export default function Alias ({ alias, status, children, ellipsis = true, class
   const dashIndex = alias?.lastIndexOf('.dash')
   const statusClass = status === 'locked' ? 'Alias--Locked' : ''
 
-  return (
-    <div className={`Alias ${statusClass} ${ellipsis ? 'Alias--Ellipsis' : ''}  ${className || ''}`}>
-      <span className={'Alias__Name'}>
-        {dashIndex !== -1
-          ? alias?.slice(0, dashIndex)
-          : alias
-        }
-      </span>
+  const Container = ({ children }) => (
+    status === 'locked'
+      ? <Tooltip content={'Alias is locked'} placement={'top'}>{children}</Tooltip>
+      : <>{children}</>
+  )
 
-      {dashIndex !== -1 &&
-        <span className={'Alias__Domain'}>
-          {alias?.slice(dashIndex)}
+  return (
+    <Container>
+      <div className={`Alias ${statusClass} ${ellipsis ? 'Alias--Ellipsis' : ''}  ${className || ''}`}>
+        <span className={'Alias__Name'}>
+          {dashIndex !== -1
+            ? alias?.slice(0, dashIndex)
+            : alias
+          }
         </span>
-      }
-    </div>
+
+        {dashIndex !== -1 &&
+          <span className={'Alias__Domain'}>
+            {alias?.slice(dashIndex)}
+          </span>
+        }
+      </div>
+    </Container>
   )
 }
