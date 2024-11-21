dd4b11faf3d5a369bfc2a5ac7c4747eafdd0cb55
Alexey <andreewsky.alexey@gmail.com>
2024-11-21 19:34:00 +0300

    Add support off statuses in string format

--- packages/frontend/src/components/transfers/withdrawals/StatusIcon.js
+++ packages/frontend/src/components/transfers/withdrawals/StatusIcon.js
@@ -6,7 +6,12 @@ function StatusIcon ({ status, ...props }) {
     1: <PooledIcon {...props}/>,
     2: <BroadcastedIcon {...props}/>,
     3: <SuccessIcon {...props}/>,
-    4: <ErrorIcon {...props}/>
+    4: <ErrorIcon {...props}/>,
+    QUEUED: <QueuedIcon {...props}/>,
+    POOLED: <PooledIcon {...props}/>,
+    BROADCASTED: <BroadcastedIcon {...props}/>,
+    COMPLETE: <SuccessIcon {...props}/>,
+    EXPIRED: <ErrorIcon {...props}/>
   }
 
   return StatusIcons[status] || <></>
