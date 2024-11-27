722efd073d3042c8f2c787b9883fc57d6be7f0b9
Alexey <andreewsky.alexey@gmail.com>
2024-11-27 19:17:31 +0300

    Add document Identitfier field to Transiton Card

--- packages/frontend/src/components/transactions/TransitionCard.js
+++ packages/frontend/src/components/transactions/TransitionCard.js
@@ -26,6 +26,18 @@ function TransitionCard ({ transition }) {
           error={!transition.dataContractId}
         />
         <InfoLine
+          className={'TransitionCard__InfoLine TransitionCard__InfoLine--DataContract'}
+          title={'Document Identifier'}
+          value={(
+            <ValueCard link={`/document/${transition.id}`}>
+              <Identifier avatar={true} copyButton={true} ellipsis={true} styles={['highlight-both']}>
+                {transition.id}
+              </Identifier>
+            </ValueCard>
+          )}
+          error={!transition.id}
+        />
+        <InfoLine
           className={'TransitionCard__InfoLine TransitionCard__InfoLine--Revision'}
           title={'Revision'}
           value={transition?.revision}
