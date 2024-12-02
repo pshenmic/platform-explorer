106b3c57a0a4fff5028d73efcd75cb4d96aeb98a
Alexey <andreewsky.alexey@gmail.com>
2024-12-02 21:25:08 +0300

    Implement time zone info in Footer component

---
+++ packages/frontend/src/components/layout/footer/LocalTime.js
@@ -0,0 +1,27 @@
+import './LocalTime.scss'
+
+function LocalTime () {
+  const now = new Date()
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+
+  const time = now.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit'
+  })
+
+  const date = now.toLocaleDateString('en-US', {
+    weekday: 'short',
+    day: '2-digit',
+    month: 'short'
+  })
+
+  return (
+    <div className={'LocalTime'}>
+      <span className={'LocalTime__Time'}>{time}</span>
+      <span className={'LocalTime__Date'}>{date}</span>
+      <span className={'LocalTime__TimeZone'}>({timeZone})</span>
+    </div>
+  )
+}
+
+export default LocalTime
