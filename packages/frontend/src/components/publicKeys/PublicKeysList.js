1db5f854d941be9d04a2e15623503a17f41934a3
Alexey <andreewsky.alexey@gmail.com>
2024-12-05 19:56:00 +0300

    Update PublicKeysListItem styles

--- packages/frontend/src/components/publicKeys/PublicKeysList.js
+++ packages/frontend/src/components/publicKeys/PublicKeysList.js
@@ -38,27 +38,31 @@ function PublicKeysList ({ publicKeys = [] }) {
       {publicKeys.map((publicKey, i) => (
         <div className={'PublicKeysList__ListItem PublicKeysListItem'} key={i}>
           <Grid className={'PublicKeysListItem__Content'}>
-            <GridItem className={'PublicKeysListItem__Column PublicKeysListItem__Column--Id'}>{publicKey?.id}</GridItem>
+            <GridItem className={'PublicKeysListItem__Column PublicKeysListItem__Column--Id'}>
+              {publicKey?.id}
+            </GridItem>
             <GridItem className={'PublicKeysListItem__Column--PublicKeyHash'}>
-              <ValueCard className={'PublicKeysListItem__PublicKeyHash'}>{publicKey?.publicKeyHash}</ValueCard>
+              <ValueCard className={'PublicKeysListItem__PublicKeyHash'} size={'sm'} colorScheme={'transparent'}>
+                {publicKey?.publicKeyHash}
+              </ValueCard>
             </GridItem>
             <GridItem className={'PublicKeysListItem__Column PublicKeysListItem__Column--Type'}>
-              <ValueContainer colorScheme={'gray'}>
+              <ValueContainer colorScheme={'gray'} size={'sm'}>
                 {publicKey?.type}
               </ValueContainer>
             </GridItem>
             <GridItem className={'PublicKeysListItem__Column PublicKeysListItem__Column--Purpose'}>
-              <ValueContainer colorScheme={'blue'}>
+              <ValueContainer colorScheme={'blue'} size={'sm'}>
                 {publicKey?.purpose}
               </ValueContainer>
             </GridItem>
             <GridItem className={'PublicKeysListItem__Column PublicKeysListItem__Column--SecurityLevel'}>
-              <ValueContainer colorScheme={'green'}>
+              <ValueContainer colorScheme={'green'} size={'sm'}>
                 {publicKey?.securityLevel}
               </ValueContainer>
             </GridItem>
             <GridItem className={'PublicKeysListItem__Column PublicKeysListItem__Column--ReadOnly'}>
-              <ValueContainer colorScheme={!publicKey?.readOnly ? 'green' : 'red'}>
+              <ValueContainer colorScheme={!publicKey?.readOnly ? 'green' : 'red'} size={'sm'}>
                 {publicKey?.readOnly ? 'True' : 'False'}
               </ValueContainer>
             </GridItem>
