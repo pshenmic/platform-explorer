05bafe97f4e0ba3d5cf623bf697943382fd533e0
Alexey <andreewsky.alexey@gmail.com>
2024-12-06 01:04:11 +0300

    Add link properties and LightGray colorScheme to ValueContainer component

--- packages/frontend/src/components/ui/containers/ValueContainer.js
+++ packages/frontend/src/components/ui/containers/ValueContainer.js
@@ -1,9 +1,12 @@
-import './ValueContainer.scss'
 import { ArrowCornerIcon } from '../icons'
+import Link from 'next/link'
+import './ValueContainer.scss'
 
 function ValueContainer ({
   children,
   clickable,
+  link,
+  elipsed = false,
   colorScheme = 'default',
   size = 'default',
   external,
@@ -11,30 +14,39 @@ function ValueContainer ({
   className,
   ...props
 }) {
+  const Wrapper = (props) => {
+    return typeof link === 'string'
+      ? <Link href={link} className={props.className}>{props.children}</Link>
+      : <div className={props.className}>{props.children}</div>
+  }
+
   const colorClasses = {
     default: '',
     red: 'ValueContainer--Red',
     green: 'ValueContainer--Green',
     blue: 'ValueContainer--Blue',
+    lightGray: 'ValueContainer--LightGray',
     gray: 'ValueContainer--Gray'
   }
 
   const sizeClasses = {
     default: '',
-    sm: 'ValueContainer--SizeSm'
+    sm: 'ValueContainer--SizeSm',
+    xs: 'ValueContainer--SizeXs'
   }
 
   let extraClass = ''
 
-  if (clickable) extraClass += 'ValueContainer--Clickable'
-  if (external) extraClass += 'ValueContainer--External'
-  if (light) extraClass += 'ValueContainer--Light'
+  if (clickable || link) extraClass += ' ValueContainer--Clickable'
+  if (external) extraClass += ' ValueContainer--External'
+  if (light) extraClass += ' ValueContainer--Light'
+  if (elipsed) extraClass += ' ValueContainer--Elipsed'
 
   extraClass += ' ' + colorClasses?.[colorScheme] || colorClasses.default
   extraClass += ' ' + sizeClasses?.[size] || sizeClasses.default
 
   return (
-    <div className={`ValueContainer ${extraClass} ${className || ''}`} {...props}>
+    <Wrapper className={`ValueContainer ${extraClass} ${className || ''}`} {...props}>
       {external && (
         <div className={'ValueContainer__ExternalIcon'}>
           <ArrowCornerIcon color={'brand.normal'} w={'10px'} h={'10px'} mr={'12px'}/>
@@ -43,7 +55,7 @@ function ValueContainer ({
       <div className={'ValueContainer__Value'}>
         {children}
       </div>
-    </div>
+    </Wrapper>
   )
 }
 
