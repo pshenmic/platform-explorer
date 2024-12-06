d658520c6db1949da4e6c9bfc759ae7dbd047aa2
Alexey <andreewsky.alexey@gmail.com>
2024-12-06 03:00:46 +0300

    Update AliasesList styles

--- packages/frontend/src/app/identity/[identifier]/AliasesList.js
+++ packages/frontend/src/app/identity/[identifier]/AliasesList.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import { Alias } from '../../../components/data'
+import { Alias, DateBlock } from '../../../components/data'
 import { Button } from '@chakra-ui/react'
+import './AliasesList.scss'
 
 function AliasesList ({ aliases = [], smallCount = 5 }) {
   const [showAll, setShowAll] = useState(false)
@@ -13,14 +14,17 @@ function AliasesList ({ aliases = [], smallCount = 5 }) {
     <div className={'AliasesList'}>
       <div className={'AliasesList__ItemsContainer'}>
         {filteredArray?.map((alias, i) => (
-          <Alias status={alias.status} key={i}>{alias.alias}</Alias>
+          <div className={'AliasesList__Item'} key={i}>
+            <Alias status={alias.status} key={i}>{alias.alias}</Alias>
+            <DateBlock timestamp={1233123333332} format={'deltaOnly'}/>
+          </div>
         ))}
       </div>
       <Button
         onClick={() => setShowAll(!showAll)}
         className={'AliasesList__ShowMoreButton'}
         size={'sm'}
-        colorScheme={showAll ? 'gray' : 'blue'}
+        variant={showAll ? 'gray' : 'blue'}
       >
         {showAll ? 'Show less' : 'Show more'}
       </Button>
