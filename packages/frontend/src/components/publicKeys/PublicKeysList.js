b1857b74bb660cd030fd0c38be48e73f8241f583
Alexey <andreewsky.alexey@gmail.com>
2024-12-10 18:40:33 +0300

    Add scroll to PublicKeysList for small resolution

--- packages/frontend/src/components/publicKeys/PublicKeysList.js
+++ packages/frontend/src/components/publicKeys/PublicKeysList.js
@@ -8,37 +8,39 @@ import './PublicKeyBoundCard.scss'
 function PublicKeysList ({ publicKeys = [], className }) {
   return (
     <div className={`PublicKeysList ${className || ''}`}>
-      <Grid className={'PublicKeysList__ColumnTitles'}>
-        <GridItem className={'PublicKeysList__ColumnTitle PublicKeysList__ColumnTitle--Timestamp'}>
-          Key Id
-        </GridItem>
-        <GridItem className={'PublicKeysList__ColumnTitle PublicKeysList__ColumnTitle--Hash'}>
-          Public Key Hash
-        </GridItem>
-        <GridItem className={'PublicKeysList__ColumnTitle PublicKeysList__ColumnTitle--GasUsed'}>
-          Type
-        </GridItem>
-        <GridItem className={'PublicKeysList__ColumnTitle PublicKeysList__ColumnTitle--Owner'}>
-          Purpose
-        </GridItem>
-        <GridItem className={'PublicKeysList__ColumnTitle PublicKeysList__ColumnTitle--Type'}>
-          Security Level
-        </GridItem>
-        <GridItem className={'PublicKeysList__ColumnTitle PublicKeysList__ColumnTitle--Type'}>
-          Read only
-        </GridItem>
-        <GridItem className={'PublicKeysList__ColumnTitle PublicKeysList__ColumnTitle--Type'}>
-          Data
-        </GridItem>
-      </Grid>
+      <div className={'PublicKeysList__ScrollZone'}>
+        <Grid className={'PublicKeysList__ColumnTitles'}>
+          <GridItem className={'PublicKeysList__ColumnTitle PublicKeysList__ColumnTitle--Timestamp'}>
+            Key Id
+          </GridItem>
+          <GridItem className={'PublicKeysList__ColumnTitle PublicKeysList__ColumnTitle--Hash'}>
+            Public Key Hash
+          </GridItem>
+          <GridItem className={'PublicKeysList__ColumnTitle PublicKeysList__ColumnTitle--GasUsed'}>
+            Type
+          </GridItem>
+          <GridItem className={'PublicKeysList__ColumnTitle PublicKeysList__ColumnTitle--Owner'}>
+            Purpose
+          </GridItem>
+          <GridItem className={'PublicKeysList__ColumnTitle PublicKeysList__ColumnTitle--Type'}>
+            Security Level
+          </GridItem>
+          <GridItem className={'PublicKeysList__ColumnTitle PublicKeysList__ColumnTitle--Type'}>
+            Read only
+          </GridItem>
+          <GridItem className={'PublicKeysList__ColumnTitle PublicKeysList__ColumnTitle--Type'}>
+            Data
+          </GridItem>
+        </Grid>
 
-      {publicKeys?.length > 0 &&
-        publicKeys.map((publicKey, i) => <PublicKeysListItem publicKey={publicKey} key={i}/>)
-      }
+        {publicKeys?.length > 0 &&
+          publicKeys.map((publicKey, i) => <PublicKeysListItem publicKey={publicKey} key={i}/>)
+        }
 
-      {publicKeys?.length === 0 &&
-        <EmptyListMessage>There are no public keys</EmptyListMessage>
-      }
+        {publicKeys?.length === 0 &&
+          <EmptyListMessage>There are no public keys</EmptyListMessage>
+        }
+      </div>
     </div>
   )
 }
