48614b7c0775b21f29a18115165e282c0f225730
Alexey <andreewsky.alexey@gmail.com>
2024-12-11 18:43:28 +0300

    Implement SmoothSize container to index.js container file

--- packages/frontend/src/components/ui/containers/index.js
+++ packages/frontend/src/components/ui/containers/index.js
@@ -1,9 +1,11 @@
 import ValueContainer from './ValueContainer'
 import PageDataContainer from './PageDataContainer'
 import InfoContainer from './InfoContainer'
+import SmoothSize from './SmoothSize'
 
 export {
   ValueContainer,
   PageDataContainer,
-  InfoContainer
+  InfoContainer,
+  SmoothSize
 }
