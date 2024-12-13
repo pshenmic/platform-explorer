1de0141af9443828c35a7a8bd9333d67cb19a764
Alexey <andreewsky.alexey@gmail.com>
2024-12-13 12:55:09 +0300

    Update BackButton logic in PageDataContainer

--- packages/frontend/src/components/ui/containers/PageDataContainer.js
+++ packages/frontend/src/components/ui/containers/PageDataContainer.js
@@ -1,7 +1,7 @@
 import { Container } from '@chakra-ui/react'
-import Link from 'next/link'
 import { ChevronLeftIcon } from '@chakra-ui/icons'
 import './PageDataContainer.scss'
+import BackButton from '../Buttons/BackButton'
 
 function PageDataContainer ({ className, title, backLink, children }) {
   return (
@@ -14,10 +14,9 @@ function PageDataContainer ({ className, title, backLink, children }) {
     >
       <Container maxW={'container.maxPageW'} px={[0]} py={0}>
         <div className={'PageDataContainer__Header'}>
-          {backLink &&
-            <Link href={backLink} className={'PageDataContainer__BackLink'}>
-              <ChevronLeftIcon w={4} h={4} color='brand.normal'/>
-            </Link>}
+          <BackButton link={backLink} className={'PageDataContainer__BackLink'}>
+            <ChevronLeftIcon w={4} h={4} color={'brand.normal'}/>
+          </BackButton>
           {title && <div className={'PageDataContainer__Title'}>{title}</div>}
         </div>
 
