c8761faa973baa74bf57603cdec248e16bf77127
Alexey <andreewsky.alexey@gmail.com>
2024-12-17 16:24:50 +0300

    Create LinkContainer component

--- packages/frontend/src/components/ui/containers/index.js
+++ packages/frontend/src/components/ui/containers/index.js
@@ -2,10 +2,12 @@ import ValueContainer from './ValueContainer'
 import PageDataContainer from './PageDataContainer'
 import InfoContainer from './InfoContainer'
 import SmoothSize from './SmoothSize'
+import LinkContainer from './LinkContainer'
 
 export {
   ValueContainer,
   PageDataContainer,
   InfoContainer,
-  SmoothSize
+  SmoothSize,
+  LinkContainer
 }
