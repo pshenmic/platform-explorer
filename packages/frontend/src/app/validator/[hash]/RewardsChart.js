0007bc650df804999bde92a117e0d2139fce1b70
Alexey <andreewsky.alexey@gmail.com>
2024-12-19 14:50:19 +0300

    Create TabsChartBlock component and optimization Charts code on the specific Validator page

--- packages/frontend/src/app/validator/[hash]/RewardsChart.js
+++ packages/frontend/src/app/validator/[hash]/RewardsChart.js
@@ -1,44 +1,15 @@
-import { useState, useEffect, useRef } from 'react'
-import useResizeObserver from '@react-hook/resize-observer'
-import { fetchHandlerSuccess, fetchHandlerError, getDaysBetweenDates, getDynamicRange } from '../../../util'
-import { LineChart, TimeframeSelector } from './../../../components/charts'
 import * as Api from '../../../util/Api'
-import { ErrorMessageBlock } from '../../../components/Errors'
-
-const chartConfig = {
-  timespan: {
-    defaultIndex: 3,
-    values: [
-      {
-        label: '24 hours',
-        range: getDynamicRange(24 * 60 * 60 * 1000)
-      },
-      {
-        label: '3 days',
-        range: getDynamicRange(3 * 24 * 60 * 60 * 1000)
-      },
-      {
-        label: '1 week',
-        range: getDynamicRange(7 * 24 * 60 * 60 * 1000)
-      },
-      {
-        label: '1 Month',
-        range: getDynamicRange(30 * 24 * 60 * 60 * 1000)
-      }
-    ]
-  }
-}
+import { useState, useEffect } from 'react'
+import { fetchHandlerSuccess, fetchHandlerError, getDaysBetweenDates } from '../../../util'
+import TabsChartBlock from '../../../components/charts/TabsChartBlock'
+import { defaultChartConfig } from '../../../components/charts/config'
 
 export default function RewardsChart ({ hash, isActive }) {
   const [rewardsHistory, setRewardsHistory] = useState({ data: {}, loading: true, error: false })
-  const [timespan, setTimespan] = useState(chartConfig.timespan.values[chartConfig.timespan.defaultIndex])
-  const [customRange, setCustomRange] = useState({ start: null, end: null })
-  const [menuIsOpen, setMenuIsOpen] = useState(false)
-  const TimeframeMenuRef = useRef(null)
-  const [selectorHeight, setSelectorHeight] = useState(0)
+  const [timespan, setTimespan] = useState(defaultChartConfig.timespan.values[defaultChartConfig.timespan.defaultIndex])
 
   useEffect(() => {
-    const { start = null, end = null } = timespan.range
+    const { start = null, end = null } = timespan?.range
     if (!start || !end) return
 
     setRewardsHistory(state => ({ ...state, loading: true }))
@@ -46,58 +17,30 @@ export default function RewardsChart ({ hash, isActive }) {
     Api.getRewardsStatsByValidator(hash, start, end)
       .then(res => fetchHandlerSuccess(setRewardsHistory, { resultSet: res }))
       .catch(err => fetchHandlerError(setRewardsHistory, err))
-  }, [timespan, customRange])
-
-  const updateMenuHeight = () => {
-    if (menuIsOpen && TimeframeMenuRef?.current) {
-      const element = TimeframeMenuRef.current
-      const height = element.getBoundingClientRect().height
-      setSelectorHeight(height)
-    } else {
-      setSelectorHeight(0)
-    }
-  }
-
-  useEffect(updateMenuHeight, [menuIsOpen, TimeframeMenuRef])
-
-  useResizeObserver(TimeframeMenuRef, updateMenuHeight)
-
-  if (rewardsHistory.error || (!rewardsHistory.loading && !rewardsHistory.data?.resultSet)) {
-    return (<ErrorMessageBlock/>)
-  }
+  }, [timespan, hash])
 
   return (
-    <div style={{ height: menuIsOpen ? `${Math.max(selectorHeight, 350)}px` : '350px' }} className={'TabsChart'}>
-      <TimeframeSelector
-        menuRef={TimeframeMenuRef}
-        className={'TabsChart__TimeframeSelector'}
-        config={chartConfig}
-        changeCallback={setTimespan}
-        isActive={isActive}
-        openStateCallback={setMenuIsOpen}
-      />
-      <div className={`TabsChart__ChartContiner ${menuIsOpen ? 'TabsChart__ChartContiner--Hidden' : ''}`}>
-        <LineChart
-          data={rewardsHistory.data?.resultSet?.map((item) => ({
-            x: new Date(item.timestamp),
-            y: item.data.reward
-          })) || []}
-          dataLoading={rewardsHistory.loading}
-          timespan={timespan.range}
-          xAxis={{
-            type: (() => {
-              if (getDaysBetweenDates(timespan.range.start, timespan.range.end) > 7) return { axis: 'date' }
-              if (getDaysBetweenDates(timespan.range.start, timespan.range.end) > 3) return { axis: 'date', tooltip: 'datetime' }
-              return { axis: 'time' }
-            })()
-          }}
-          yAxis={{
-            type: 'number',
-            abbreviation: 'Credits'
-          }}
-          height={'350px'}
-        />
-      </div>
-    </div>
+    <TabsChartBlock
+      isActive={isActive}
+      timespanChangeCallback={setTimespan}
+      timespan={timespan}
+      data={rewardsHistory.data?.resultSet?.map((item) => ({
+        x: new Date(item.timestamp),
+        y: item.data.reward
+      })) || []}
+      loading={rewardsHistory.loading}
+      error={rewardsHistory.error}
+      xAxis={{
+        type: (() => {
+          if (getDaysBetweenDates(timespan.range.start, timespan.range.end) > 7) return { axis: 'date' }
+          if (getDaysBetweenDates(timespan.range.start, timespan.range.end) > 3) return { axis: 'date', tooltip: 'datetime' }
+          return { axis: 'time' }
+        })()
+      }}
+      yAxis={{
+        type: 'number',
+        abbreviation: 'Credits'
+      }}
+    />
   )
 }
---
+++ packages/frontend/src/components/charts/TabsChartBlock.js
@@ -0,0 +1,69 @@
+'use client'
+
+import { useState, useEffect, useRef } from 'react'
+import useResizeObserver from '@react-hook/resize-observer'
+import { LineChart, TimeframeSelector } from './'
+import { ErrorMessageBlock } from '../Errors'
+import { defaultChartConfig } from './config'
+import './TabsChartBlock.scss'
+
+export default function TabsChartBlock ({
+  isActive,
+  timespanChangeCallback,
+  loading,
+  error,
+  data,
+  xAxis,
+  yAxis,
+  timespan,
+  chartConfig = defaultChartConfig,
+  heightPx = 350
+}) {
+  const [menuIsOpen, setMenuIsOpen] = useState(false)
+  const TimeframeMenuRef = useRef(null)
+  const [selectorHeight, setSelectorHeight] = useState(0)
+
+  const updateMenuHeight = () => {
+    if (menuIsOpen && TimeframeMenuRef?.current) {
+      const element = TimeframeMenuRef.current
+      const height = element.getBoundingClientRect().height
+      setSelectorHeight(height)
+    } else {
+      setSelectorHeight(0)
+    }
+  }
+
+  useEffect(updateMenuHeight, [menuIsOpen, TimeframeMenuRef])
+
+  useResizeObserver(TimeframeMenuRef, updateMenuHeight)
+
+  if (error || (!loading && !data)) {
+    return (<ErrorMessageBlock/>)
+  }
+
+  return (
+    <div
+      className={'TabsChartBlock'}
+      style={{ height: menuIsOpen ? `${Math.max(selectorHeight, heightPx)}px` : `${heightPx}px` }}
+    >
+      <TimeframeSelector
+        menuRef={TimeframeMenuRef}
+        className={'TabsChartBlock__TimeframeSelector'}
+        config={chartConfig}
+        changeCallback={timespanChangeCallback}
+        isActive={isActive}
+        openStateCallback={setMenuIsOpen}
+      />
+      <div className={`TabsChartBlock__ChartContiner ${menuIsOpen ? 'TabsChartBlock__ChartContiner--Hidden' : ''}`}>
+        <LineChart
+          data={data}
+          dataLoading={loading}
+          timespan={timespan}
+          xAxis={xAxis}
+          yAxis={yAxis}
+          height={`${heightPx}px`}
+        />
+      </div>
+    </div>
+  )
+}
