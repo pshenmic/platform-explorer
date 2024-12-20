45dc5c0d6669c83fd031d0b390221ee3ab0b5f80
Alexey <andreewsky.alexey@gmail.com>
2024-12-20 18:00:12 +0300

    remove mock data

--- packages/frontend/src/app/identity/[identifier]/Identity.js
+++ packages/frontend/src/app/identity/[identifier]/Identity.js
@@ -49,51 +49,6 @@ function Identity ({ identifier }) {
     ])
   }, [setBreadcrumbs, identifier])
 
-  console.log('identity', identity)
-
-  // mock //
-
-  // if (!identity.error && !identity.data?.lastWithdrawal) identity.data.lastWithdrawal = '6AC5EDA942093A9275A2837CFDF2C18CAAD9D922BA211BD5EA5E6333FE904CE7'
-  // if (!identity.error && !identity.data?.lastWithdrawalTime) identity.data.lastWithdrawalTime = '2024-11-21T10:26:04.053Z'
-
-  // if (!identity.data?.publicKeys?.length && identity.data !== null) {
-  //   identity.data.publicKeys = [
-  //     {
-  //       contractBounds: null,
-  //       id: 0,
-  //       type: 'ECDSA_SECP256K1',
-  //       data: '0348a6a633850f3c83a0cb30a9fceebbaa3b9ab3f923f123d92728cef234176dc5',
-  //       publicKeyHash: '07630dddc55729c043de7bdeb145ee0d44feae3b',
-  //       purpose: 'AUTHENTICATION',
-  //       securityLevel: 'MASTER',
-  //       readOnly: false,
-  //       signature: '2042186a3dec52bfe9a24ee17b98adc5efcbc0a0a6bacbc9627f1405ea5e1bb7ae2bb94a270363400969669e9884ab9967659e9a0d8de7464ee7c47552c8cb0e99'
-  //     },
-  //     {
-  //       contractBounds: null,
-  //       id: 1,
-  //       type: 'ECDSA_SECP256K1',
-  //       data: '034278b0d7f5e6d902ec5a30ae5c656937a0323bdc813e851eb8a2d6a1d23c51cf',
-  //       publicKeyHash: 'e2615c5ef3f910ebe5ada7930e7b2c04a7ffbb23',
-  //       purpose: 'AUTHENTICATION',
-  //       securityLevel: 'HIGH',
-  //       readOnly: true,
-  //       signature: '1fbb0d0bb63d26c0d5b6e1f4b8c0eebef4d256c4e8aa933a2cb6bd6b2d8aae545215312924c7dd41c963071e2ccfe2187a8684d93c55063cb45fdd03e76344d6a4'
-  //     },
-  //     {
-  //       contractBounds: null,
-  //       id: 2,
-  //       type: 'ECDSA_SECP256K1',
-  //       data: '0245c3b0f0323ddbb9ddf123f939bf37296af4f38fa489aad722c50486575cd8f4',
-  //       publicKeyHash: 'd53ee3b3518fee80816ab26af98a34ea60ae9af7',
-  //       purpose: 'AUTHENTICATION',
-  //       securityLevel: 'CRITICAL',
-  //       readOnly: false,
-  //       signature: '204013dcca13378b820e40cf1da77abe38662546ef0a304545de3c35845b83a7ad4b42051c2b3539c9181b3f0cb3fb4bc970db89663c6bd6ca1468568a62beaa75'
-  //     }
-  //   ]
-  // }
-
   const fetchData = () => {
     Promise.all([
       Api.getIdentity(identifier)
