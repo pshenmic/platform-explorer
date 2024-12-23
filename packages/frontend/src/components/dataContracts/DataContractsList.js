c636632716ad28fcaef8d4c6fd869b628074f0cb
Alexey <andreewsky.alexey@gmail.com>
2024-12-23 23:17:31 +0300

    Add pagination and loading state to DataContractsList

--- packages/frontend/src/components/dataContracts/DataContractsList.js
+++ packages/frontend/src/components/dataContracts/DataContractsList.js
@@ -1,16 +1,34 @@
 import DataContractsListItem from './DataContractsListItem'
 import { EmptyListMessage } from '../ui/lists'
+import Pagination from '../pagination'
+import { ErrorMessageBlock } from '../Errors'
+import { LoadingList } from '../loading'
 import './DataContractsList.scss'
 
-function DataContractsList ({ dataContracts = [] }) {
+function DataContractsList ({ dataContracts = [], pagination, loading, itemsCount = 10 }) {
   return (
     <div className={'DataContractsList'}>
-      {dataContracts.map((dataContract, key) =>
-        <DataContractsListItem dataContract={dataContract} key={key}/>
-      )}
+      {!loading
+        ? <div className={'DataContractsList__Items'}>
+            {dataContracts?.map((dataContract, key) =>
+              <DataContractsListItem dataContract={dataContract} key={key}/>
+            )}
+            {dataContracts?.length === 0 &&
+              <EmptyListMessage>There are no data contracts created yet.</EmptyListMessage>
+            }
+            {dataContracts === undefined && <ErrorMessageBlock/>}
+          </div>
+        : <LoadingList itemsCount={itemsCount}/>
+      }
 
-      {dataContracts.length === 0 &&
-        <EmptyListMessage>There are no data contracts created yet.</EmptyListMessage>
+      {pagination &&
+        <Pagination
+          className={'DataContractsList__Pagination'}
+          onPageChange={pagination.onPageChange}
+          pageCount={pagination.pageCount}
+          forcePage={pagination.forcePage}
+          justify={true}
+        />
       }
     </div>
   )
