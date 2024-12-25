8d9b2e9db34527f3746e18c2654373f2f441f093
Alexey <andreewsky.alexey@gmail.com>
2024-12-25 16:19:50 +0300

    Update VoteChoice styles

--- packages/frontend/src/components/data/VoteChoice.js
+++ packages/frontend/src/components/data/VoteChoice.js
@@ -1,26 +1,45 @@
 import ValueContainer from '../ui/containers/ValueContainer'
 import Identifier from './Identifier'
+import { ValueCard } from '../cards'
+import './VoteChoice.scss'
+
+function transformTypeString (input) {
+  if (typeof input !== 'string') return null
+  return input?.trim()?.replace(/ /g, '_')?.toUpperCase()
+}
 
 function VoteChoice ({ choiceStr }) {
   if (typeof choiceStr !== 'string') return 'n/a'
-
   const [choice, parameter] = choiceStr.split(/[()]/)
+  const type = transformTypeString(choice)
+  const colorScheme = {
+    TOWARDSIDENTITY: 'green',
+    LOCK: 'red',
+    ABSTAIN: 'orange'
+  }
 
   if (parameter) {
     return (
-      <ValueContainer>
-        <span>{choice}</span>
-        <ValueContainer>
+      <ValueContainer
+        className={'VoteChoice VoteChoice--TowardsIdentity'}
+        colorScheme={colorScheme?.[type] || 'grey'}
+      >
+        <span>{choice}:</span>
+        <ValueCard className={'VoteChoice__Parameter'} colorScheme={colorScheme?.[type] || 'grey'}>
           <Identifier avatar={true} copyButton={true} ellipsis={true} styles={['highlight-both']}>
             {parameter}
           </Identifier>
-        </ValueContainer>
+        </ValueCard>
       </ValueContainer>
     )
   }
 
   return (
-    <ValueContainer>
+    <ValueContainer
+      className={'VoteChoice'}
+      colorScheme={colorScheme?.[type] || 'grey'}
+      size={'sm'}
+    >
       {choice}
     </ValueContainer>
   )
