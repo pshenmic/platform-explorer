92ef2b568a3b651fe5c0faaa03e8715b7066de97
Alexey <andreewsky.alexey@gmail.com>
2024-12-26 15:23:20 +0300

    Update BackButton click

--- packages/frontend/src/components/ui/Buttons/BackButton.js
+++ packages/frontend/src/components/ui/Buttons/BackButton.js
@@ -1,14 +1,11 @@
 'use client'
 
-import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 
 function BackButton ({ link, children, ...props }) {
   const router = useRouter()
 
-  return link
-    ? <Link href={link} {...props}>{children}</Link>
-    : <button onClick={() => router.back()} {...props}>{children}</button>
+  return <button onClick={() => router.back()} {...props}>{children}</button>
 }
 
 export default BackButton
