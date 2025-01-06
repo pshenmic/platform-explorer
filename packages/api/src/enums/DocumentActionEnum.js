46b75f74a327821f2e367d26bc8c20354606c3e1
owl352 <kokosinca123@gmail.com>
2025-01-06 16:00:17 +0300

    enum fix

--- packages/api/src/enums/DocumentActionEnum.js
+++ packages/api/src/enums/DocumentActionEnum.js
@@ -3,14 +3,14 @@ const DocumentActionEnum = {
   1: 'Replace',
   2: 'Delete',
   3: 'Transfer',
-  4: 'Update price',
-  5: 'Purchase',
+  4: 'Purchase',
+  5: 'Update price',
   Create: 0,
   Replace: 1,
   Delete: 2,
   Transfer: 3,
-  UpdatePrice: 4,
-  Purchase: 5
+  Purchase: 4,
+  UpdatePrice: 5,
 }
 
 module.exports = DocumentActionEnum
