bca8f8e928497644e1673d58cf17ad527e3914ff
owl352 <kokosinca123@gmail.com>
2025-01-14 01:02:40 +0300

    some filters

--- packages/api/src/controllers/MasternodeVotesController.js
+++ packages/api/src/controllers/MasternodeVotesController.js
@@ -7,9 +7,13 @@ class MasternodeVotesController {
   }
 
   getMasternodeVotes = async (request, response) => {
-    const { page = 1, limit = 10, order = 'asc' } = request.query
+    const {
+      page = 1, limit = 10,
+      order = 'asc', timestamp_start: timestampStart, timestamp_end: timestampEnd,
+      voter_identity: voterIdentity, towards_identity: towardsIdentity, choice, power
+    } = request.query
 
-    const masternodeVotes = await this.masternodeVotesDAO.getMasternodeVotes(Number(page ?? 1), Number(limit ?? 10), order)
+    const masternodeVotes = await this.masternodeVotesDAO.getMasternodeVotes(timestampStart, timestampEnd, voterIdentity, towardsIdentity, choice, power, Number(page ?? 1), Number(limit ?? 10), order)
 
     response.send(masternodeVotes)
   }
--- packages/api/src/dao/MasternodeVotesDAO.js
+++ packages/api/src/dao/MasternodeVotesDAO.js
@@ -6,21 +6,58 @@ module.exports = class MasternodeVotesDAO {
     this.knex = knex
   }
 
-  getMasternodeVotes = async (page, limit, order) => {
+  getMasternodeVotes = async (timestampStart, timestampEnd, voterIdentity, towardsIdentity, choice, power, page, limit, order) => {
     const fromRank = ((page - 1) * limit) + 1
     const toRank = fromRank + limit - 1
 
+    const timestampFilter = timestampStart && timestampEnd
+      ? [
+          'blocks.timestamp BETWEEN ? AND ?',
+          [timestampStart, timestampEnd]
+        ]
+      : ['true']
+
+    const voterIdentityFilter = voterIdentity
+      ? [
+          'state_transitions.owner = ?',
+          [voterIdentity]
+        ]
+      : ['true']
+
+    const towardsIdentityFilter = towardsIdentity
+      ? [
+          'towards_identity_identifier = ?',
+          [voterIdentity]
+        ]
+      : ['true']
+
+    const choiceFilter = choice
+      ? [
+          'towards_identity_identifier = ?',
+          [voterIdentity]
+        ]
+      : ['true']
+
+    // TODO: Implement Power filter
+
     const subquery = this.knex('masternode_votes')
       .select('id', 'pro_tx_hash', 'state_transition_hash', 'voter_identity_id', 'choice',
-        'towards_identity_identifier', 'data_contract_id', 'document_type_name', 'index_name', 'index_values')
+        'towards_identity_identifier', 'data_contract_id', 'document_type_name', 'index_name', 'index_values',
+        'data_contracts.identifier as data_contract_identifier', 'blocks.timestamp as timestamp')
       .select(this.knex.raw(`rank() over (order by id ${order}) rank`))
       .select(this.knex('masternode_votes').count('id').as('total_count'))
+      .whereRaw(...timestampFilter)
+      .whereRaw(...voterIdentityFilter)
+      .whereRaw(...towardsIdentityFilter)
+      .whereRaw(...choiceFilter)
+      .leftJoin('state_transitions', 'state_transition_hash', 'state_transitions.hash')
+      .leftJoin('blocks', 'blocks.hash', 'state_transitions.block_hash')
       .as('subquery')
 
     const rows = await this.knex(subquery)
-      .select('pro_tx_hash', 'subquery.state_transition_hash as state_transition_hash', 'voter_identity_id', 'choice',
-        'blocks.timestamp as timestamp', 'towards_identity_identifier', 'total_count',
-        'data_contracts.identifier as data_contract_identifier', 'document_type_name', 'index_name', 'index_values')
+      .select('pro_tx_hash', 'subquery.state_transition_hash as state_transition_hash', 'choice',
+        'timestamp', 'towards_identity_identifier', 'total_count', 'voter_identity_id',
+        'data_contract_identifier', 'document_type_name', 'index_name', 'index_values')
       .leftJoin('state_transitions', 'state_transition_hash', 'state_transitions.hash')
       .leftJoin('blocks', 'blocks.hash', 'state_transitions.block_hash')
       .leftJoin('data_contracts', 'data_contract_id', 'data_contracts.id')
