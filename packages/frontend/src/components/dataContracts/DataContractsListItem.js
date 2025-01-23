cb051531ed00aae0876bc4d138cb6480c0747011
Alexey <andreewsky.alexey@gmail.com>
2025-01-23 22:23:46 +0300

    remove debug comment

--- packages/frontend/src/components/dataContracts/DataContractsListItem.js
+++ packages/frontend/src/components/dataContracts/DataContractsListItem.js
@@ -43,37 +43,6 @@ function DataContractsListItem ({ dataContract }) {
       </Grid>
     </Link>
   )
-
-  // return (
-  //   <Link
-  //     href={`/dataContract/${dataContract?.identifier}`}
-  //     className={'DataContractsListItem'}
-  //   >
-  //     <div className={'DataContractsListItem__IdentifierContainer'}>
-  //       {dataContract?.name
-  //         ? <>
-  //             <ImageGenerator className={'DataContractsListItem__Avatar'} username={dataContract?.identifier} lightness={50} saturation={50} width={24} height={24}/>
-  //             <Alias>{dataContract.name}</Alias>
-  //           </>
-  //         : <Identifier
-  //           className={'DataContractsListItem__Identifier'}
-  //           avatar={true}
-  //           styles={['highlight-both']}
-  //           ellipsis={false}
-  //         >
-  //           {dataContract.identifier}
-  //         </Identifier>}
-  //     </div>
-  //
-  //     {dataContract?.isSystem && <div className={'DataContractsListItem__SystemLabel'}>SYSTEM</div>}
-  //
-  //     {(typeof dataContract?.timestamp === 'string') &&
-  //       <div className={'DataContractsListItem__Timestamp'}>
-  //         <DateBlock timestamp={dataContract?.timestamp} format={'dateOnly'} showTime={true}/>
-  //       </div>
-  //     }
-  //   </Link>
-  // )
 }
 
 export default DataContractsListItem
