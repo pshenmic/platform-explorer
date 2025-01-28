b6221d6baf51e0f4fb98cf33e8c103a740098f6a
Alexey <andreewsky.alexey@gmail.com>
2025-01-28 17:59:20 +0300

    Create DataContractDigestCard component

--- packages/frontend/src/components/dataContracts/index.js
+++ packages/frontend/src/components/dataContracts/index.js
@@ -2,10 +2,12 @@ import DataContractsList from './DataContractsList'
 import { DataContractCards } from './Cards'
 import InternalConfigCard from './InternalConfigCard'
 import DataContractTotalCard from './DataContractTotalCard'
+import DataContractDigestCard from './DataContractDigestCard'
 
 export {
   DataContractsList,
   DataContractCards,
   InternalConfigCard,
-  DataContractTotalCard
+  DataContractTotalCard,
+  DataContractDigestCard
 }
