04089f6139d2b0fd004357c3e09c269f2be50ea2
Alexey <andreewsky.alexey@gmail.com>
2025-02-03 22:25:24 +0300

    Update DocumentTotalCard responsive styles

--- packages/frontend/src/components/data/Identifier.js
+++ packages/frontend/src/components/data/Identifier.js
@@ -11,9 +11,13 @@ export default function Identifier ({ children, ellipsis = true, avatar, styles 
     return null
   })()
 
+  console.log('styles', styles)
+
   const sizeClass = (() => {
     if (styles.includes('size-32')) return 'Identifier--Size32'
+    if (styles.includes('size-43')) return 'Identifier--Size44'
     if (styles.includes('size-44')) return 'Identifier--Size44'
+    if (styles.includes('size-64')) return 'Identifier--Size64'
     return ''
   })()
 
