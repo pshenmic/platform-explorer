267290df77f5d26bbd77b167265d4e64f4dac69f
Alexey <andreewsky.alexey@gmail.com>
2025-02-18 20:06:28 +0300

    Remove debug comments

--- packages/frontend/src/app/block/[hash]/Block.js
+++ packages/frontend/src/app/block/[hash]/Block.js
@@ -3,27 +3,19 @@
 import { useState, useEffect } from 'react'
 import * as Api from '../../../util/Api'
 import TransactionsList from '../../../components/transactions/TransactionsList'
-// import {LoadingBlock, LoadingLine} from '../../../components/loading'
 import { ErrorMessageBlock } from '../../../components/Errors'
-// import { fetchHandlerSuccess, fetchHandlerError, paginationHandler } from '../../../util'
 import { fetchHandlerSuccess, fetchHandlerError } from '../../../util'
 import { InfoContainer, PageDataContainer } from '../../../components/ui/containers'
-// import {DocumentsRevisionsList, DocumentTotalCard} from '../../../components/documents'
 import {
   Container,
-  // TableContainer, Table, Thead, Tbody, Tr, Th, Td,
-  // Heading, Tabs, TabList, Tab, TabPanels, TabPanel
   Tabs, TabList, Tab, TabPanels, TabPanel
 } from '@chakra-ui/react'
 import { BlockDigestCard, BlockTotalCard, QuorumMembersList } from '../../../components/blocks'
-// import {CodeBlock} from "../../../components/data";
 import './Block.scss'
 
 function Block ({ hash }) {
   const [block, setBlock] = useState({ data: {}, loading: true, error: false })
   const [rate, setRate] = useState({ data: {}, loading: true, error: false })
-  const [transactions] = useState({ data: {}, props: { currentPage: 0 }, loading: true, error: false })
-  // const tdTitleWidth = 250
 
   const fetchData = () => {
     setBlock(state => ({ ...state, loading: true }))
@@ -50,7 +42,7 @@ function Block ({ hash }) {
       </div>
 
       <InfoContainer styles={['tabs']}>
-        <Tabs index={1}>
+        <Tabs>
           <TabList>
             <Tab>Transactions {block.data?.txs?.length !== undefined
               ? <span
@@ -69,24 +61,22 @@ function Block ({ hash }) {
           </TabList>
           <TabPanels>
             <TabPanel position={'relative'}>
-              {!transactions.error
+              {!block.error
                 ? <TransactionsList
-                  transactions={block.data?.txs}
-                  rate={rate.data}
-                  // pagination={{
-                  //   onPageChange: pagination => paginationHandler(setRevisions, pagination.selected),
-                  //   pageCount: Math.ceil(transactions.data?.pagination?.total / pageSize) || 1,
-                  //   forcePage: transactions.props.currentPage
-                  // }}
-                />
+                    transactions={block.data?.txs}
+                    rate={rate.data}
+                  />
                 : <Container h={20}><ErrorMessageBlock/></Container>
               }
             </TabPanel>
             <TabPanel position={'relative'}>
-              <QuorumMembersList
-                members={block?.data?.quorum?.members}
-                loading={block.loading}
-              />
+              {!block.error
+                ? <QuorumMembersList
+                    members={block?.data?.quorum?.members}
+                    loading={block.loading}
+                  />
+                : <Container h={20}><ErrorMessageBlock/></Container>
+              }
             </TabPanel>
           </TabPanels>
         </Tabs>
@@ -96,98 +86,3 @@ function Block ({ hash }) {
 }
 
 export default Block
-
-// <Container
-// maxW={'container.xl'}
-// color={'white'}
-//   >
-//   <TableContainer
-// maxW={'none'}
-// borderWidth={'1px'}
-// borderRadius={'block'}
-//   >
-//   {!block.error
-//   ? <Table variant={'simple'}>
-//     <Thead>
-//       <Tr>
-//         <Th><div className={'Table__Title'}>Block info</div></Th>
-//         <Th></Th>
-//       </Tr>
-//     </Thead>
-//     <Tbody>
-//       <Tr>
-//         <Td w={tdTitleWidth}>Hash</Td>
-//         <Td className={'Table__Cell--BreakWord Table__Cell--Mono'}>
-//           <LoadingLine loading={block.loading}>
-//             {block.data?.header?.hash}
-//           </LoadingLine>
-//         </Td>
-//       </Tr>
-//       <Tr>
-//         <Td w={tdTitleWidth}>Height</Td>
-//         <Td>
-//           <LoadingLine loading={block.loading}>{block.data?.header?.height}</LoadingLine>
-//         </Td>
-//       </Tr>
-//       <Tr>
-//         <Td w={tdTitleWidth}>Timestamp</Td>
-//         <Td>
-//           <LoadingLine loading={block.loading}>{new Date(block.data?.header?.timestamp).toLocaleString()}</LoadingLine>
-//         </Td>
-//       </Tr>
-//       <Tr>
-//         <Td w={tdTitleWidth}>Block Version</Td>
-//         <Td>
-//           <LoadingLine loading={block.loading}>{block.data?.header?.blockVersion}</LoadingLine>
-//         </Td>
-//       </Tr>
-//       <Tr>
-//         <Td w={tdTitleWidth}>App Version</Td>
-//         <Td>
-//           <LoadingLine loading={block.loading}>{block.data?.header?.appVersion}</LoadingLine>
-//         </Td>
-//       </Tr>
-//       <Tr>
-//         <Td w={tdTitleWidth}>L1 Locked Height</Td>
-//         <Td>
-//           <LoadingLine loading={block.loading}>{block.data?.header?.l1LockedHeight}</LoadingLine>
-//         </Td>
-//       </Tr>
-//       <Tr>
-//         <Td w={tdTitleWidth}>Transactions count</Td>
-//         <Td>
-//           <LoadingLine loading={block.loading}>{block.data?.txs?.length}</LoadingLine>
-//         </Td>
-//       </Tr>
-//       <Tr>
-//         <Td w={tdTitleWidth}>Validator</Td>
-//         <Td className={'Table__Cell--BreakWord Table__Cell--Mono'}>
-//           <LoadingLine loading={block.loading}>
-//             {block.data?.header?.validator}
-//           </LoadingLine>
-//         </Td>
-//       </Tr>
-//     </Tbody>
-//   </Table>
-//   : <Container h={20}><ErrorMessageBlock/></Container>}
-// </TableContainer>
-// </Container>
-
-// {block.data?.txs?.length
-//   ? <Container
-//     width={'100%'}
-//     maxW={'none'}
-//     mt={5}
-//     className={'InfoBlock'}
-//   >
-//     <Heading className={'InfoBlock__Title'} as={'h1'}>Transactions</Heading>
-//     <TransactionsList
-//       transactions={block.data?.txs} rate={rate.data}
-//       // pagination={{
-//       //   onPageChange: pagination => paginationHandler(setRevisions, pagination.selected),
-//       //   pageCount: Math.ceil(transactions.data?.pagination?.total / pageSize) || 1,
-//       //   forcePage: transactions.props.currentPage
-//       // }}
-//     />
-//   </Container>
-//   : null}
