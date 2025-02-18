5828f97b81bdaa767e30926d8772bc73e91450d2
Alexey <andreewsky.alexey@gmail.com>
2025-02-18 19:31:53 +0300

    Update QuorumMembersList responsive styles

--- packages/frontend/src/components/blocks/quorum/QuorumMembersListItem.js
+++ packages/frontend/src/components/blocks/quorum/QuorumMembersListItem.js
@@ -1,37 +1,42 @@
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Badge, Grid, GridItem } from '@chakra-ui/react'
 import { Identifier, IpAddress } from '../../data'
+import Link from 'next/link'
 import './QuorumMembersListItem.scss'
 
 function QuorumMembersListItem ({ member }) {
   return (
-    <div className={'QuorumListItem'}>
+    <Link href={`/validator/${member?.proTxHash}`} className={'QuorumListItem'}>
       <Grid className={'QuorumListItem__Content'}>
         <GridItem className={'QuorumListItem__Column QuorumListItem__Column--ProtxHash'}>
           <Identifier
-            styles={['highlight-both']}
-            copyButton={true}
-            ellipsis={false}
+            styles={['highlight-both', 'size-64']}
+            ellipsis={true}
+            avatar={true}
           >
             {member?.proTxHash}
           </Identifier>
         </GridItem>
         <GridItem className={'QuorumListItem__Column QuorumListItem__Column--Service'}>
-          <IpAddress>{member?.service}</IpAddress>
+          <IpAddress variant={'dim'} clickable={false}>{member?.service}</IpAddress>
         </GridItem>
         <GridItem className={'QuorumListItem__Column QuorumListItem__Column--OperatorPubKey'}>
           <Identifier
-            styles={['highlight-both']}
-            copyButton={true}
+            styles={['highlight-both', 'size-96']}
             ellipsis={false}
+            copyButton={true}
           >
             {member?.pubKeyOperator}
           </Identifier>
         </GridItem>
         <GridItem className={'QuorumListItem__Column QuorumListItem__Column--Valid'}>
-          {member?.valid ? 'Yes' : 'No'}
+          {typeof member?.valid === 'boolean' &&
+            <Badge colorScheme={member?.valid ? 'green' : 'red'}>
+              {member?.valid ? 'Valid' : 'No'}
+            </Badge>
+          }
         </GridItem>
       </Grid>
-    </div>
+    </Link>
   )
 }
 
