c34edcb6009a3d9bdb1fab8ad121650384d9e899
Alexey <andreewsky.alexey@gmail.com>
2025-02-20 00:35:27 +0300

    Update popover theme styles

--- packages/frontend/src/styles/components/Popover.js
+++ packages/frontend/src/styles/components/Popover.js
@@ -1,5 +1,20 @@
 // import { badgeColors } from '../colors'
 
+//&__Content {
+//  background-color: #3E4A4F !important;
+//  border-radius: 10px !important;
+//  border: 4px solid !important;
+//  border-top-color: #72787B4D !important;
+//  border-bottom-color: #181F23 !important;
+//  border-left: var(--chakra-borders-none) !important;
+//  border-right: var(--chakra-borders-none) !important;
+//  padding: 1rem 1.25rem !important;
+//}
+//
+//&__Body, &__Header {
+//  padding: 0 !important;
+//}
+
 const Popover = {
   baseStyle: props => {
     // const { colorScheme } = props
@@ -9,14 +24,27 @@ const Popover = {
         // bg: 'green',
         p: 0,
         borderBottom: 0,
-        textAlign: 'left'
+        textAlign: 'left',
+        fontFamily: 'heading',
+        fontWeight: 700,
+        fontSize: '0.75rem'
       },
       body: {
+        fontFamily: 'mono',
         // bg: 'green',
-        p: 0
+        p: 0,
+        // fontSize: '0.688rem'
       },
       content: {
+        gap: '0.5rem',
         p: '1rem 1.25rem',
+        bg: '#3E4A4F',
+        borderRadius: '10px',
+        border: '4px solid',
+        borderTopColor: '#72787B4D',
+        borderBottomColor: '#181F23',
+        borderLeft: 'none',
+        borderRight: 'none',
         _focusVisible: {
           boxShadow: 'none'
         }
