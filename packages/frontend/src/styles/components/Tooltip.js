083b09cc29f9182a4f2c025a59d8b251d08ea92b
Alexey <andreewsky.alexey@gmail.com>
2025-02-20 18:36:06 +0300

    Update tooltip styles

--- packages/frontend/src/styles/components/Tooltip.js
+++ packages/frontend/src/styles/components/Tooltip.js
@@ -1,11 +1,11 @@
 import { flyoutStyles } from './flyoutStyles'
 
 const Tooltip = {
-  baseStyle: () => ({
+  baseStyle: {
     ...flyoutStyles.content,
     fontSize: '0.75rem',
     fontWeight: 500
-  })
+  }
 }
 
 export default Tooltip
