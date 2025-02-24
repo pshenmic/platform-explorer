9dbedc69c3e209acdf1b3ae67dd89056ac4c8956
Alexey <andreewsky.alexey@gmail.com>
2025-02-24 18:21:33 +0300

    Remove unused QuorumPopover tooltip

--- packages/frontend/src/components/ui/Tooltips/index.js
+++ packages/frontend/src/components/ui/Tooltips/index.js
@@ -1,13 +1,11 @@
 import RateTooltip from './RateTooltip'
 import Tooltip from './Tooltip'
 import EpochTooltip from './EpochTooltip'
-import QuorumPopover from './QuorumPopover'
 import Popover from './Popover'
 
 export {
   Tooltip,
   RateTooltip,
   EpochTooltip,
-  QuorumPopover,
   Popover
 }
