1641bb9bfc0a5d3338f84255aeb12bd1c1c1d473
Alexey <andreewsky.alexey@gmail.com>
2025-03-04 17:39:46 +0300

    Update badge size props

--- packages/frontend/src/styles/components/Badge.js
+++ packages/frontend/src/styles/components/Badge.js
@@ -1,13 +1,8 @@
 import { badgeColors } from '../colors'
 
-const lineHeight = {
-  xs: '18px'
-}
-
 const Badge = {
   baseStyle: props => {
     const { colorScheme } = props
-    const { size } = props
 
     return {
       borderWidth: '0px',
@@ -19,11 +14,16 @@ const Badge = {
       textTransform: 'none',
       display: 'inline-flex',
       alignItems: 'center',
-      lineHeight: lineHeight?.[size] || '20px',
+      lineHeight: '20px',
       bg: badgeColors[colorScheme].bg || 'gray.500',
       color: badgeColors[colorScheme].text || 'white'
     }
-  }
+  },
+  sizes: {
+    xs: {
+      lineHeight: '18px'
+    }
+  },
 }
 
 export default Badge
