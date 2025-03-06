6ab00d13b2ce638d621c12561fe4196b81e31c6b
Alexey <andreewsky.alexey@gmail.com>
2025-03-06 19:39:57 +0300

    Implement auto request to GlobalSearchInput

--- packages/frontend/src/components/search/GlobalSearchInput.js
+++ packages/frontend/src/components/search/GlobalSearchInput.js
@@ -1,96 +1,36 @@
 import { useState, useEffect } from 'react'
-import ModalWindow from '../modalWindow'
 import * as Api from '../../util/Api'
 import { Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
 import { SearchIcon } from '@chakra-ui/icons'
-import {
-  ResponseErrorNotFound,
-  ResponseErrorTimeout,
-  ResponseErrorInternalServer
-} from '../../util/Errors'
 import './GlobalSearchInput.scss'
 import mockSearchResults from '../layout/navbar/mockSearchResults'
-
+import { useDebounce } from '../../hooks'
 import { fetchHandlerSuccess, fetchHandlerError } from '../../util'
 
 function GlobalSearchInput ({ onResultChange, forceValue, onChange }) {
-  const [showModal, setShowModal] = useState(false)
-  const [modalText, setModalText] = useState('false')
   const [searchQuery, setSearchQuery] = useState('')
+  const debouncedQuery = useDebounce(searchQuery, 200)
+
+  const search = () => {
+    onResultChange({ data: {}, loading: true, error: false })
+
+    Api.search(searchQuery)
+      .then(res => fetchHandlerSuccess(onResultChange, mockSearchResults))
+      .catch(err => fetchHandlerError(onResultChange, err))
+  }
 
   useEffect(() => {
-    let timer
-    if (showModal) {
-      timer = setTimeout(() => {
-        setShowModal(false)
-        setModalText('')
-      }, 6000)
+    if (!debouncedQuery.length) {
+      onResultChange({})
+      return
     }
-    return () => clearTimeout(timer)
-  }, [showModal])
+
+    search()
+  }, [debouncedQuery])
 
   useEffect(() => {
     setSearchQuery(forceValue)
   }, [forceValue])
-
-  const showModalWindow = (text, timeout) => {
-    setShowModal(true)
-    setModalText(text)
-
-    if (timeout) {
-      setTimeout(() => {
-        setShowModal(false)
-        setModalText('')
-      }, timeout)
-    }
-  }
-
-  const search = async () => {
-    try {
-      onResultChange({ data: {}, loading: true, error: false })
-
-      Api.search(searchQuery)
-        .then(res => fetchHandlerSuccess(onResultChange, mockSearchResults))
-        .catch(err => fetchHandlerError(onResultChange, err))
-
-      // const searchResult = await Api.search(searchQuery)
-
-      // fetchHandlerSuccess(onResultChange, mockSearchResults)
-
-      // const searchTypeMap = {
-      //   block: `/block/${searchResult?.block?.header?.hash}`,
-      //   transaction: `/transaction/${searchResult?.transaction?.hash}`,
-      //   dataContract: `/dataContract/${searchResult?.dataContract?.identifier}`,
-      //   dataContracts: `/dataContract/${searchResult?.dataContracts?.[0].identifier}`,
-      //   document: `/document/${searchResult?.document?.identifier}`,
-      //   identity: `/identity/${searchResult?.identity?.identifier}`,
-      //   identities: `/identity/${searchResult?.identities?.[0].identifier}`,
-      //   validator: `/validator/${searchResult?.validator?.proTxHash}`
-      // }
-
-      // for (const key in searchTypeMap) {
-      //   if (searchResult[key]) {
-      //     searchRedirect(searchTypeMap[key])
-      //     return
-      //   }
-      // }
-
-      // showModalWindow('Not found', 6000)
-    } catch (e) {
-      console.error(e)
-
-      const errorMessage = (() => {
-        if (e instanceof ResponseErrorNotFound ||
-            e instanceof ResponseErrorTimeout ||
-            e instanceof ResponseErrorInternalServer) {
-          return e.message
-        }
-        return 'Request error'
-      })()
-
-      showModalWindow(errorMessage)
-    }
-  }
 
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
@@ -106,17 +46,17 @@ function GlobalSearchInput ({ onResultChange, forceValue, onChange }) {
   return (
     <div>
       <InputGroup size={'md'} className={'GlobalSearchInput'}>
-          <Input
-            pr={'4.5rem'}
-            value={searchQuery}
-            type={'text'}
-            placeholder={'Search...'}
-            onChange={handleSearchInput}
-            onKeyPress={handleKeyPress}
-            color={'gray.250'}
-            fontSize={'12px'}
-            className={'GlobalSearchInput__Field'}
-          />
+        <Input
+          pr={'4.5rem'}
+          value={searchQuery}
+          type={'text'}
+          placeholder={'Search...'}
+          onChange={handleSearchInput}
+          onKeyPress={handleKeyPress}
+          color={'gray.250'}
+          fontSize={'12px'}
+          className={'GlobalSearchInput__Field'}
+        />
           <InputRightElement>
             <Button
               h={'28px'}
@@ -128,14 +68,8 @@ function GlobalSearchInput ({ onResultChange, forceValue, onChange }) {
             >
               <SearchIcon color={'whiteAlpha.900'}/>
             </Button>
-        </InputRightElement>
+          </InputRightElement>
       </InputGroup>
-
-      <ModalWindow
-        open={showModal}
-        text={modalText}
-        setShowModal={setShowModal}
-      />
     </div>
   )
 }
