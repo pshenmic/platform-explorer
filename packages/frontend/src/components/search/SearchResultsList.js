dbfb1e6f2ae668be99a8fdd549b4c658494f84dd
Alexey <andreewsky.alexey@gmail.com>
2025-03-11 23:40:39 +0300

    Update fields titles in SearchResultsList

--- packages/frontend/src/components/search/SearchResultsList.js
+++ packages/frontend/src/components/search/SearchResultsList.js
@@ -1,24 +1,28 @@
 import SearchResultsListItem from './SearchResultsListItem'
 import './SearchResultsList.scss'
 import { Grid, GridItem } from '@chakra-ui/react'
-import { CATEGORY_MAP } from './constants'
+import { CATEGORY_MAP, SINGULAR_CATEGORY_NAMES, PLURAL_CATEGORY_NAMES, MODIFIER_MAP } from './constants'
 
 const COLUMN_TITLES = {
-  [CATEGORY_MAP.validators]: ['Validator', 'Identity', 'Balance'],
-  [CATEGORY_MAP.identities]: ['Identity', 'Status', 'Time'],
-  [CATEGORY_MAP.dataContracts]: ['Data Contract', 'Identity', 'Time'],
-  [CATEGORY_MAP.blocks]: ['Block', 'Epoch', 'Time'],
-  [CATEGORY_MAP.documents]: ['Document', 'Identity', 'Time'],
-  [CATEGORY_MAP.transactions]: ['Transaction', 'Status', 'Time']
+  [CATEGORY_MAP.validators]: ['Identity', 'Balance'],
+  [CATEGORY_MAP.identities]: ['Status', 'Time'],
+  [CATEGORY_MAP.dataContracts]: ['Owner', 'Time'],
+  [CATEGORY_MAP.blocks]: ['Epoch', 'Time'],
+  [CATEGORY_MAP.documents]: ['Identity', 'Time'],
+  [CATEGORY_MAP.transactions]: ['Status', 'Time']
 }
 
 function ListCategory ({ type, data }) {
   const titles = COLUMN_TITLES[CATEGORY_MAP[type]]
+
   if (!titles) return null
 
   return (
     <div className={'SearchResultsList__Category'}>
-      <Grid className={'SearchResultsList__ColumnTitles'}>
+      <Grid className={`SearchResultsList__ColumnTitles SearchResultsList__ColumnTitles--${MODIFIER_MAP[CATEGORY_MAP[type]] || ''}`}>
+        <GridItem className={'SearchResultsList__ColumnTitle'}>
+          {data?.length} {data?.length > 1 ? PLURAL_CATEGORY_NAMES[type] : SINGULAR_CATEGORY_NAMES[CATEGORY_MAP[type]]} FOUND
+        </GridItem>
         {titles.map((title, i) => (
           <GridItem key={i} className={'SearchResultsList__ColumnTitle'}>
             {title}
