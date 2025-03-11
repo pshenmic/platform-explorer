0c92071e8f8ef98490a05b4cec24d7d0d5b61bb9
Alexey <andreewsky.alexey@gmail.com>
2025-03-11 23:41:21 +0300

    Update IdentitySearchItem

--- packages/frontend/src/components/search/items/IdentitySearchItem.js
+++ packages/frontend/src/components/search/items/IdentitySearchItem.js
@@ -1,5 +1,5 @@
 import { Badge } from '@chakra-ui/react'
-import { Alias, Identifier, TimeDelta } from '../../data'
+import { Alias, Identifier, TimeDelta, NotActive } from '../../data'
 import { BaseSearchItem, BaseSearchItemContent } from './BaseSearchItem'
 
 const STATUS_COLORS = {
@@ -12,7 +12,7 @@ export function IdentitySearchItem ({ identity, className }) {
   return (
     <BaseSearchItem
       href={`/identity/${identity?.identifier}`}
-      className={className}
+      className={`SearchResultsListItem--Identity ${className || ''}`}
     >
       <BaseSearchItemContent
         mainContent={
@@ -21,11 +21,13 @@ export function IdentitySearchItem ({ identity, className }) {
             : <Identifier avatar={true} ellipsis={true} styles={['highlight-both']}>{identity?.identifier}</Identifier>
         }
         additionalContent={
-          <Badge size={'xs'} colorScheme={STATUS_COLORS[identity?.status?.status] || 'gray'}>
-            {identity?.status?.status}
-          </Badge>
+          identity?.status?.status ?
+            <Badge size={'xs'} colorScheme={STATUS_COLORS[identity?.status?.status] || 'gray'}>
+              {identity?.status?.status}
+            </Badge>
+            : <NotActive>-</NotActive>
         }
-        timestamp={<TimeDelta endDate={new Date(identity.timestamp)}/>}
+        timestamp={<TimeDelta endDate={identity?.status?.timestamp ? new Date(identity?.status?.timestamp) : new Date(identity?.timestamp)}/>}
       />
     </BaseSearchItem>
   )
