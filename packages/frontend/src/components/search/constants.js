e4538953288dc11e903bad878a34818b96bb08c0
Alexey <andreewsky.alexey@gmail.com>
2025-03-11 23:40:59 +0300

    Update constants for SearchResultsList

--- packages/frontend/src/components/search/constants.js
+++ packages/frontend/src/components/search/constants.js
@@ -16,3 +16,30 @@ export const CATEGORY_MAP = {
   blocks: ENTITY_TYPES.BLOCK,
   validators: ENTITY_TYPES.VALIDATOR
 }
+
+export const SINGULAR_CATEGORY_NAMES = {
+  [ENTITY_TYPES.TRANSACTION]: 'Transaction',
+  [ENTITY_TYPES.DATA_CONTRACT]: 'Data Contract',
+  [ENTITY_TYPES.DOCUMENT]: 'Document',
+  [ENTITY_TYPES.IDENTITY]: 'Identity',
+  [ENTITY_TYPES.BLOCK]: 'Block',
+  [ENTITY_TYPES.VALIDATOR]: 'Validator'
+}
+
+export const PLURAL_CATEGORY_NAMES = {
+  transactions: 'Transactions',
+  dataContracts: 'Data Contracts',
+  documents: 'Documents',
+  identities: 'Identities',
+  blocks: 'Blocks',
+  validators: 'Validators'
+}
+
+export const MODIFIER_MAP = {
+  [ENTITY_TYPES.TRANSACTION]: 'Transaction',
+  [ENTITY_TYPES.DATA_CONTRACT]: 'DataContract',
+  [ENTITY_TYPES.DOCUMENT]: 'Document',
+  [ENTITY_TYPES.IDENTITY]: 'Identity',
+  [ENTITY_TYPES.BLOCK]: 'Block',
+  [ENTITY_TYPES.VALIDATOR]: 'Validator'
+}
