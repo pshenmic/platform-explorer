c6910c5df9636b954fad252b2600e9f7c293adcf
Alexey <andreewsky.alexey@gmail.com>
2025-03-24 19:47:56 +0300

    Add 'menu' variant to popover styles

--- packages/frontend/src/styles/components/Popover.js
+++ packages/frontend/src/styles/components/Popover.js
@@ -14,6 +14,30 @@ const Popover = {
       ...flyoutStyles.content,
       fontSize: '0.688rem'
     }
+  },
+  variants: {
+    menu: {
+      content: {
+        bg: 'gray.800',
+        borderColor: 'transpatent',
+        border: 'none',
+        boxShadow: '0px 1.25rem 2rem rgba(0, 0, 0, 0.25)',
+        borderRadius: '1.25rem',
+        padding: '1.5rem',
+        _focus: {
+          boxShadow: '0px 1.25rem 2rem rgba(0, 0, 0, 0.25)'
+        }
+      },
+      body: {
+        p: 0,
+        borderRadius: '1.25rem',
+        overflow: 'hidden'
+      },
+      arrow: {
+        bg: 'gray.800',
+        borderColor: 'whiteAlpha.100'
+      }
+    }
   }
 }
 
