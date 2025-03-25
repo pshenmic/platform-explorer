e177034ae60ca58c4766ad7d2fb48dc21f3b8651
Alexey <andreewsky.alexey@gmail.com>
2025-03-26 00:54:18 +0300

    Update RangeFilter styles

--- packages/frontend/src/components/filters/RangeFilter.js
+++ packages/frontend/src/components/filters/RangeFilter.js
@@ -1,4 +1,5 @@
-import { Box, Text, SimpleGrid, Input } from '@chakra-ui/react'
+import { Input } from '@chakra-ui/react'
+import './RangeFilter.scss'
 
 export const RangeFilter = ({
   value = { min: '', max: '' },
@@ -9,36 +10,36 @@ export const RangeFilter = ({
   maxPlaceholder = 'Max',
   maxTitle = 'To'
 }) => (
-  <Box mb={6}>
-    <SimpleGrid columns={2} spacing={4}>
-      <Box>
-        <Text fontSize="xs" color="gray.600" mb={1}>
-          {minTitle}
-        </Text>
-        <Input
-          type={type}
-          value={value.min || ''}
-          onChange={(e) => onChange({
-            ...value,
-            min: e.target.value
-          })}
-          placeholder={minPlaceholder}
-        />
-      </Box>
-      <Box>
-        <Text fontSize="xs" color="gray.600" mb={1}>
-          {maxTitle}
-        </Text>
-        <Input
-          type={type}
-          value={value.max || ''}
-          onChange={(e) => onChange({
-            ...value,
-            max: e.target.value
-          })}
-          placeholder={maxPlaceholder}
-        />
-      </Box>
-    </SimpleGrid>
-  </Box>
+  <div className={'RangeFilter'}>
+    <div className={'RangeFilter__InputContainer'}>
+      <span className={'RangeFilter__InputTitle'} color="gray.600">
+        {minTitle}
+      </span>
+      <Input
+        className={'RangeFilter__Input'}
+        type={type}
+        value={value.min || ''}
+        onChange={(e) => onChange({
+          ...value,
+          min: e.target.value
+        })}
+        placeholder={minPlaceholder}
+      />
+    </div>
+    <div className={'RangeFilter__InputContainer'}>
+      <span className={'RangeFilter__InputTitle'} color="gray.600">
+        {maxTitle}
+      </span>
+      <Input
+        className={'RangeFilter__Input'}
+        type={type}
+        value={value.max || ''}
+        onChange={(e) => onChange({
+          ...value,
+          max: e.target.value
+        })}
+        placeholder={maxPlaceholder}
+      />
+    </div>
+  </div>
 )
