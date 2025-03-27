13c8fc90841e5332cef62b9284fe5e1527104196
Alexey <andreewsky.alexey@gmail.com>
2025-03-27 14:31:55 +0300

    Update MobileFilterMenu styles

--- packages/frontend/src/components/filters/MobileFilterMenu.js
+++ packages/frontend/src/components/filters/MobileFilterMenu.js
@@ -1,7 +1,8 @@
 import { useState, useEffect, useCallback } from 'react'
-import { Box, Button, Flex, Icon, Text, Fade } from '@chakra-ui/react'
-import { ChevronLeftIcon } from '@chakra-ui/icons'
+import { Box, Button, Flex, Text, Fade } from '@chakra-ui/react'
+import { ChevronIcon } from '../ui/icons'
 import { FilterValueTag } from './FilterValueTag'
+import { SubmitButton } from '../ui/forms'
 import './MobileFilterMenu.scss'
 
 export const MobileFilterMenu = ({
@@ -43,91 +44,78 @@ export const MobileFilterMenu = ({
   const currentActiveItem = activeItem ? getUpdatedMenuItem(activeItem) : null
 
   return (
-    <Box className="MobileFilterMenu" position="relative">
+    <div className={'MobileFilterMenu'}>
       {renderList && (
         <Fade in={!activeItem} unmountOnExit>
-          <Box className="MobileFilterMenu__List">
+          <div className={'MobileFilterMenu__Title'}>Add Filters</div>
+
+          {hasActiveFilters && (
+            <Button
+              variant={'outline'}
+              width={'48%'}
+              onClick={onReset}
+            >
+              Reset Filters
+            </Button>
+          )}
+
+          <div className={'MobileFilterMenu__List'}>
             {menuData.map((item, index) => (
-              <Flex
-                key={`menu-item-${index}-${item.label}`}
-                className="MobileFilterMenu__Item"
+              <div
+                className={'MobileFilterMenu__Item'}
                 onClick={() => selectMenuItem(item)}
-                justifyContent="space-between"
-                alignItems="center"
-                p={3}
-                borderBottom="1px solid"
-                borderColor="gray.200"
-                cursor="pointer"
+                key={index}
               >
-                <Text fontWeight="medium">{item.label}</Text>
+                <Flex justifyContent={'space-between'} alignItems={'center'}>
+                  <span className={'MobileFilterMenu__ItemTitle'}>{item.label}</span>
 
-                <Flex alignItems="center">
-                  {item.activeFilterValue && (
-                    <Box mr={2}>
-                      <FilterValueTag
-                        value={item.activeFilterValue}
-                        type={item.type}
-                        rawValue={item.rawValue}
-                        options={item.options}
-                        customRenderer={item.customRenderer}
-                      />
-                    </Box>
-                  )}
-                  <Icon
-                    viewBox="0 0 24 24"
-                    boxSize={5}
-                    color="gray.400"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"
-                    />
-                  </Icon>
+                  <ChevronIcon/>
                 </Flex>
-              </Flex>
+
+                {item.activeFilterValue && (
+                  <FilterValueTag
+                    value={item.activeFilterValue}
+                    type={item.type}
+                    rawValue={item.rawValue}
+                    options={item.options}
+                    customRenderer={item.customRenderer}
+                  />
+                )}
+              </div>
             ))}
+          </div>
 
-            <Flex mt={6} width="100%" justifyContent="space-between">
-              {hasActiveFilters && (
-                <Button
-                  variant="outline"
-                  width="48%"
-                  onClick={onReset}
-                >
-                  Reset Filters
-                </Button>
-              )}
-
-              <Button
-                colorScheme="blue"
-                width={hasActiveFilters ? '48%' : '100%'}
-                onClick={onSubmit}
-              >
-                Apply Filters
-              </Button>
-            </Flex>
-          </Box>
+          <Flex mt={6} width={'100%'} justifyContent={'space-between'}>
+            <SubmitButton
+              onClick={onSubmit}
+              width={'100%'}
+            >
+              Close
+            </SubmitButton>
+          </Flex>
         </Fade>
       )}
 
       {renderDetails && currentActiveItem && (
         <Fade in={!!currentActiveItem} unmountOnExit>
-          <Box className="MobileFilterMenu__DetailView">
+          <div className={'MobileFilterMenu__Header'}>
             <Flex
-              className="MobileFilterMenu__BackButton"
+              className={'MobileFilterMenu__BackButton'}
               onClick={goToMainMenu}
               alignItems="center"
-              mb={4}
               p={2}
               borderRadius="md"
               color="blue.500"
               cursor="pointer"
               width="fit-content"
             >
-              <Icon as={ChevronLeftIcon} boxSize={5} mr={2} />
-              <Text fontWeight="medium">Back to filters</Text>
+              <ChevronIcon transform={'rotate(180deg)'}/>
             </Flex>
 
+            <div className={'MobileFilterMenu__Title'}>Add Filters</div>
+          </div>
+
+          <Box className="MobileFilterMenu__DetailView">
             <Box className="MobileFilterMenu__Content">
               <Text fontWeight="bold" mb={4} fontSize="lg">{currentActiveItem.label}</Text>
               {currentActiveItem.content}
@@ -135,6 +123,6 @@ export const MobileFilterMenu = ({
           </Box>
         </Fade>
       )}
-    </Box>
+    </div>
   )
 }
