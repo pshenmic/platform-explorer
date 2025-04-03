f43df49824eac75904b989a53bbdd58cd8452194
Alexey <andreewsky.alexey@gmail.com>
2025-04-04 01:16:35 +0300

    Remove deprecated ValidatorsTotal

--- packages/frontend/src/components/validators/index.js
+++ packages/frontend/src/components/validators/index.js
@@ -1,11 +1,9 @@
 import ValidatorsList from './ValidatorsList'
-import ValidatorsTotal from './ValidatorsTotal'
 import ValidatorCard from './ValidatorCard'
 import ValidatorsDashboardCards from './ValidatorsDashboardCards'
 
 export {
   ValidatorsList,
-  ValidatorsTotal,
   ValidatorCard,
   ValidatorsDashboardCards
 }
