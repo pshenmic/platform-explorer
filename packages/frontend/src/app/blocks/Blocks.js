c7cb0f13f7d1c3295b5815e43711223c193ec05d
Alexey <andreewsky.alexey@gmail.com>
2025-04-07 21:34:19 +0300

    Fix search focus on Blocks

--- packages/frontend/src/app/blocks/Blocks.js
+++ packages/frontend/src/app/blocks/Blocks.js
@@ -153,7 +153,6 @@ function Blocks ({ defaultPage = 1, defaultPageSize }) {
             </div>
 
             <Popover
-              onClose={closeSearchHandler}
               closeOnBlur={true}
               placement={'bottom'}
               variant={'menu'}
