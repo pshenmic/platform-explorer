261ce7443d3d5d088bffa3962337d66726387160
Alexey <andreewsky.alexey@gmail.com>
2025-04-07 21:09:11 +0300

    Remove IdentityFilter import from Filters

--- packages/frontend/src/components/filters/Filters.js
+++ packages/frontend/src/components/filters/Filters.js
@@ -3,7 +3,7 @@
 import { useCallback, useEffect, useRef } from 'react'
 import { Button, useDisclosure } from '@chakra-ui/react'
 import { useFilters } from '../../hooks'
-import { MultiSelectFilter, InputFilter, RangeFilter, FilterGroup, ActiveFilters, IdentityFilter, SearchFilter } from './'
+import { MultiSelectFilter, InputFilter, RangeFilter, FilterGroup, ActiveFilters, SearchFilter } from './'
 import { BottomSheet } from '../ui/sheets'
 import { ChevronIcon } from '../ui/icons'
 import { MultiLevelMenu } from '../ui/menus'
@@ -162,19 +162,6 @@ export const Filters = ({
         content = (
           <FilterGroup title={config.title}>
             <InputFilter
-              value={filters[key]}
-              onChange={(value) => handleFilterChange(key, value)}
-              placeholder={config.placeholder}
-              showSubmitButton={true}
-              onSubmit={submitHandler}
-            />
-          </FilterGroup>
-        )
-        break
-      case 'identity':
-        content = (
-          <FilterGroup title={config.title}>
-            <IdentityFilter
               value={filters[key]}
               onChange={(value) => handleFilterChange(key, value)}
               placeholder={config.placeholder}
