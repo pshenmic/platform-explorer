02069f4440b1bff049ab11ed0f787a5a60f56911
Alexey <andreewsky.alexey@gmail.com>
2025-04-09 23:14:21 +0300

    Update ContestedResourcesDashboardCards

--- packages/frontend/src/components/contestedResources/ContestedResourcesDashboardCards.js
+++ packages/frontend/src/components/contestedResources/ContestedResourcesDashboardCards.js
@@ -1,19 +1,32 @@
 'use client'
 
-// import * as Api from '../../util/Api'
-// import { useState, useEffect } from 'react'
-// import { fetchHandlerSuccess, fetchHandlerError, currencyRound } from '../../util'
+import * as Api from '../../util/Api'
+import { useState, useEffect } from 'react'
+import { fetchHandlerSuccess, fetchHandlerError, currencyRound } from '../../util'
 import { DashboardCards } from '../cards'
 import { SignatureIcon, ListIcon, CalendarGradientIcon } from '../ui/icons'
+import { ExpiringContestedResourceContent } from "../cards/dashboard";
 import './ContestedResourcesDashboardCards.scss'
 
 function ContestedResourcesDashboardCards () {
+  const [stats, setStats] = useState({ data: {}, loading: true, error: false })
+
+  console.log('stats', stats)
+
+  const fetchData = () => {
+    Api.getContestedResourcesStats()
+      .then(res => fetchHandlerSuccess(setStats, res))
+      .catch(err => fetchHandlerError(setStats, err))
+  }
+
+  useEffect(fetchData, [])
+
   return (
     <DashboardCards
       cards={[
         {
           title: 'Total Contested Resources',
-          value: <div className={'TotalContestedResourcesCard'}>123</div>,
+          value: stats.data?.totalContestedResources,
           className: 'ContestedResourcesDashboardCards__Card',
           error: false,
           loading: false,
@@ -21,7 +34,7 @@ function ContestedResourcesDashboardCards () {
         },
         {
           title: 'Total Votes Casted',
-          value: 123,
+          value: stats.data?.totalVotesCount,
           className: 'ContestedResourcesDashboardCards__Card',
           error: false,
           loading: false,
@@ -29,7 +42,7 @@ function ContestedResourcesDashboardCards () {
         },
         {
           title: 'Pending Contested Resources',
-          value: 123,
+          value: stats.data?.totalPendingContestedResources,
           className: 'ContestedResourcesDashboardCards__Card',
           error: false,
           loading: false,
@@ -37,7 +50,7 @@ function ContestedResourcesDashboardCards () {
         },
         {
           title: 'Ending soon',
-          value: 123,
+          value: <ExpiringContestedResourceContent contestedResource={stats.data?.expiringContestedResource}/>,
           error: false,
           loading: false,
           icon: CalendarGradientIcon
