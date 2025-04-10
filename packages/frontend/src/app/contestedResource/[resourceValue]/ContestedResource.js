4350e34c19c887049041c6a2f560ef00f1e1be8b
Alexey <andreewsky.alexey@gmail.com>
2025-04-11 00:13:00 +0300

    Implement TotalCard to ContestedResource

--- packages/frontend/src/app/contestedResource/[resourceValue]/ContestedResource.js
+++ packages/frontend/src/app/contestedResource/[resourceValue]/ContestedResource.js
@@ -1,12 +1,16 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-// import * as Api from '../../../util/Api'
-// import { fetchHandlerSuccess, fetchHandlerError, setLoadingProp } from '../../../util'
-// import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import * as Api from '../../../util/Api'
+import { fetchHandlerSuccess, fetchHandlerError } from '../../../util'
+import {
+  // usePathname, useRouter,
+  useSearchParams
+} from 'next/navigation'
 import { InfoContainer, PageDataContainer } from '../../../components/ui/containers'
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Code, Box } from '@chakra-ui/react'
 import { useBreadcrumbs } from '../../../contexts/BreadcrumbsContext'
+import { ContestedResourcesTotalCard } from '../../../components/contestedResources'
 
 // const pagintationConfig = {
 //   itemsOnPage: {
@@ -27,31 +31,53 @@ const defaultTabName = 'All votes'
 
 function ContestedResource ({ resourceValue }) {
   const { setBreadcrumbs } = useBreadcrumbs()
-  const [contestedResource] = useState({ data: {}, loading: true, error: false })
+  const [contestedResource, setContestedResource] = useState({ data: {}, loading: true, error: false })
   // const [rate, setRate] = useState({ data: {}, loading: true, error: false })
   const [activeTab, setActiveTab] = useState(tabs.indexOf(defaultTabName.toLowerCase()) !== -1 ? tabs.indexOf(defaultTabName.toLowerCase()) : 0)
   // const router = useRouter()
   // const pathname = usePathname()
-  // const searchParams = useSearchParams()
+  const searchParams = useSearchParams()
 
   useEffect(() => {
     setBreadcrumbs([
       { label: 'Home', path: '/' },
       { label: 'Contested Resources', path: '/contestedResources' },
-      { label: contestedResource.data?.name || resourceValue, avatarSource: resourceValue }
+      { label: resourceValue }
     ])
   }, [setBreadcrumbs, resourceValue, contestedResource])
+
+  useEffect(() => {
+    Api.getContestedResourceByValue(resourceValue)
+      .then(res => fetchHandlerSuccess(setContestedResource, res))
+      .catch(err => fetchHandlerError(setContestedResource, err))
+    //
+    // Api.getRate()
+    //   .then(res => fetchHandlerSuccess(setRate, res))
+    //   .catch(err => fetchHandlerError(setRate, err))
+  }, [resourceValue])
+
+  useEffect(() => {
+    const tab = searchParams.get('tab')
+
+    if (tab && tabs.indexOf(tab.toLowerCase()) !== -1) {
+      setActiveTab(tabs.indexOf(tab.toLowerCase()))
+      return
+    }
+
+    setActiveTab(tabs.indexOf(defaultTabName.toLowerCase()) !== -1 ? tabs.indexOf(defaultTabName.toLowerCase()) : 0)
+  }, [searchParams])
+
+  console.log('contestedResource', contestedResource)
+  console.log('contestedResource', JSON.stringify(contestedResource))
 
   return (
     <PageDataContainer
       className={'ContestedResource'}
       title={'Contested Resource info'}
     >
-      resourceValue = {resourceValue}
+      <ContestedResourcesTotalCard contestedResource={contestedResource}/>
 
-      <div className={'ContestedResource__InfoBlocks'}>
-        Cards
-      </div>
+      <Box boxSize={'2rem'}/>
 
       <InfoContainer styles={['tabs']}>
         <Tabs onChange={(index) => setActiveTab(index)} index={activeTab}>
@@ -97,6 +123,15 @@ function ContestedResource ({ resourceValue }) {
           </TabPanels>
         </Tabs>
       </InfoContainer>
+
+      <Code
+        borderRadius={'lg'}
+        px={5}
+        py={4}
+        mt={10}
+      >
+        {JSON.stringify(contestedResource.data, null, 2)}
+      </Code>
     </PageDataContainer>
   )
 }
