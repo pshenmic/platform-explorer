9fba52d641c12e9fefacf879a45ec6f8b85e4049
Alexey <andreewsky.alexey@gmail.com>
2025-04-15 16:36:55 +0300

    Added values to ContestedResourceDigestCard

--- packages/frontend/src/components/contestedResources/ContestedResourceDigestCard.js
+++ packages/frontend/src/components/contestedResources/ContestedResourceDigestCard.js
@@ -3,27 +3,29 @@ import { LoadingLine } from '../loading'
 import './ContestedResourceDigestCard.scss'
 
 function ContestedResourceDigestCard ({ contestedResource, className }) {
+  console.log('contestedResource', contestedResource)
+
   return (
     <div className={`ContestedResourcesDigestCard ${className || ''} ${contestedResource?.loading ? 'ContestedResourcesDigestCard--Loading' : ''}`}>
       <div className={'ContestedResourcesDigestCard__Transfers'}>
         <div className={'ContestedResourcesDigestCard__Transfer ContestedResourcesDigestCard__Transfer--TupUp'}>
           <div className={'ContestedResourcesDigestCard__TransferTitle'}>Towards Identity</div>
           <LoadingLine loading={contestedResource?.loading}>
-            123
+            {contestedResource?.data?.totalCountTowardsIdentity}
           </LoadingLine>
         </div>
 
         <div className={'ContestedResourcesDigestCard__Transfer ContestedResourcesDigestCard__Transfer--Withdrawals'}>
           <div className={'ContestedResourcesDigestCard__TransferTitle'}>Abstain</div>
           <LoadingLine loading={contestedResource?.loading}>
-            123
+            {contestedResource?.data?.totalCountAbstain}
           </LoadingLine>
         </div>
 
         <div className={'ContestedResourcesDigestCard__Transfer ContestedResourcesDigestCard__Transfer--Withdrawals'}>
           <div className={'ContestedResourcesDigestCard__TransferTitle'}>Locked</div>
           <LoadingLine loading={contestedResource?.loading}>
-            123
+            {contestedResource?.data?.totalCountLock}
           </LoadingLine>
         </div>
       </div>
@@ -32,21 +34,21 @@ function ContestedResourceDigestCard ({ contestedResource, className }) {
         <InfoLine
           className={'ContestedResourcesDigestCard__InfoLine'}
           title={'Total Votes'}
-          value={123}
+          value={contestedResource?.data?.totalCountVotes}
           loading={contestedResource.loading}
           error={contestedResource.error}
         />
         <InfoLine
           className={'ContestedResourcesDigestCard__InfoLine ContestedResourcesDigestCard__InfoLine--LastWithdrawal'}
           title={'Status'}
-          value={123}
+          value={contestedResource?.data?.status}
           loading={contestedResource.loading}
           error={contestedResource.error}
         />
         <InfoLine
           className={'ContestedResourcesDigestCard__InfoLine'}
           title={'Ends In'}
-          value={123}
+          value={contestedResource?.data?.endTimestamp}
           loading={contestedResource.loading}
           error={contestedResource.error}
         />
