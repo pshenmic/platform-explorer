59574c8f135088469b781039f10b5b0c61df297d
Alexey <andreewsky.alexey@gmail.com>
2025-04-17 20:17:23 +0300

    Update ContestedResourcesDigestCard styles

--- packages/frontend/src/components/contestedResources/ContestedResourceDigestCard.js
+++ packages/frontend/src/components/contestedResources/ContestedResourceDigestCard.js
@@ -1,47 +1,54 @@
 import { InfoLine } from '../data'
 import { LoadingLine } from '../loading'
+import { VoteStatusValue } from './'
+import ChoiceBadge from './ChoiceBadge'
 import './ContestedResourceDigestCard.scss'
 
 function ContestedResourceDigestCard ({ contestedResource, className }) {
-  console.log('contestedResource', contestedResource)
-
   return (
     <div className={`ContestedResourcesDigestCard ${className || ''} ${contestedResource?.loading ? 'ContestedResourcesDigestCard--Loading' : ''}`}>
-      <div className={'ContestedResourcesDigestCard__Transfers'}>
-        <div className={'ContestedResourcesDigestCard__Transfer ContestedResourcesDigestCard__Transfer--TupUp'}>
-          <div className={'ContestedResourcesDigestCard__TransferTitle'}>Towards Identity</div>
-          <LoadingLine loading={contestedResource?.loading}>
-            {contestedResource?.data?.totalCountTowardsIdentity}
-          </LoadingLine>
+      <div className={'ContestedResourcesDigestCard__Cards'}>
+        <div className={'ContestedResourcesDigestCard__Card ContestedResourcesDigestCard__Card--TupUp'}>
+          <div className={'ContestedResourcesDigestCard__CardTitle'}><ChoiceBadge choice={0}/></div>
+          <div className={'ContestedResourcesDigestCard__CardValue'}>
+            <LoadingLine loading={contestedResource?.loading}>
+              {contestedResource?.data?.totalCountTowardsIdentity}
+            </LoadingLine>
+          </div>
         </div>
 
-        <div className={'ContestedResourcesDigestCard__Transfer ContestedResourcesDigestCard__Transfer--Withdrawals'}>
-          <div className={'ContestedResourcesDigestCard__TransferTitle'}>Abstain</div>
-          <LoadingLine loading={contestedResource?.loading}>
-            {contestedResource?.data?.totalCountAbstain}
-          </LoadingLine>
+        <div className={'ContestedResourcesDigestCard__Card ContestedResourcesDigestCard__Card--Withdrawals'}>
+          <div className={'ContestedResourcesDigestCard__CardTitle'}><ChoiceBadge choice={1}/></div>
+          <div className={'ContestedResourcesDigestCard__CardValue'}>
+            <LoadingLine loading={contestedResource?.loading}>
+              {contestedResource?.data?.totalCountAbstain}
+            </LoadingLine>
+          </div>
         </div>
 
-        <div className={'ContestedResourcesDigestCard__Transfer ContestedResourcesDigestCard__Transfer--Withdrawals'}>
-          <div className={'ContestedResourcesDigestCard__TransferTitle'}>Locked</div>
-          <LoadingLine loading={contestedResource?.loading}>
-            {contestedResource?.data?.totalCountLock}
-          </LoadingLine>
+        <div className={'ContestedResourcesDigestCard__Card ContestedResourcesDigestCard__Card--Withdrawals'}>
+          <div className={'ContestedResourcesDigestCard__CardTitle'}><ChoiceBadge choice={2}/></div>
+          <div className={'ContestedResourcesDigestCard__CardValue'}>
+
+            <LoadingLine loading={contestedResource?.loading}>
+              {contestedResource?.data?.totalCountLock}
+            </LoadingLine>
+          </div>
         </div>
       </div>
 
-      <div className={'ContestedResourcesDigestCard__LinesContainer'}>
+        <div className={'ContestedResourcesDigestCard__LinesContainer'}>
         <InfoLine
           className={'ContestedResourcesDigestCard__InfoLine'}
           title={'Total Votes'}
-          value={contestedResource?.data?.totalCountVotes}
+          value={<span>{contestedResource?.data?.totalCountVotes} Votes</span>}
           loading={contestedResource.loading}
           error={contestedResource.error}
         />
         <InfoLine
           className={'ContestedResourcesDigestCard__InfoLine ContestedResourcesDigestCard__InfoLine--LastWithdrawal'}
           title={'Status'}
-          value={contestedResource?.data?.status}
+          value={<VoteStatusValue status={contestedResource?.data?.status}/>}
           loading={contestedResource.loading}
           error={contestedResource.error}
         />
