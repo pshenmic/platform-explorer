5eb6c004e490267a2d97b018c55f621c12b2d615
Alexey <andreewsky.alexey@gmail.com>
2025-04-17 18:35:37 +0300

    Add scroll to Votes list

--- packages/frontend/src/components/contestedResources/votes/VotesList.js
+++ packages/frontend/src/components/contestedResources/votes/VotesList.js
@@ -14,45 +14,49 @@ function VotesList ({ votes = [], headerStyles, pagination, loading, itemsCount 
 
   return (
     <div className={'VotesList'}>
-      <Grid className={`VotesList__ColumnTitles ${headerExtraClass?.[headerStyles] || ''}`}>
-        <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--Timestamp'}>
-          Time
-        </GridItem>
-        <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--ProTxHash'}>
-          PRO TX HASH
-        </GridItem>
-        <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--DataContract'}>
-          Data Contract
-        </GridItem>
-        <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--Voter'}>
-          Voter
-        </GridItem>
-        <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--Document'}>
-          Document
-        </GridItem>
-        <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--TowardsIdentity'}>
-          Towards Identity
-        </GridItem>
-        <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--Choice'}>
-          Choice
-        </GridItem>
-        <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--Power'}>
-          Power
-        </GridItem>
-      </Grid>
+      <div className={'VotesList__ListContainer'}>
+        <div className={'VotesList__ScrollZone'}>
+          <Grid className={`VotesList__ColumnTitles ${headerExtraClass?.[headerStyles] || ''}`}>
+            <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--Timestamp'}>
+              Time
+            </GridItem>
+            <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--ProTxHash'}>
+              PRO TX HASH
+            </GridItem>
+            <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--DataContract'}>
+              Data Contract
+            </GridItem>
+            <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--Voter'}>
+              Voter
+            </GridItem>
+            <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--Document'}>
+              Document
+            </GridItem>
+            <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--TowardsIdentity'}>
+              Towards Identity
+            </GridItem>
+            <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--Choice'}>
+              Choice
+            </GridItem>
+            <GridItem className={'VotesList__ColumnTitle VotesList__ColumnTitle--Power'}>
+              Power
+            </GridItem>
+          </Grid>
 
-      {!loading
-        ? <div className={'VotesList__Items'}>
-          {votes?.map((vote, i) =>
-            <VotesListItem vote={vote} key={i}/>
-          )}
-          {votes?.length === 0 &&
-            <EmptyListMessage>There are no data contracts created yet.</EmptyListMessage>
+          {!loading
+            ? <div className={'VotesList__Items'}>
+              {votes?.map((vote, i) =>
+                <VotesListItem vote={vote} key={i}/>
+              )}
+              {votes?.length === 0 &&
+                <EmptyListMessage>There are no data contracts created yet.</EmptyListMessage>
+              }
+              {votes === undefined && <ErrorMessageBlock/>}
+            </div>
+            : <LoadingList itemsCount={itemsCount}/>
           }
-          {votes === undefined && <ErrorMessageBlock/>}
         </div>
-        : <LoadingList itemsCount={itemsCount}/>
-      }
+      </div>
 
       {pagination &&
         <Pagination
