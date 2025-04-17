0c3e301b6b5736b76f29489b223680779ea453ac
Alexey <andreewsky.alexey@gmail.com>
2025-04-17 17:06:30 +0300

    Update n/a state of ExpiringContestedResourceContent

--- packages/frontend/src/components/cards/dashboard/ExpiringContestedResourceContent.js
+++ packages/frontend/src/components/cards/dashboard/ExpiringContestedResourceContent.js
@@ -7,37 +7,42 @@ import './ExpiringContestedResourceContent.scss'
 export function ExpiringContestedResourceContent ({ contestedResource }) {
   return (
     <div className={'ExpiringContestedResourceContent'}>
-      <div className={'ExpiringContestedResourceContent__ValueContainer'}>
-        <Alias
-          className={'ExpiringContestedResourceContent__Value'}
-          ellipsis={false}
-        >
-          {contestedResources.getResourceValue(contestedResource?.resourceValue)}
-        </Alias>
-        {contestedResource?.contenders &&
-          <Badge
-            className={'ExpiringContestedResourceContent__ContendersBadge'}
-            colorScheme={'blue'}
-            size={'xs'}
-            ml={'0.25rem'}
-          >
-            {contestedResource.contenders}
-          </Badge>
-        }
-      </div>
+      {contestedResource
+        ? <>
+          <div className={'ExpiringContestedResourceContent__ValueContainer'}>
+            <Alias
+              className={'ExpiringContestedResourceContent__Value'}
+              ellipsis={false}
+            >
+              {contestedResources.getResourceValue(contestedResource?.resourceValue)}
+            </Alias>
+            {contestedResource?.contenders &&
+              <Badge
+                className={'ExpiringContestedResourceContent__ContendersBadge'}
+                colorScheme={'blue'}
+                size={'xs'}
+                ml={'0.25rem'}
+              >
+                {contestedResource.contenders}
+              </Badge>
+            }
+          </div>
 
-      <VoteBadges
-        className={'ExpiringContestedResourceContent__VoteBadges'}
-        totalCountAbstain={contestedResource?.totalCountAbstain}
-        totalCountLock={contestedResource?.totalCountLock}
-        totalCountTowardsIdentity={contestedResource?.totalCountTowardsIdentity}
-      />
+          <VoteBadges
+            className={'ExpiringContestedResourceContent__VoteBadges'}
+            totalCountAbstain={contestedResource?.totalCountAbstain}
+            totalCountLock={contestedResource?.totalCountLock}
+            totalCountTowardsIdentity={contestedResource?.totalCountTowardsIdentity}
+          />
 
-      {(contestedResource?.timestamp && contestedResource?.endTimestamp) &&
-        <TimeRemaining
-          startTime={contestedResource?.timestamp}
-          endTime={contestedResource?.endTimestamp}
-        />
+          {(contestedResource?.timestamp && contestedResource?.endTimestamp) &&
+            <TimeRemaining
+              startTime={contestedResource?.timestamp}
+              endTime={contestedResource?.endTimestamp}
+            />
+          }
+        </>
+        : <>All completed</>
       }
     </div>
   )
