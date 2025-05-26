cae9d21941661656e974e2f3e8cf4f8dfef098e8
owl352 <kokosinca123@gmail.com>
2025-05-26 22:13:57 +0300

    initial commit

--- packages/api/src/dao/MasternodeVotesDAO.js
+++ packages/api/src/dao/MasternodeVotesDAO.js
@@ -1,6 +1,7 @@
 const Vote = require('../models/Vote')
 const PaginatedResultSet = require('../models/PaginatedResultSet')
-const { getAliasInfo, getAliasStateByVote } = require('../utils')
+const { getAliasInfo, getAliasStateByVote, buildIndexBuffer } = require('../utils')
+const { base58 } = require('@scure/base')
 
 module.exports = class MasternodeVotesDAO {
   constructor (knex, dapi) {
@@ -60,9 +61,52 @@ module.exports = class MasternodeVotesDAO {
       .groupBy('identity_identifier')
       .as('aliases')
 
+    const prefundedDocumentsSubquery = this.knex('documents')
+      .whereRaw('prefunded_voting_balance is not null')
+      .andWhereRaw('data is not null')
+      .groupBy('identifier', 'id', 'data_contract_id')
+      .as('sub')
+
+    const documentsPrefundingIndexeKeysSubquery = this.knex(prefundedDocumentsSubquery)
+      .select('id', 'data_contract_id', 'data', 'state_transition_hash')
+      .select(this.knex.raw('jsonb_object_keys(prefunded_voting_balance) as key'))
+      .as('sub')
+
+    const dataContractsWithContestedDocs = this.knex(documentsPrefundingIndexeKeysSubquery)
+      .select('schema', 'sub.id as document_id', 'data', 'sub.key as key')
+      .leftJoin('data_contracts', 'sub.data_contract_id', 'data_contracts.id')
+      .as('data_contracts_with_contested')
+
+    const filteredIndexElementSubquery = this.knex
+      .select('index_element', 'data', 'document_id', 'data_contracts_with_contested.key as key')
+      .select(this.knex.raw('index_element->>\'name\' as name'))
+      .whereRaw('index_element->>\'name\' = data_contracts_with_contested.key')
+      .fromRaw('?, jsonb_each(schema) AS key_value(key, value), jsonb_array_elements(value->\'indices\') AS index_element', dataContractsWithContestedDocs)
+      .as('index_elements')
+
+    const keysSubquery = this.knex
+      .select(
+        'document_id', 'field_index', 'name as index_name', 'data',
+        this.knex.raw('jsonb_object_keys(property) as property_keys')
+      )
+      .fromRaw('?, jsonb_array_elements(index_elements.index_element -> \'properties\') WITH ORDINALITY AS t(property,field_index)', filteredIndexElementSubquery)
+      .as('keys_subquery')
+
+    const resourceValuesSubquery = this.knex(keysSubquery)
+      .select('document_id')
+      .select(this.knex.raw('jsonb_agg(data->property_keys order by field_index asc) as resource_value'))
+      .groupBy('document_id')
+      .as('resource_values')
+
+    const documentsGroupSubquery = this.knex(resourceValuesSubquery)
+      .select('resource_value')
+      .select(this.knex.raw('array_agg(document_id) as document_ids'))
+      .groupBy('resource_value')
+      .as('resource_values')
+
     const subquery = this.knex('masternode_votes')
       .select('masternode_votes.id as id', 'pro_tx_hash', 'masternode_votes.state_transition_hash as state_transition_hash', 'voter_identity_id', 'choice',
-        'towards_identity_identifier', 'data_contract_id', 'document_type_name', 'index_name', 'index_values',
+        'towards_identity_identifier', 'data_contract_id', 'document_type_name', 'index_name', 'index_values', 'document_ids',
         'data_contracts.identifier as data_contract_identifier', 'blocks.timestamp as timestamp', 'aliases', 'power')
       .select(this.knex.raw(`rank() over (order by masternode_votes.id ${order}) rank`))
       .whereRaw(...timestampFilter)
@@ -74,15 +118,33 @@ module.exports = class MasternodeVotesDAO {
       .leftJoin('state_transitions', 'masternode_votes.state_transition_hash', 'state_transitions.hash')
       .leftJoin('blocks', 'blocks.hash', 'state_transitions.block_hash')
       .leftJoin(aliasesSubquery, 'aliases.identity_identifier', 'towards_identity_identifier')
+      .leftJoin(documentsGroupSubquery, 'resource_values.resource_value', 'index_values')
       .as('subquery')
 
     const rows = await this.knex(subquery)
       .select('pro_tx_hash', 'subquery.state_transition_hash as state_transition_hash', 'choice',
-        'subquery.timestamp as timestamp', 'towards_identity_identifier', 'voter_identity_id',
+        'subquery.timestamp as timestamp', 'towards_identity_identifier', 'voter_identity_id', 'document_ids',
         'data_contract_identifier', 'document_type_name', 'index_name', 'index_values', 'aliases', 'power')
       .select(this.knex(subquery).count('*').as('total_count'))
       .whereBetween('rank', [fromRank, toRank])
       .orderBy('subquery.id', order)
+
+    const t = await Promise.all(rows.map(async row => {
+      const value = row.index_values.map(val => buildIndexBuffer(val))
+
+      const d = await this.dapi.getContestedState(
+        Buffer.from(base58.decode(row.data_contract_identifier)).toString('base64'),
+        row.document_type_name.trim(),
+        row.index_name.trim(),
+        1,
+        value
+      )
+
+      return {
+        ...row,
+        d
+      }
+    }))
 
     const resultSet = await Promise.all(rows.map(async (row) => {
       const aliases = await Promise.all((row.aliases ?? []).map(async alias => {
