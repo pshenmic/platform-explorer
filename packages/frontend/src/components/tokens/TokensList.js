0dce7baf5b50906205a2a98d9d8eb049f8039ecd
Alexey <andreewsky.alexey@gmail.com>
2025-06-04 17:58:12 +0300

    Initialize TokensList component

---
+++ packages/frontend/src/components/tokens/TokensList.js
@@ -0,0 +1,61 @@
+'use client'
+
+import TokensListItem from './TokensListItem'
+import { EmptyListMessage } from '../ui/lists'
+import { Grid, GridItem } from '@chakra-ui/react'
+import Pagination from '../pagination'
+import { ErrorMessageBlock } from '../Errors'
+import { LoadingList } from '../loading'
+import './TokensList.scss'
+
+function TokensList ({ identities, headerStyles = 'default', pagination, loading, itemsCount = 10 }) {
+  const headerExtraClass = {
+    default: '',
+    light: 'IdentitiesList__ColumnTitles--Light'
+  }
+
+  return (
+    <div className={'IdentitiesList'}>
+      <Grid className={`IdentitiesList__ColumnTitles ${headerExtraClass[headerStyles] || ''}`}>
+        <GridItem className={'IdentitiesList__ColumnTitle IdentitiesList__ColumnTitle--Identifier'}>
+          Identifier
+        </GridItem>
+        <GridItem className={'IdentitiesList__ColumnTitle IdentitiesList__ColumnTitle--Timestamp'}>
+          Timestamp
+        </GridItem>
+      </Grid>
+
+      {!loading
+        ? <div className={'IdentitiesList__Items'}>
+          {identities?.map((identity, key) =>
+            <TokensListItem
+              key={key}
+              identity={identity}
+            />
+          )}
+          {!identities?.length &&
+            <EmptyListMessage>There are no identities yet.</EmptyListMessage>
+          }
+          {identities === undefined && <ErrorMessageBlock/>}
+        </div>
+        : <LoadingList itemsCount={itemsCount}/>
+      }
+
+      {pagination &&
+        <Pagination
+          className={'IdentitiesList__Pagination'}
+          onPageChange={pagination.onPageChange}
+          pageCount={pagination.pageCount}
+          forcePage={pagination.forcePage}
+          justify={true}
+        />
+      }
+
+      {identities.length === 0 &&
+        <EmptyListMessage>There are no identities created yet.</EmptyListMessage>
+      }
+    </div>
+  )
+}
+
+export default TokensList
---
+++ packages/frontend/src/components/tokens/TokensListItem.js
@@ -0,0 +1,56 @@
+import Link from 'next/link'
+import { Identifier, Alias, DateBlock } from '../data'
+import { Grid, GridItem } from '@chakra-ui/react'
+import './TokensListItem.scss'
+
+function TokensListItem ({ identity }) {
+  const { aliases, identifier, timestamp, isSystem } = identity
+  const activeAlias = aliases?.find(alias => alias?.status === 'ok')
+
+  return (
+    <Link
+      href={`/identity/${identifier}`}
+      className={'IdentitiesListItem'}
+    >
+      <Grid className={'IdentitiesListItem__Content'}>
+        <GridItem className={'IdentitiesListItem__Column IdentitiesListItem__Column--Identifier'}>
+
+          <div className={'IdentitiesListItem__IdentifierContainer'}>
+            {activeAlias
+              ? <Alias
+                className={'IdentitiesListItem__Alias'}
+                alias={activeAlias?.alias}
+                avatarSource={identifier}
+              />
+              : <Identifier
+                className={'IdentitiesListItem__Identifier'}
+                ellipsis={true}
+                styles={['highlight-both']}
+                avatar={true}
+              >
+                {identifier}
+              </Identifier>
+            }
+          </div>
+        </GridItem>
+
+        <GridItem className={'IdentitiesListItem__Column IdentitiesListItem__Column--Timestamp'}>
+          {isSystem && <div>SYSTEM</div>}
+
+          {typeof timestamp === 'string' &&
+            <div className={'IdentitiesListItem__Timestamp'}>
+              <DateBlock
+                format={'dateOnly'}
+                showTime={true}
+                timestamp={timestamp}
+                showRelativeTooltip={true}
+              />
+            </div>
+          }
+        </GridItem>
+      </Grid>
+    </Link>
+  )
+}
+
+export default TokensListItem
