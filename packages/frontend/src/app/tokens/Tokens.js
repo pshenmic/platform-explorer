ea4e847d748a5103ffebac2e9c6f4b0f90969053
Alexey <andreewsky.alexey@gmail.com>
2025-06-04 17:40:41 +0300

    Init tokens page

---
+++ packages/frontend/src/app/tokens/Tokens.js
@@ -0,0 +1,112 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import * as Api from '../../util/Api'
+import IdentitiesList from '../../components/identities/IdentitiesList'
+import Pagination from '../../components/pagination'
+import PageSizeSelector from '../../components/pageSizeSelector/PageSizeSelector'
+import { LoadingList } from '../../components/loading'
+import { ErrorMessageBlock } from '../../components/Errors'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { fetchHandlerSuccess, fetchHandlerError } from '../../util'
+
+import {
+  Container,
+  Heading,
+  Box
+} from '@chakra-ui/react'
+
+const paginateConfig = {
+  pageSize: {
+    default: 25,
+    values: [10, 25, 50, 75, 100]
+  },
+  defaultPage: 1
+}
+
+function Tokens ({ defaultPage = 1, defaultPageSize }) {
+  const [identities, setIdentities] = useState({ data: {}, loading: true, error: false })
+  const [total, setTotal] = useState(1)
+  const [pageSize, setPageSize] = useState(defaultPageSize || paginateConfig.pageSize.default)
+  const [currentPage, setCurrentPage] = useState(defaultPage ? defaultPage - 1 : 0)
+  const pageCount = Math.ceil(total / pageSize)
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  const fetchData = (page, count) => {
+    setIdentities(state => ({ ...state, loading: true }))
+
+    Api.getIdentities(page, count, 'desc')
+      .then(res => {
+        if (res.pagination.total === -1) {
+          setCurrentPage(0)
+        }
+        fetchHandlerSuccess(setIdentities, res)
+        setTotal(res.pagination.total)
+      })
+      .catch(err => fetchHandlerError(setIdentities, err))
+  }
+
+  useEffect(() => fetchData(currentPage + 1, pageSize), [pageSize, currentPage])
+
+  useEffect(() => {
+    const page = parseInt(searchParams.get('page')) || paginateConfig.defaultPage
+    setCurrentPage(Math.max(page - 1, 0))
+    setPageSize(parseInt(searchParams.get('page-size')) || paginateConfig.pageSize.default)
+  }, [searchParams, pathname])
+
+  useEffect(() => {
+    const urlParameters = new URLSearchParams(Array.from(searchParams.entries()))
+
+    if (currentPage + 1 === paginateConfig.defaultPage && pageSize === paginateConfig.pageSize.default) {
+      urlParameters.delete('page')
+      urlParameters.delete('page-size')
+    } else {
+      urlParameters.set('page', currentPage + 1)
+      urlParameters.set('page-size', pageSize)
+    }
+
+    router.push(`${pathname}?${urlParameters.toString()}`, { scroll: false })
+  }, [currentPage, pageSize])
+
+  return (
+    <Container
+        maxW={'container.maxPageW'}
+        mt={8}
+        className={'IdentitiesPage'}
+    >
+      <Container
+        maxW={'container.maxPageW'}
+        className={'InfoBlock'}
+      >
+        <Heading className={'InfoBlock__Title'} as={'h1'}>Identities</Heading>
+
+        {!identities.error
+          ? !identities.loading
+              ? <IdentitiesList identities={identities.data.resultSet}/>
+              : <LoadingList itemsCount={pageSize}/>
+          : <ErrorMessageBlock h={20}/>
+        }
+
+        {identities.data?.resultSet?.length > 0 &&
+          <div className={'ListNavigation'}>
+            <Box display={['none', 'none', 'block']} width={'155px'}/>
+            <Pagination
+              onPageChange={({ selected }) => setCurrentPage(selected)}
+              pageCount={pageCount}
+              forcePage={currentPage}
+            />
+            <PageSizeSelector
+              PageSizeSelectHandler={e => setPageSize(e.value)}
+              value={pageSize}
+              items={paginateConfig.pageSize.values}
+            />
+          </div>
+        }
+      </Container>
+    </Container>
+  )
+}
+
+export default Tokens
---
+++ packages/frontend/src/app/tokens/page.js
@@ -0,0 +1,35 @@
+import Tokens from './Tokens'
+import Intro from '../../components/intro'
+import Markdown from '../../components/markdown'
+import introContent from './intro.md'
+import { Container } from '@chakra-ui/react'
+
+export const metadata = {
+  title: 'Tokens — Dash Platform Explorer',
+  description: 'Tokens on the Platform Explorer are digital assets managed through Dash Platform data contracts.',
+  keywords: ['Dash', 'platform', 'explorer', 'blockchain', 'Tokens', 'digital assets'],
+  applicationName: 'Dash Platform Explorer'
+}
+
+function IdentitiesRoute ({ searchParams }) {
+  const page = Number(searchParams.page) || 1
+  const pageSize = Number(searchParams['page-size'])
+
+  return <>
+    <Container
+      maxW={'container.maxPageW'}
+      color={'white'}
+      mt={8}
+      mb={0}
+    >
+      <Intro
+        title={'Tokens'}
+        description={<Markdown>{introContent}</Markdown>}
+        block={<>The best tokens will be here</>}
+      />
+    </Container>
+    <Tokens defaultPage={page} defaultPageSize={pageSize}/>
+  </>
+}
+
+export default IdentitiesRoute
