f028abcb3647154642c18291d64776bbf928e41e
Alexey <andreewsky.alexey@gmail.com>
2025-06-05 16:17:05 +0300

    Update token components exports

--- packages/frontend/src/components/tokens/index.js
+++ packages/frontend/src/components/tokens/index.js
@@ -1,2 +1,4 @@
 export { default as TokenTransitionCard } from './TokenTransitionCard'
 export { default as TokenTransitionBadge } from './TokenTransitionBadge'
+export { default as Supply } from './Supply'
+export { default as TickerBadge } from './TickerBadge'
