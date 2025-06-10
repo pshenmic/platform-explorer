d93d0549023c28ce04d816f6b7de11e85571fb5c
Alexey <andreewsky.alexey@gmail.com>
2025-06-10 23:51:33 +0300

    Implement mock LocalisationList to TokenTotalCard

--- packages/frontend/src/components/tokens/TokenTotalCard.js
+++ packages/frontend/src/components/tokens/TokenTotalCard.js
@@ -2,13 +2,38 @@
 
 import { Alias, DateBlock, Identifier, InfoLine } from '../data'
 import { HorisontalSeparator } from '../ui/separators'
+import { SmoothSize, ValueContainer } from '../ui/containers'
+import { Button } from '@chakra-ui/react'
+import { ChevronIcon } from '../ui/icons'
 import { findActiveAlias } from '../../util'
 import TokenDigestCard from './TokenDigestCard'
 import { ValueCard } from '../cards'
+import { LocalisationList } from './localisation'
+import { useState } from 'react'
 import './TokenTotalCard.scss'
+
+const LocalisationTranslations = ({ className, show, localisations = [] }) => (
+  <SmoothSize className={className || ''}>
+    {localisations.length > 0 &&
+      <LocalisationList
+        className={`TokenTotalCard__LocalisationList ${show ? 'TokenTotalCard__LocalisationList--Show' : ''}`}
+        localisations={localisations}
+      />
+    }
+  </SmoothSize>
+)
 
 function TokenTotalCard ({ token, loading }) {
   const activeAlias = findActiveAlias(token.data?.aliases)
+  const [showLocalisations, setShowLocalisations] = useState(false)
+
+  const mockLocalisations = [
+    { language: 'English', singular: 'Tether', plural: 'Tethers', capitalize: true },
+    { language: 'Russian', singular: 'Тэзэр', plural: 'Тэзэры', capitalize: true },
+    { language: 'Japanese', singular: 'ダッシュ (Tetheruu)', plural: 'ダッシュズ (Tetheeres)', capitalize: true },
+    { language: 'Thai', singular: 'แดช (Tethe)', plural: 'แดชหลายตัว (Te le teher)', capitalize: true },
+    { language: 'Chinese', singular: '达世币 (Tethrr)', plural: '达世币们 (Telother)', capitalize: true }
+  ]
 
   return (
     <div className={`InfoBlock InfoBlock--Gradient tokenPage__CommonInfo TokenTotalCard ${loading ? 'TokenTotalCard--Loading' : ''} `}>
@@ -95,11 +120,30 @@ function TokenTotalCard ({ token, loading }) {
               error={token.error}
             />
             <InfoLine
-              className={'TokenTotalCard__InfoLine'}
+              className={'TokenTotalCard__InfoLine TokenTotalCard__InfoLine--Localisation'}
               title={'Localisation'}
-              value={<>Localisation button</>}
+              value={mockLocalisations?.length
+                ? <Button
+                    className={'TokenTotalCard__LocalisationShowButton'}
+                    size={'sm'}
+                    variant={showLocalisations && mockLocalisations?.length > 0 ? 'gray' : 'blue'}
+                    onClick={() => setShowLocalisations(prev => !prev)}
+                  >
+                    {mockLocalisations?.length} translations
+                    <ChevronIcon ml={'4px'} h={'10px'} w={'10px'}
+                                 transform={`rotate(${showLocalisations ? '-90deg' : '90deg'})`}/>
+                  </Button>
+                : <ValueContainer className={'TokenTotalCard__ZeroListBadge'}>none</ValueContainer>
+              }
               loading={loading}
               error={token.error}
+            />
+            <LocalisationTranslations
+              localisations={mockLocalisations}
+              show={showLocalisations}
+              className={`TokenTotalCard__LocalisationListContainer TokenTotalCard__LocalisationListContainer--Mobile ${showLocalisations
+                ? ' TokenTotalCard__LocalisationListContainer--Opened'
+                : ' TokenTotalCard__LocalisationListContainer--Hidden'}`}
             />
           </div>
         </div>
@@ -111,6 +155,13 @@ function TokenTotalCard ({ token, loading }) {
           />
         </div>
       </div>
+      <LocalisationTranslations
+        localisations={mockLocalisations}
+        show={showLocalisations}
+        className={`TokenTotalCard__LocalisationListContainer TokenTotalCard__LocalisationListContainer--Desktop ${showLocalisations
+          ? ' TokenTotalCard__LocalisationListContainer--Opened'
+          : ' TokenTotalCard__LocalisationListContainer--Hidden'}`}
+      />
     </div>
   )
 }
