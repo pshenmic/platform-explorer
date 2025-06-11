768453f0868ccbcd59793842591eb64ee80dc20c
Alexey <andreewsky.alexey@gmail.com>
2025-06-11 14:37:57 +0300

    Implement mock identities list as Holder list in Token page

--- packages/frontend/src/app/token/[identifier]/Token.js
+++ packages/frontend/src/app/token/[identifier]/Token.js
@@ -8,7 +8,114 @@ import { useBreadcrumbs } from '../../../contexts/BreadcrumbsContext'
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import { InfoContainer, PageDataContainer } from '../../../components/ui/containers'
 import { TokenTotalCard } from '../../../components/tokens'
+import { IdentitiesList } from '../../../components/identities'
 import './Token.scss'
+
+const mockIdentities = {
+  resultSet: [
+    {
+      identifier: '8jRiSLHXeRnmtc8F9iEeoxVZorAywG4apurjS2BHUgQM',
+      revision: 0,
+      balance: 783312679,
+      timestamp: '2024-09-03T18:46:26.452Z',
+      txHash: '64CAD045D424344C6251947F452283BD1EB0CFA863A84A6A1B0B02FEA22794AD',
+      totalTxs: 4,
+      totalTransfers: 2,
+      totalDocuments: 2,
+      totalDataContracts: 0,
+      isSystem: false,
+      aliases: [
+        {
+          alias: '2x2.dash',
+          status: 'ok',
+          contested: false,
+          timestamp: '2024-09-03T21:47:12.253+03:00',
+          txHash: '996161AFE95D7D0E12E35A6519E76C5FAA00FAA59FAF8CF998ACB1741E3C1834'
+        }
+      ],
+      totalGasSpent: null,
+      averageGasSpent: null,
+      totalTopUpsAmount: null,
+      totalWithdrawalsAmount: null,
+      lastWithdrawalHash: null,
+      publicKeys: [],
+      fundingCoreTx: null,
+      totalTopUps: null,
+      totalWithdrawals: null,
+      lastWithdrawalTimestamp: null,
+      owner: '8jRiSLHXeRnmtc8F9iEeoxVZorAywG4apurjS2BHUgQM'
+    },
+    {
+      identifier: '6Lar6YD3ZBWhV6kmstSPmDpsgJ9BT3DGA7cV2yp251Jf',
+      revision: 0,
+      balance: 782183263,
+      timestamp: '2024-09-03T18:46:42.400Z',
+      txHash: '4573477297AE942AB64E689C3CD3A62B46DF7FBBF8460A9F86CD8DA8FAE2FCFD',
+      totalTxs: 4,
+      totalTransfers: 2,
+      totalDocuments: 2,
+      totalDataContracts: 0,
+      isSystem: false,
+      aliases: [
+        {
+          alias: 'Dash-007.dash',
+          status: 'ok',
+          contested: false,
+          timestamp: '2024-09-03T21:48:29.074+03:00',
+          txHash: '90BFFF2B9C3BED44336A6F29EE95F09DF596AD3A3FF11FEFFB724BAC5C7C2C06'
+        }
+      ],
+      totalGasSpent: null,
+      averageGasSpent: null,
+      totalTopUpsAmount: null,
+      totalWithdrawalsAmount: null,
+      lastWithdrawalHash: null,
+      publicKeys: [],
+      fundingCoreTx: null,
+      totalTopUps: null,
+      totalWithdrawals: null,
+      lastWithdrawalTimestamp: null,
+      owner: '6Lar6YD3ZBWhV6kmstSPmDpsgJ9BT3DGA7cV2yp251Jf'
+    },
+    {
+      identifier: '96C5EqyAZh3eDsPpFTrFXrMefWnMegxZLq9wUYUUkosX',
+      revision: 0,
+      balance: 782907318,
+      timestamp: '2024-09-03T18:46:56.812Z',
+      txHash: '65025CDD150D3B18B0D6D75D9B0BBB1CC86F38840D074DA3A0D3298D3B41D9E6',
+      totalTxs: 4,
+      totalTransfers: 2,
+      totalDocuments: 2,
+      totalDataContracts: 0,
+      isSystem: false,
+      aliases: [
+        {
+          alias: 'Aaron2.dash',
+          status: 'ok',
+          contested: false,
+          timestamp: '2024-09-03T21:52:17.562+03:00',
+          txHash: '1A98D47561ACF4E5451AE50A7A6D06517B5BD511A113C231622CCC8FB4EA537A'
+        }
+      ],
+      totalGasSpent: null,
+      averageGasSpent: null,
+      totalTopUpsAmount: null,
+      totalWithdrawalsAmount: null,
+      lastWithdrawalHash: null,
+      publicKeys: [],
+      fundingCoreTx: null,
+      totalTopUps: null,
+      totalWithdrawals: null,
+      lastWithdrawalTimestamp: null,
+      owner: '96C5EqyAZh3eDsPpFTrFXrMefWnMegxZLq9wUYUUkosX'
+    }
+  ],
+  pagination: {
+    page: 105,
+    limit: 3,
+    total: 1761
+  }
+}
 
 const tabs = [
   'activity',
@@ -99,7 +206,7 @@ function Token ({ identifier }) {
               Activity
             </TabPanel>
             <TabPanel>
-              Holders
+              <IdentitiesList identities={mockIdentities.resultSet} loading={false} error={false}/>
             </TabPanel>
           </TabPanels>
         </Tabs>
