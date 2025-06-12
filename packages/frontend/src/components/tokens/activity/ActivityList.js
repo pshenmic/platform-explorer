9ff8377d7414db3193624808a8ab328c78e2c2a7
Alexey <andreewsky.alexey@gmail.com>
2025-06-13 01:43:53 +0300

    Implement ScrollZone to ActivityList

--- packages/frontend/src/components/tokens/activity/ActivityList.js
+++ packages/frontend/src/components/tokens/activity/ActivityList.js
@@ -24,43 +24,45 @@ export default function ActivityList ({
 
   return (
     <div className={'ActivityList'}>
-      <Grid className={`ActivityList__ColumnTitles ${headerExtraClass[headerStyles] || ''}`}>
-        <GridItem className={'ActivityList__ColumnTitle ActivityList__ColumnTitle--Timestamp'}>
-          Time
-        </GridItem>
-        <GridItem className={'ActivityList__ColumnTitle ActivityList__ColumnTitle--Hash'}>
-          Hash
-        </GridItem>
-        <GridItem className={'ActivityList__ColumnTitle ActivityList__ColumnTitle--Creator'}>
-          Tx Creator
-        </GridItem>
-        <GridItem className={'ActivityList__ColumnTitle ActivityList__ColumnTitle--Recipient'}>
-          Recipient
-        </GridItem>
-        <GridItem className={'ActivityList__ColumnTitle ActivityList__ColumnTitle--Amount'}>
-          Amount {!isLargeScreen && <>Tokens</>}
-        </GridItem>
-        <GridItem className={'ActivityList__ColumnTitle ActivityList__ColumnTitle--Type'}>
-          Type
-        </GridItem>
-      </Grid>
+      <div className={'ActivityList__ScrollZone'}>
+        <Grid className={`ActivityList__ColumnTitles ${headerExtraClass[headerStyles] || ''}`}>
+          <GridItem className={'ActivityList__ColumnTitle ActivityList__ColumnTitle--Timestamp'}>
+            Time
+          </GridItem>
+          <GridItem className={'ActivityList__ColumnTitle ActivityList__ColumnTitle--Hash'}>
+            Hash
+          </GridItem>
+          <GridItem className={'ActivityList__ColumnTitle ActivityList__ColumnTitle--Creator'}>
+            Tx Creator
+          </GridItem>
+          <GridItem className={'ActivityList__ColumnTitle ActivityList__ColumnTitle--Recipient'}>
+            Recipient
+          </GridItem>
+          <GridItem className={'ActivityList__ColumnTitle ActivityList__ColumnTitle--Amount'}>
+            Amount {!isLargeScreen && <>Tokens</>}
+          </GridItem>
+          <GridItem className={'ActivityList__ColumnTitle ActivityList__ColumnTitle--Type'}>
+            Type
+          </GridItem>
+        </Grid>
 
-      {!loading
-        ? <div className={'ActivityList__Items'}>
-            {activities?.map((activity, key) => (
-              <ActivityListItem
-                key={key}
-                activity={activity}
-                rate={rate}
-              />
-            ))}
-            {activities?.length === 0 &&
-              <EmptyListMessage>There are no activities yet.</EmptyListMessage>
-            }
-            {activities === undefined && <ErrorMessageBlock/>}
-          </div>
-        : <LoadingList itemsCount={itemsCount}/>
-      }
+        {!loading
+          ? <div className={'ActivityList__Items'}>
+              {activities?.map((activity, key) => (
+                <ActivityListItem
+                  key={key}
+                  activity={activity}
+                  rate={rate}
+                />
+              ))}
+              {activities?.length === 0 &&
+                <EmptyListMessage>There are no activities yet.</EmptyListMessage>
+              }
+              {activities === undefined && <ErrorMessageBlock/>}
+            </div>
+          : <LoadingList itemsCount={itemsCount}/>
+        }
+      </div>
 
       {pagination &&
         <Pagination
