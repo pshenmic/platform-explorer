91e203d5c31eb8c5c84c72f3442fa8497b677505
Alexey <andreewsky.alexey@gmail.com>
2025-07-22 22:35:26 +0300

    Remove outdated TokenTransitionBadge

--- packages/frontend/src/components/tokens/index.js
+++ packages/frontend/src/components/tokens/index.js
@@ -1,5 +1,4 @@
 export { default as TokenTransitionCard } from './TokenTransitionCard'
-export { default as TokenTransitionBadge } from './TokenTransitionBadge'
 export { default as Supply } from './Supply'
 export { default as TickerBadge } from './TickerBadge'
 export { default as TokenFilters } from './TokenFilters'
