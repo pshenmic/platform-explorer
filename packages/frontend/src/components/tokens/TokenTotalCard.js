a8bc3082e8b625ff436f148d266a44e01b9ef012
Alexey <andreewsky.alexey@gmail.com>
2025-07-22 18:14:14 +0300

    Replace mock fields with data from parameters in the TokenTotalCard

--- packages/frontend/src/components/tokens/TokenTotalCard.js
+++ packages/frontend/src/components/tokens/TokenTotalCard.js
@@ -5,16 +5,49 @@ import { HorisontalSeparator } from '../ui/separators'
 import { SmoothSize, ValueContainer } from '../ui/containers'
 import { Button } from '@chakra-ui/react'
 import { ChevronIcon } from '../ui/icons'
-import { findActiveAlias } from '../../util'
+import { findActiveAlias, getTokenName } from '../../util'
 import TokenDigestCard from './TokenDigestCard'
 import { ValueCard } from '../cards'
 import { LocalisationList } from './localisation'
 import { useState } from 'react'
+import ImageGenerator from '../imageGenerator'
 import './TokenTotalCard.scss'
 
-const LocalisationTranslations = ({ className, show, localisations = [] }) => (
+const m = {
+  identifier: '4xd9usiX6WCPE4h1AFPQBJ4Rje6TfZw8kiBzkSAzvmCL',
+  position: 0,
+  timestamp: '2025-07-15T09:14:43.782Z',
+  description: null,
+  localizations: {
+    en: {
+      pluralForm: 'A1-preprog-1',
+      singularForm: 'A1-preprog-1',
+      shouldCapitalize: true
+    }
+  },
+  baseSupply: '100000',
+  totalSupply: '101310',
+  maxSupply: null,
+  owner: 'DTFPLKMVbnkVQWEfkxHX7Ch62ytjvbtqH6eG1TF3nMbD',
+  mintable: true,
+  burnable: true,
+  freezable: true,
+  unfreezable: true,
+  destroyable: true,
+  allowedEmergencyActions: true,
+  dataContractIdentifier: 'BU9B9aoh54Y8aXqRnrD6zmerxivw1ePLeARSmqGm52eN',
+  changeMaxSupply: true,
+  distributionType: null,
+  totalGasUsed: 921522940,
+  mainGroup: null,
+  totalTransitionsCount: 13,
+  totalFreezeTransitionsCount: 1,
+  totalBurnTransitionsCount: 0
+}
+
+const LocalisationTranslations = ({ className, show, localisations = {} }) => (
   <SmoothSize className={className || ''}>
-    {localisations.length > 0 &&
+    {Object.keys(localisations).length > 0 &&
       <LocalisationList
         className={`TokenTotalCard__LocalisationList ${show ? 'TokenTotalCard__LocalisationList--Show' : ''}`}
         localisations={localisations}
@@ -27,13 +60,36 @@ function TokenTotalCard ({ token, loading }) {
   const activeAlias = findActiveAlias(token.data?.aliases)
   const [showLocalisations, setShowLocalisations] = useState(false)
 
-  const mockLocalisations = [
-    { language: 'English', singular: 'Tether', plural: 'Tethers', capitalize: true },
-    { language: 'Russian', singular: 'Тэзэр', plural: 'Тэзэры', capitalize: true },
-    { language: 'Japanese', singular: 'ダッシュ (Tetheruu)', plural: 'ダッシュズ (Tetheeres)', capitalize: true },
-    { language: 'Thai', singular: 'แดช (Tethe)', plural: 'แดชหลายตัว (Te le teher)', capitalize: true },
-    { language: 'Chinese', singular: '达世币 (Tethrr)', plural: '达世币们 (Telother)', capitalize: true }
-  ]
+  console.log('token', token)
+  const {
+    identifier,
+    position,
+    timestamp,
+    description,
+    localizations,
+    baseSupply,
+    totalSupply,
+    maxSupply,
+    owner,
+    mintable,
+    burnable,
+    freezable,
+    unfreezable,
+    destroyable,
+    allowedEmergencyActions,
+    dataContractIdentifier,
+    changeMaxSupply,
+    distributionType,
+    totalGasUsed,
+    mainGroup,
+    totalTransitionsCount,
+    totalFreezeTransitionsCount,
+    totalBurnTransitionsCountDecimals
+  } = token?.data || {}
+
+  console.log('identifieridentifier', identifier)
+
+  const localizationsCount = Object.keys(localizations || {}).length
 
   return (
     <div className={`InfoBlock InfoBlock--Gradient tokenPage__CommonInfo TokenTotalCard ${loading ? 'TokenTotalCard--Loading' : ''} `}>
@@ -58,20 +114,26 @@ function TokenTotalCard ({ token, loading }) {
                     styles={['highlight-both']}
                     ellipsis={false}
                   >
-                    CoUR4uj27qcnY29ZDLmDj33RhLyMDp9z8spDBjpZ9r3o
+                    {identifier}
                   </Identifier>
                 )}
               />
               <InfoLine
                 className={'TokenTotalCard__InfoLine TokenTotalCard__InfoLine--Balance'}
                 title={'Name'}
-                value={'Tether'}
+                value={getTokenName(localizations)}
                 loading={loading}
                 error={token.error}
               />
             </div>
             <div className={'TokenTotalCard__Avatar'}>
-              image
+              <ImageGenerator
+                username={identifier}
+                lightness={50}
+                saturation={50}
+                width={88}
+                height={88}
+              />
             </div>
           </div>
 
@@ -81,9 +143,9 @@ function TokenTotalCard ({ token, loading }) {
             <InfoLine
               className={'TokenTotalCard__InfoLine'}
               title={'Token Info'}
-              value={'The first stablecoin on Dash. Tether is everywhere, even on Platform-explorer thanks to Pshenmic team and DCG'}
+              value={description}
               loading={loading}
-              error={token.error || (!loading && token.data?.revision === undefined)}
+              error={token.error || (!loading && !description)}
             />
             <InfoLine
               className={'TokenTotalCard__InfoLine'}
@@ -95,41 +157,41 @@ function TokenTotalCard ({ token, loading }) {
             <InfoLine
               className={'TokenTotalCard__InfoLine'}
               title={'Token Contract Position'}
-              value={<ValueCard link={'/tokens'}>22</ValueCard>}
+              value={position}
               loading={loading}
-              error={token.error}
+              error={token.error || (!loading && position == null)}
             />
             <InfoLine
               className={'TokenTotalCard__InfoLine TokenTotalCard__InfoLine--DataContract'}
               title={'Data Contract'}
               value={
-                <ValueCard link={`/dataContract/${'Cgjuqav7uD7FnWNqNyHEWet382Yzx7NYNRUNqfJ3d2je'}`}>
+                <ValueCard link={`/dataContract/${dataContractIdentifier}`}>
                   <Identifier avatar={true} copyButton={true} ellipsis={false} styles={['highlight-both']}>
-                    Cgjuqav7uD7FnWNqNyHEWet382Yzx7NYNRUNqfJ3d2je
+                    {dataContractIdentifier}
                   </Identifier>
                 </ValueCard>
               }
               loading={loading}
-              error={token.error}
+              error={token.error || (!loading && !dataContractIdentifier)}
             />
             <InfoLine
               className={'TokenTotalCard__InfoLine'}
               title={'Creation date'}
-              value={<DateBlock timestamp={new Date().toISOString()} showTime={true}/>}
+              value={<DateBlock timestamp={timestamp || null} showTime={true}/>}
               loading={loading}
-              error={token.error}
+              error={token.error || (!loading && !timestamp)}
             />
             <InfoLine
               className={'TokenTotalCard__InfoLine TokenTotalCard__InfoLine--Localisation'}
               title={'Localisation'}
-              value={mockLocalisations?.length
+              value={localizationsCount > 0
                 ? <Button
                     className={'TokenTotalCard__LocalisationShowButton'}
                     size={'sm'}
-                    variant={showLocalisations && mockLocalisations?.length > 0 ? 'gray' : 'blue'}
+                    variant={showLocalisations && localizationsCount > 0 ? 'gray' : 'blue'}
                     onClick={() => setShowLocalisations(prev => !prev)}
                   >
-                    {mockLocalisations?.length} translations
+                    {localizationsCount} translations
                     <ChevronIcon ml={'4px'} h={'10px'} w={'10px'}
                                  transform={`rotate(${showLocalisations ? '-90deg' : '90deg'})`}/>
                   </Button>
@@ -139,7 +201,7 @@ function TokenTotalCard ({ token, loading }) {
               error={token.error}
             />
             <LocalisationTranslations
-              localisations={mockLocalisations}
+              localisations={localizations}
               show={showLocalisations}
               className={`TokenTotalCard__LocalisationListContainer TokenTotalCard__LocalisationListContainer--Mobile ${showLocalisations
                 ? ' TokenTotalCard__LocalisationListContainer--Opened'
@@ -156,7 +218,7 @@ function TokenTotalCard ({ token, loading }) {
         </div>
       </div>
       <LocalisationTranslations
-        localisations={mockLocalisations}
+        localisations={localizations}
         show={showLocalisations}
         className={`TokenTotalCard__LocalisationListContainer TokenTotalCard__LocalisationListContainer--Desktop ${showLocalisations
           ? ' TokenTotalCard__LocalisationListContainer--Opened'
