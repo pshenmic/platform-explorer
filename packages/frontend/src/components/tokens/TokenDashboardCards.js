84ead0ac9ea0c58191a276a74f37954aef8527ed
Alexey <andreewsky.alexey@gmail.com>
2025-07-29 18:33:57 +0300

    Update TokenDashboardCards

--- packages/frontend/src/components/tokens/TokenDashboardCards.js
+++ packages/frontend/src/components/tokens/TokenDashboardCards.js
@@ -1,87 +1,30 @@
 'use client'
 
-import { useState } from 'react'
-import { Flex, Box } from '@chakra-ui/react'
-import Link from 'next/link'
-import { InfoCard } from '../cards'
+import { DashboardCards } from '../cards'
+import { TokenCardContent } from '../cards/dashboard'
 import { ErrorMessageBlock } from '../Errors'
-import { Identifier } from '../data'
-import { Slider, SliderElement } from '../ui/Slider'
-import { WheelControls } from '../ui/Slider/plugins'
-import { TickerBadge } from './index'
+import { NotActive } from '../data'
 import './TokenDashboardCards.scss'
 import './TokenDashboardCard.scss'
 
-function TokenDashboardCard ({ token, className, loading = false }) {
+function TokenDashboardCards ({ items, error, loading, className }) {
+  const cards = items?.map(token => ({
+    value: <TokenCardContent token={token} />,
+    className: 'TokenDashboardCards__Card',
+    error: false,
+    loading,
+    link: `/token/${token?.identifier}`
+  })) || Array.from({ length: 6 }, () => ({ loading: true }))
+
   return (
-    <InfoCard className={`TokenDashboardCard ${className || ''}`} loading={loading} clickable={true}>
-      {!loading
-        ? <Link href={`/token/${token?.identifier}`}>
-          <Flex mb={1} gap={'0.75rem'} alignItems={'center'}>
-            <div className={'TokenDashboardCard__Name'}>{token?.name}</div>
-            {token?.ticker &&
-              <TickerBadge size={'lg'} className={'TokenDashboardCard__Ticker'}>
-                {token.ticker}
-              </TickerBadge>
-            }
-          </Flex>
-          <Identifier className={'TokenDashboardCard__Id'} ellipsis={true} styles={['highlight-both']}>{token.identifier}</Identifier>
-        </Link>
-        : <Box h={'55px'}/>
+    <div className={`TokenDashboardCards ${loading ? 'TokenDashboardCards--Loading' : ''} ${className || ''}`}>
+      {!error
+        ? !loading && (!items || items.length === 0)
+            ? <NotActive>No tokens available</NotActive>
+            : <DashboardCards cards={cards} columnLayout={[3, 3]}/>
+        : <ErrorMessageBlock h={250}/>
       }
-    </InfoCard>
-  )
-}
-
-function TokenDashboardCards ({ items, className }) {
-  const [sliderLoaded, setSliderLoaded] = useState(false)
-
-  const chunkArray = (array, chunkSize) => {
-    const result = []
-    for (let i = 0; i < array.length; i += chunkSize) {
-      result.push(array.slice(i, i + chunkSize))
-    }
-    return result
-  }
-
-  const columns = chunkArray(items?.data?.resultSet, 3)
-
-  return (
-    !items.error
-      ? <div className={`TokenDashboardCards ${className || ''}`}>
-        <Slider
-          className={'TokenDashboardCards__Slider'}
-          settings={{
-            rubberband: false,
-            renderMode: 'performance',
-            breakpoints: {
-              '(max-width: 48em)': {
-                slides: {
-                  origin: 'center',
-                  perView: 1.1
-                }
-              }
-            },
-            slides: { perView: 2 }
-          }}
-          createdCallback={() => setSliderLoaded(true)}
-          plugins={[WheelControls]}
-        >
-          {columns.map((column, columnIndex) => (
-            <SliderElement className={'TokenDashboardCards__CardsColumn'} key={columnIndex} style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
-              {column.map((token, i) => (
-                <TokenDashboardCard
-                  className={'TokenDashboardCards__Card'}
-                  token={token}
-                  loading={!sliderLoaded}
-                  key={i}
-                />
-              ))}
-            </SliderElement>
-          ))}
-        </Slider>
-      </div>
-      : <ErrorMessageBlock h={250}/>
+    </div>
   )
 }
 
