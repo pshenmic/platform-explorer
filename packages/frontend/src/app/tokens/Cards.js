f80ab182144e88e59c59d75a755f57ec88aaed39
Alexey <andreewsky.alexey@gmail.com>
2025-07-29 18:35:50 +0300

    Implement api request of trending tokens in Tokens Intro block

--- packages/frontend/src/app/tokens/Cards.js
+++ packages/frontend/src/app/tokens/Cards.js
@@ -1,72 +1,33 @@
 'use client'
 
+import * as Api from '../../util/Api'
 import { TokenDashboardCards } from '../../components/tokens/TokenDashboardCards'
+import { useEffect, useState } from 'react'
+import { fetchHandlerError, fetchHandlerSuccess } from '../../util'
+import { Flex } from '@chakra-ui/react'
+import styles from './Cards.module.scss'
 
 export default function Cards () {
-  const tokens = {
-    data: {
-      resultSet: [
-        {
-          name: 'BetaCoin',
-          ticker: 'BTCN',
-          identifier: 'DFTG9G5mzmb4ANpoTtfRigrPjnrYTapHvznnCXNA3vfF',
-          dataContract: 'DFTG9G5mzmb4ANpoTtfRigrPjnrYTapHvznnCXNA3vfF',
-          currentSupply: '40 MLN',
-          maxSupply: '50 MLN',
-          ownerIdentity: 'FXyN2NZAdRFADg7CeGjfUBdEqGPRw8RJXkpN5r4tMuLQ'
-        },
-        {
-          name: 'BetaCoin',
-          ticker: 'BTCN',
-          identifier: 'DFTG9G5mzmb4ANpoTtfRigrPjnrYTapHvznnCXNA3vfF',
-          dataContract: 'DFTG9G5mzmb4ANpoTtfRigrPjnrYTapHvznnCXNA3vfF',
-          currentSupply: '40 MLN',
-          maxSupply: '50 MLN',
-          ownerIdentity: 'FXyN2NZAdRFADg7CeGjfUBdEqGPRw8RJXkpN5r4tMuLQ'
-        },
-        {
-          name: 'BetaCoin',
-          ticker: 'BTCN',
-          identifier: 'DFTG9G5mzmb4ANpoTtfRigrPjnrYTapHvznnCXNA3vfF',
-          dataContract: 'DFTG9G5mzmb4ANpoTtfRigrPjnrYTapHvznnCXNA3vfF',
-          currentSupply: '40 MLN',
-          maxSupply: '50 MLN',
-          ownerIdentity: 'FXyN2NZAdRFADg7CeGjfUBdEqGPRw8RJXkpN5r4tMuLQ'
-        },
-        {
-          name: 'BetaCoin',
-          ticker: 'BTCN',
-          identifier: 'DFTG9G5mzmb4ANpoTtfRigrPjnrYTapHvznnCXNA3vfF',
-          dataContract: 'DFTG9G5mzmb4ANpoTtfRigrPjnrYTapHvznnCXNA3vfF',
-          currentSupply: '40 MLN',
-          maxSupply: '50 MLN',
-          ownerIdentity: 'FXyN2NZAdRFADg7CeGjfUBdEqGPRw8RJXkpN5r4tMuLQ'
-        },
-        {
-          name: 'BetaCoin',
-          ticker: 'BTCN',
-          identifier: 'DFTG9G5mzmb4ANpoTtfRigrPjnrYTapHvznnCXNA3vfF',
-          dataContract: 'DFTG9G5mzmb4ANpoTtfRigrPjnrYTapHvznnCXNA3vfF',
-          currentSupply: '40 MLN',
-          maxSupply: '50 MLN',
-          ownerIdentity: 'FXyN2NZAdRFADg7CeGjfUBdEqGPRw8RJXkpN5r4tMuLQ'
-        },
-        {
-          name: 'BetaCoin',
-          ticker: 'BTCN',
-          identifier: 'DFTG9G5mzmb4ANpoTtfRigrPjnrYTapHvznnCXNA3vfF',
-          dataContract: 'DFTG9G5mzmb4ANpoTtfRigrPjnrYTapHvznnCXNA3vfF',
-          currentSupply: '40 MLN',
-          maxSupply: '50 MLN',
-          ownerIdentity: 'FXyN2NZAdRFADg7CeGjfUBdEqGPRw8RJXkpN5r4tMuLQ'
-        }
-      ]
-    },
-    loading: false,
-    error: false
+  const [tokens, setTokens] = useState({ data: {}, loading: true, error: false })
+
+  const fetchData = () => {
+    setTokens({ data: {}, loading: true, error: false })
+
+    Api.getTokensRating(1, 6, 'desc')
+      .then(res => fetchHandlerSuccess(setTokens, res))
+      .catch(err => fetchHandlerError(setTokens, err))
   }
 
+  useEffect(() => fetchData(), [])
+
   return (
-    <TokenDashboardCards items={tokens}/>
+    <Flex className={styles.Container} direction={'column'} w={'100%'}>
+      <div className={styles.Title}>Trending Tokens of the week:</div>
+      <TokenDashboardCards
+        items={tokens?.data?.resultSet}
+        loading={tokens?.loading}
+        error={tokens?.error}
+      />
+    </Flex>
   )
 }
