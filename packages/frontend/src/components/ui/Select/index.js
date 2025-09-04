5d696d9bed6769d7bd4aa7c28b6fe1d8b4df5f20
Alexey <andreewsky.alexey@gmail.com>
2025-09-04 23:02:36 +0300

    Implement portal props to Select component

--- packages/frontend/src/components/ui/Select/index.js
+++ packages/frontend/src/components/ui/Select/index.js
@@ -1,13 +1,28 @@
+'use client'
+
 import ReactSelect from 'react-select'
 import './Select.scss'
 
-export default function Select ({ value, onChange, options, menuPlacement = 'auto', className }) {
+export default function Select ({
+  value,
+  onChange,
+  options,
+  menuPlacement = 'auto',
+  menuPortalTarget,
+  usePortal = false,
+  className
+}) {
+  const portalTarget = usePortal || menuPortalTarget
+    ? menuPortalTarget ?? document?.body
+    : null
+
   return (
     <ReactSelect
       className={`Select ${className || ''}`}
       isSearchable={false}
       classNamePrefix={'react-select'}
       menuPlacement={menuPlacement}
+      menuPortalTarget={usePortal && (portalTarget ?? null)}
       onChange={onChange}
       {...(value ? { value: { value: value?.value || value, label: value?.label || value?.label || value } } : {})}
       options={options?.map((option) => ({
