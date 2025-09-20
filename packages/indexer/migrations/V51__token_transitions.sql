4f768146a4c0ce182ae2bc3cc39513b5a7fb91cc
pshenmic <pshenmic@gmail.com>
2025-09-20 14:00:07 +0700

    Bump limit to 2048

--- packages/indexer/migrations/V51__token_transitions.sql
+++ packages/indexer/migrations/V51__token_transitions.sql
@@ -4,7 +4,7 @@ CREATE TABLE token_transitions (
       owner varchar(64) NOT NULL,
       action SMALLINT NOT NULL,
       amount bigint,
-      public_note varchar(200),
+      public_note varchar(2048),
       state_transition_hash char(64) NOT NULL references state_transitions(hash),
       token_contract_position SMALLINT NOT NULL,
       data_contract_id int NOT NULL references data_contracts(id),
