50ce31fdc89f8f0fb12781be7d1628cb6aa56fda
Vladislav <vv.timoshenko@vk.team>
2025-09-26 00:14:41 +0300

    support formatting uint64

--- packages/frontend/src/util/numbers.js
+++ packages/frontend/src/util/numbers.js
@@ -1,6 +1,10 @@
 export const formatNumberWithSpaces = (num, decimalPlaces) => {
   if (num === undefined || num === null) return ''
 
+  if (typeof num === 'bigint') {
+    return num.toLocaleString('ru-RU').replace(/,/g, ', ')
+  }
+
   const number = Number(num)
   if (isNaN(number)) return String(num)
 
@@ -11,11 +15,33 @@ export const formatNumberWithSpaces = (num, decimalPlaces) => {
 }
 
 export const formatNumberByDecimals = (str, decimalPlaces) => {
-  const number = Number(str)
+  if (str === undefined || str === null) return '0'
+
+  const strNum = String(str).replace(/[^0-9]/g, '') // Remove non-digits
+  if (strNum === '' || strNum.startsWith('-')) return '0'
+
   const decimal = Number(decimalPlaces)
+  if (isNaN(decimal) || !Number.isInteger(decimal) || decimal <= 0) {
+    return strNum
+  }
 
-  if (isNaN(number)) return 0
-  if (!Number.isInteger(decimal) || decimal <= 0) return number
+  const len = strNum.length
+  let integerPart = '0'
+  let fractionalPart = ''
 
-  return number / (10 ** decimal)
+  if (len > decimal) {
+    integerPart = strNum.slice(0, len - decimal)
+    fractionalPart = strNum.slice(len - decimal)
+  } else {
+    fractionalPart = '0'.repeat(decimal - len) + strNum
+  }
+
+  fractionalPart = fractionalPart.replace(/0+$/, '')
+
+  let result = integerPart
+  if (fractionalPart) {
+    result += ',' + fractionalPart
+  }
+
+  return result
 }
