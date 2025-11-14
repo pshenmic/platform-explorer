1a9a6e4369538c57f6a756f7f185e782ef03c69e
Vladislav <vv.timoshenko@vk.team>
2025-11-15 02:48:07 +0300

    name edit screen layout

--- packages/frontend/src/components/dataContracts/DataContractModal/NameScreen.js
+++ packages/frontend/src/components/dataContracts/DataContractModal/NameScreen.js
@@ -1,7 +1,10 @@
 import { Divider, Input } from '@chakra-ui/react'
 import { useId, useState } from 'react'
+import { cva } from 'class-variance-authority'
 
 import styles from './NameScreen.module.scss'
+
+const button = cva([styles.btn])
 
 export const NameScreen = () => {
   const [form, setForm] = useState({ name: '' })
@@ -13,14 +16,26 @@ export const NameScreen = () => {
         <Divider />
       </div>
       <form className={styles.form}>
-        <label className={styles.label} for={id}>Data Contract Name:</label>
+        <label
+          className={styles.label}
+          for={id}
+        >
+          Data Contract Name:
+        </label>
         <Input
+          placeholder='Enter Name...'
           id={id}
           value={form.name}
           className={styles.input}
           onChange={(e) => setForm({ name: e.target.value })}
         />
       </form>
+      <div className={styles.controls}>
+        <button className={button({ className: styles.cancel })}>Cancel</button>
+        <button className={button({ className: styles.submit })}>
+          Submit Changes
+        </button>
+      </div>
     </div>
   )
 }
