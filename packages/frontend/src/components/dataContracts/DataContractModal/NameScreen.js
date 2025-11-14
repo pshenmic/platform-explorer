3f909610a63e1aad9062dd9ea518dde721eb91bf
Vladislav <vv.timoshenko@vk.team>
2025-11-14 23:13:38 +0300

    Name Screen

--- packages/frontend/src/components/dataContracts/DataContractModal/NameScreen.js
+++ packages/frontend/src/components/dataContracts/DataContractModal/NameScreen.js
@@ -1,15 +1,26 @@
-import { Input } from '@chakra-ui/react'
-import { useState } from 'react'
+import { Divider, Input } from '@chakra-ui/react'
+import { useId, useState } from 'react'
+
+import styles from './NameScreen.module.scss'
 
 export const NameScreen = () => {
   const [form, setForm] = useState({ name: '' })
+  const id = useId()
 
   return (
-        <form>
-            <label>
-                Data Contract Name:
-                <Input value={form.name} onChange={(e) => setForm({ name: e.target.value })}/>
-            </label>
-        </form>
+    <div className={styles.root}>
+      <div className={styles.divider}>
+        <Divider />
+      </div>
+      <form className={styles.form}>
+        <label className={styles.label} for={id}>Data Contract Name:</label>
+        <Input
+          id={id}
+          value={form.name}
+          className={styles.input}
+          onChange={(e) => setForm({ name: e.target.value })}
+        />
+      </form>
+    </div>
   )
 }
