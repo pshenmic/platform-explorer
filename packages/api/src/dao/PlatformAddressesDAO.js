99775b1624cc287b5d99bedb9f20231c959d078d
owl352 <kokosinca123@gmail.com>
2026-03-14 16:01:43 +0300

    total outgoing, total incoming

--- packages/api/src/dao/PlatformAddressesDAO.js
+++ packages/api/src/dao/PlatformAddressesDAO.js
@@ -12,26 +12,42 @@ module.exports = class PlatformAddressesDAO {
       .orWhereRaw('LOWER(bech32m_address) = ?', [address.toLowerCase()])
       .limit(1)
 
-    const unionTransitions = this.knex('platform_address_transitions')
-      .join('address_subquery', 'platform_address_transitions.sender_id', 'address_subquery.id')
-      .select('state_transition_id', this.knex.raw('0 as incoming'))
-      .union(this.knex
-        .select('state_transition_id', this.knex.raw('1 as incoming'))
-        .from('platform_address_transitions')
-        .join('address_subquery', 'platform_address_transitions.recipient_id', 'address_subquery.id'))
-      .groupBy('state_transition_id')
+    const unionTransitions = this.knex
+      .with('address_subquery', addressSubquery)
+      .unionAll([
+        this.knex('platform_address_transitions')
+          .join('address_subquery', 'platform_address_transitions.sender_id', 'address_subquery.id')
+          .select(
+            'state_transition_id',
+            this.knex.raw('0 as incoming'),
+            this.knex.raw('SUM(amount) as amount')
+          )
+          .groupBy('state_transition_id'),
+
+        this.knex('platform_address_transitions')
+          .join('address_subquery', 'platform_address_transitions.recipient_id', 'address_subquery.id')
+          .select(
+            'state_transition_id',
+            this.knex.raw('1 as incoming'),
+            this.knex.raw('SUM(amount) as amount')
+          )
+          .groupBy('state_transition_id')
+      ])
 
     const [row] = await this.knex('address_subquery')
       .with('address_subquery', addressSubquery)
       .with('unique_transitions', unionTransitions)
       .select('address as base58_address', 'bech32m_address')
-      .select('txs_count.total_txs as total_txs', 'txs_count.incoming_txs as incoming_txs', 'txs_count.outgoing_txs as outgoing_txs')
+      .select('txs_count.total_txs as total_txs', 'txs_count.incoming_txs as incoming_txs', 'txs_count.outgoing_txs as outgoing_txs',
+        'txs_count.total_incoming_amount as total_incoming_amount', 'txs_count.total_outgoing_amount as total_outgoing_amount')
       .leftJoin(
         this.knex('unique_transitions')
           .select(
             this.knex.raw('count(*) as total_txs'),
             this.knex.raw('count(*) FILTER (WHERE incoming = 1) as incoming_txs'),
-            this.knex.raw('count(*) FILTER (WHERE incoming = 0) as outgoing_txs')
+            this.knex.raw('count(*) FILTER (WHERE incoming = 0) as outgoing_txs'),
+            this.knex.raw('SUM(amount) FILTER (WHERE incoming = 1) as total_incoming_amount'),
+            this.knex.raw('SUM(amount) FILTER (WHERE incoming = 0) as total_outgoing_amount')
           )
           .as('txs_count'),
         this.knex.raw('1 = 1')
--- packages/api/src/models/PlatformAddress.js
+++ packages/api/src/models/PlatformAddress.js
@@ -6,8 +6,10 @@ module.exports = class PlatformAddress {
   outgoingTxs
   nonce
   balance
+  totalIncomingAmount
+  totalOutgoingAmount
 
-  constructor(base58Address, bech32mAddress, totalTxs, incomingTxs, outgoingTxs, nonce, balance) {
+  constructor(base58Address, bech32mAddress, totalTxs, incomingTxs, outgoingTxs, totalIncomingAmount, totalOutgoingAmount, nonce, balance) {
     this.base58Address = base58Address ?? null
     this.bech32mAddress = bech32mAddress ?? null
     this.totalTxs = totalTxs ?? null
@@ -15,14 +17,16 @@ module.exports = class PlatformAddress {
     this.outgoingTxs = outgoingTxs ?? null
     this.nonce = nonce ?? null
     this.balance = balance ?? null
+    this.totalIncomingAmount = totalIncomingAmount ?? null
+    this.totalOutgoingAmount = totalOutgoingAmount ?? null
   }
 
   // eslint-disable-next-line
-  static fromRow({base58_address, bech32m_address, total_txs, incoming_txs, outgoing_txs}) {
-    return new PlatformAddress(base58_address, bech32m_address, Number(total_txs), Number(incoming_txs), Number(outgoing_txs))
+  static fromRow({base58_address, bech32m_address, total_txs, incoming_txs, outgoing_txs, total_incoming_amount, total_outgoing_amount}) {
+    return new PlatformAddress(base58_address, bech32m_address, Number(total_txs), Number(incoming_txs), Number(outgoing_txs), total_incoming_amount, total_outgoing_amount)
   }
 
-  static fromObject({base58Address, bech32mAddress, totalTxs, incomingTxs, outgoingTxs, nonce, balance}) {
-    return new PlatformAddress(base58Address, bech32mAddress, totalTxs, incomingTxs, outgoingTxs, nonce, balance)
+  static fromObject({base58Address, bech32mAddress, totalTxs, incomingTxs, outgoingTxs, nonce, balance, totalIncomingAmount, totalOutgoingAmount}) {
+    return new PlatformAddress(base58Address, bech32mAddress, totalTxs, incomingTxs, outgoingTxs, totalIncomingAmount, totalOutgoingAmount, nonce, balance)
   }
 }
