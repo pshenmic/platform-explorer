4f716cc0d8159218c8c6526782cc324813c7e431
Alexey <andreewsky.alexey@gmail.com>
2026-03-19 14:02:35 +0300

    Create PlatformAddressTotalCard and PlatformAddressDigestCard

--- packages/frontend/src/app/platformAddress/[hash]/PlatformAddress.js
+++ packages/frontend/src/app/platformAddress/[hash]/PlatformAddress.js
@@ -6,11 +6,11 @@ import { useQuery } from '@tanstack/react-query'
 import { useQueryState, parseAsStringEnum } from 'nuqs'
 import { Container, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import * as Api from '../../../util/Api'
-import { InfoLine, CreditsBlock, Identifier, NotActive } from '../../../components/data'
 import { PageDataContainer, InfoContainer } from '../../../components/ui/containers'
 import { ErrorMessageBlock } from '../../../components/Errors'
 import { TransactionsList } from '../../../components/transactions'
 import { useBreadcrumbs } from '../../../contexts/BreadcrumbsContext'
+import { PlatformAddressTotalCard } from '../../../components/platformAddresses'
 import './PlatformAddress.scss'
 
 const tabs = ['transitions']
@@ -60,84 +60,18 @@ function PlatformAddress () {
     <PageDataContainer className={'PlatformAddress'} title={'Platform Address Info'}>
       {address.isError
         ? <ErrorMessageBlock h={'450px'} />
-        : (
-          <div className={'PlatformAddress__CommonInfo'}>
-            <InfoLine
-              title={'Bech32m Address'}
-              value={
-                <Identifier copyButton={true} ellipsis={false} styles={['highlight-both']}>
-                  {address.data?.bech32mAddress}
-                </Identifier>
-              }
-              loading={address.isLoading}
-              error={address.isError}
-            />
-
-            <InfoLine
-              title={'Balance'}
-              value={<CreditsBlock credits={address.data?.balance} rate={rate} />}
-              loading={address.isLoading}
-              error={address.isError}
-            />
-
-            <InfoLine
-              title={'Nonce'}
-              value={address.data?.nonce ?? <NotActive />}
-              loading={address.isLoading}
-              error={address.isError}
-            />
-
-            <InfoLine
-              title={'Total Transactions'}
-              value={address.data?.totalTxs ?? <NotActive />}
-              loading={address.isLoading}
-              error={address.isError}
-            />
-
-            <InfoLine
-              title={'Incoming Transactions'}
-              value={address.data?.incomingTxs ?? <NotActive />}
-              loading={address.isLoading}
-              error={address.isError}
-            />
-
-            <InfoLine
-              title={'Outgoing Transactions'}
-              value={address.data?.outgoingTxs ?? <NotActive />}
-              loading={address.isLoading}
-              error={address.isError}
-            />
-
-            <InfoLine
-              title={'Total Incoming Amount'}
-              value={address.data?.totalIncomingAmount != null
-                ? <CreditsBlock credits={address.data.totalIncomingAmount} rate={rate} />
-                : <NotActive />}
-              loading={address.isLoading}
-              error={address.isError}
-            />
-
-            <InfoLine
-              title={'Total Outgoing Amount'}
-              value={address.data?.totalOutgoingAmount != null
-                ? <CreditsBlock credits={address.data.totalOutgoingAmount} rate={rate} />
-                : <NotActive />}
-              loading={address.isLoading}
-              error={address.isError}
-            />
-          </div>
-        )
+        : <PlatformAddressTotalCard address={address} rate={rate} />
       }
 
       <InfoContainer styles={['tabs']} id={'tabs'}>
         <Tabs onChange={handleTab} index={tabs.indexOf(activeTab)}>
           <TabList>
             <Tab>
-              Transitions{transitions.data?.pagination?.total != null
-                ? <span className={`Tabs__TabItemsCount ${transitions.data.pagination.total === 0 ? 'Tabs__TabItemsCount--Empty' : ''}`}>
-                    {transitions.data.pagination.total}
-                  </span>
-                : ''}
+              Transitions {transitions.data?.pagination?.total != null
+              ? <span className={`Tabs__TabItemsCount ${transitions.data.pagination.total === 0 ? 'Tabs__TabItemsCount--Empty' : ''}`}>
+                  {transitions.data.pagination.total}
+                </span>
+              : ''}
             </Tab>
           </TabList>
           <TabPanels>
