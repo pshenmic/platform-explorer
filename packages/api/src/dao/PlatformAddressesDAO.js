757cc14d0e3d30dad17c23c73c1fceb305ae602d
owl352 <kokosinca123@gmail.com>
2026-03-20 17:44:00 +0300

    fix platform address transaction type

--- packages/api/src/dao/PlatformAddressesDAO.js
+++ packages/api/src/dao/PlatformAddressesDAO.js
@@ -3,6 +3,7 @@ const { NETWORK } = require('../constants')
 const PaginatedResultSet = require('../models/PaginatedResultSet')
 const Transaction = require('../models/Transaction')
 const { getAliasDocumentForIdentifiers, getAliasFromDocument } = require('../utils')
+const StateTransitionEnum = require("../enums/StateTransitionEnum");
 module.exports = class PlatformAddressesDAO {
   constructor (knex, sdk) {
     this.knex = knex
@@ -177,7 +178,8 @@ module.exports = class PlatformAddressesDAO {
       return Transaction.fromRow({
         ...row,
         owner: row.owner,
-        aliases
+        aliases,
+        type: StateTransitionEnum[row.type],
       })
     })
 
