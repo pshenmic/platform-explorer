47f598ee6f4146fc586d87764bbec1773fd2c183
Litvinov Y. <litury@icloud.com>
2026-04-16 20:51:02 +0500

    fix (contestedResources): check connected ref value in VoteManager

--- packages/frontend/src/components/contestedResources/contenders/VoteManager.js
+++ packages/frontend/src/components/contestedResources/contenders/VoteManager.js
@@ -3,10 +3,10 @@ import { VoteControls } from './VoteControls'
 
 import './VoteManager.scss'
 
-export const VoteManeger = ({ connected, connectWallet, identifier, ...other }) => {
-  if (connected) {
-    return <VoteControls contender={identifier} {...other} />
+export const VoteManeger = ({ connected, connectWallet, identifier, walletInfo, ...other }) => {
+  if (connected?.current && walletInfo) {
+    return <VoteControls contender={identifier} walletInfo={walletInfo} {...other} />
   }
 
-  return <Button onClick={connectWallet} className='ContendersListItem__Column_Vote-btn' variant="brand" size="sm">Vote</Button>
+  return <Button onClick={() => connectWallet()} className='ContendersListItem__Column_Vote-btn' variant="brand" size="sm">Vote</Button>
 }
