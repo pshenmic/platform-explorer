3f63ebf2fef9b9a6a7f9e52ae5c097b7872ac9a5
Litvinov Y. <litury@icloud.com>
2026-05-03 12:08:24 +0500

    style (dataContract): refine private key form

--- packages/frontend/src/app/dataContract/create/components/SchemaAtomic/PrivateKeyForm.js
+++ packages/frontend/src/app/dataContract/create/components/SchemaAtomic/PrivateKeyForm.js
@@ -1,4 +1,4 @@
-import { FormControl, FormLabel, Input, Text } from '@chakra-ui/react'
+import { FormControl, FormHelperText, FormLabel, Input } from '@chakra-ui/react'
 import { useDeploy } from '../../DeployContext'
 import { SignerMethod } from '../../useSigner'
 
@@ -12,20 +12,20 @@ export const PrivateKeyForm = () => {
   return (
     <FormControl isDisabled={isInactive}>
       <FormLabel fontSize='xs' color='gray.400' mb={1}>
-        Private Key (WIF)
+        Private Key
       </FormLabel>
       <Input
         size='sm'
         variant='filled'
         type='password'
-        placeholder='WIF private key'
+        placeholder='WIF, hex, or base64'
         value={privateKeyForm.wif}
         onChange={(e) => privateKeyForm.setWif(e.target.value)}
         fontFamily='mono'
       />
-      <Text fontSize='xs' color='gray.500' mt={1}>
-        Key never leaves your browser. Use a testnet key for testing.
-      </Text>
+      <FormHelperText fontSize='xs' color='gray.500' mt={1}>
+        Enter your private key from your Identity
+      </FormHelperText>
     </FormControl>
   )
 }
