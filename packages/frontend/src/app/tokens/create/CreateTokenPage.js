090b0550e12d836528692c97edb0eebe9cb3066a
Litvinov Y. <litury@icloud.com>
2026-05-23 23:31:54 +0500

    feat (tokens-create): add page scaffold

---
+++ packages/frontend/src/app/tokens/create/CreateTokenPage.js
@@ -0,0 +1,38 @@
+'use client'
+
+import { TokenWizardProvider } from './TokenWizardContext'
+import TemplateStrip from './components/TemplateStrip'
+import Essentials from './components/Essentials'
+import PlainEnglishSummary from './components/Preview/PlainEnglishSummary'
+import JsonPreview from './components/Preview/JsonPreview'
+import DeployBar from './components/DeployBar'
+import './CreateTokenPage.scss'
+import './components/Preview/Preview.scss'
+
+function CreateTokenPage () {
+  return (
+    <TokenWizardProvider>
+      <div className='CreateTokenPage'>
+        <div className='CreateTokenPage__TemplateStrip'>
+          <TemplateStrip/>
+        </div>
+
+        <div className='CreateTokenPage__Layout'>
+          <div className='CreateTokenPage__FormPane'>
+            <div className='CreateTokenPage__SectionTitle'>Essentials</div>
+            <Essentials/>
+
+            <div className='CreateTokenPage__SectionTitle'>Deploy</div>
+            <DeployBar/>
+          </div>
+          <div className='CreateTokenPage__PreviewPane'>
+            <PlainEnglishSummary/>
+            <JsonPreview/>
+          </div>
+        </div>
+      </div>
+    </TokenWizardProvider>
+  )
+}
+
+export default CreateTokenPage
