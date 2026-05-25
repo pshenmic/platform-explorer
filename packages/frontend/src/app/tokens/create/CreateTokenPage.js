eba8603cfaad9e33a829fd10d0602ce485c7b140
Litvinov Y. <litury@icloud.com>
2026-05-26 03:36:30 +0500

    refactor (tokens-create): drop template strip + summary

--- packages/frontend/src/app/tokens/create/CreateTokenPage.js
+++ packages/frontend/src/app/tokens/create/CreateTokenPage.js
@@ -1,10 +1,8 @@
 'use client'
 
 import { TokenWizardProvider } from './TokenWizardContext'
-import TemplateStrip from './components/TemplateStrip'
 import Essentials from './components/Essentials'
 import Features from './components/Features'
-import PlainEnglishSummary from './components/Preview/PlainEnglishSummary'
 import JsonPreview from './components/Preview/JsonPreview'
 import DeployBar from './components/DeployBar'
 import './CreateTokenPage.scss'
@@ -16,9 +14,6 @@ function CreateTokenPage () {
       <div className='CreateTokenPage'>
         <div className='CreateTokenPage__Layout'>
           <div className='CreateTokenPage__FormPane'>
-            <div className='CreateTokenPage__SectionTitle'>Template</div>
-            <TemplateStrip/>
-
             <div className='CreateTokenPage__SectionTitle'>Essentials</div>
             <Essentials/>
 
@@ -29,7 +24,6 @@ function CreateTokenPage () {
             <DeployBar/>
           </div>
           <div className='CreateTokenPage__PreviewPane'>
-            <PlainEnglishSummary/>
             <JsonPreview/>
           </div>
         </div>
