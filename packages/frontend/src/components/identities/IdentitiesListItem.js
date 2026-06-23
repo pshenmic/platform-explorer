767e4a0a5456bb1dfd6602ae302916cc9b6cbad6
Litvinov Y. <litury@icloud.com>
2026-06-23 13:44:54 +0500

    feat(identities): rework identities columns, ids and alignment

--- packages/frontend/src/components/identities/IdentitiesListItem.js
+++ packages/frontend/src/components/identities/IdentitiesListItem.js
@@ -29,9 +29,9 @@ function IdentitiesListItem ({ identity }) {
                 />
               : <Identifier
                   className={'IdentitiesListItem__Identifier'}
-                  ellipsis={true}
-                  styles={['highlight-both']}
+                  middleEllipsis={true}
                   avatar={true}
+                  copyButton={true}
                 >
                   {identifier}
                 </Identifier>
