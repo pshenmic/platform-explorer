55e7b2b90098ec2db2a051c06bb258840dfb539c
Litvinov Y. <litury@icloud.com>
2026-07-21 02:25:30 +0500

    feat(cards): CardHead and Presets primitives

--- packages/frontend/src/components/cards/index.js
+++ packages/frontend/src/components/cards/index.js
@@ -1,11 +1,15 @@
 import InfoCard from './InfoCard'
 import ValueCard from './ValueCard'
+import CardHead from './CardHead'
+import Presets from './Presets'
 import { CardsGrid, CardsGridItems, CardsGridItem, CardsGridHeader, CardsGridTitle } from './CardsGrid'
 import DashboardCards from './DashboardCards'
 
 export {
   InfoCard,
   ValueCard,
+  CardHead,
+  Presets,
   CardsGrid,
   CardsGridItems,
   CardsGridItem,
