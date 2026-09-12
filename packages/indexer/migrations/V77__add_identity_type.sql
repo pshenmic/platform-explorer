1c1505da53514566dd224549114915b5003175f4
owl352 <kokosinca123@gmail.com>
2026-09-12 22:27:47 +0500

    fix migrations

--- packages/indexer/migrations/V77__add_identity_type.sql
+++ packages/indexer/migrations/V77__add_identity_type.sql
@@ -1,7 +1,3 @@
 ALTER TABLE identities ADD COLUMN "type" varchar(24) NOT NULL DEFAULT 'regular';
 
--- identities without a state transition were written by handle_validator,
--- which only ever created masternode owner identities before this migration
-UPDATE identities SET type = 'masternode' WHERE state_transition_hash IS NULL;
-
 CREATE INDEX identities_type ON identities(type);
